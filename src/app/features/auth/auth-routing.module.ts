d9f8b5de6a3168032f93d72595c4736dc1cfb167
kimiti-R <raphael.kimiti@turnkeyafrica.com>
2023-07-26 10:29:57 +0300

    feature-auth: Adding Auth Journey

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,9 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth.component';
+import { VerificationComponent } from './verification/verification.component';
+import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
+  { 
+        path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'verify',
+        component: VerificationComponent,
+      },
+      {
+        path: 'otp',
+        component: OtpVerificationComponent,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+      },
+      {
+        path: 'change-password',
+        component: ChangePasswordComponent,
+      },
+      // {
+      //   path: 'email-otp',
+      //   component: EmailOtpComponent,
+      // },
+      // {
+      //   path: 'modal',
+      //   component: ModalComponent,
+      // },
+      // {
+      //   path: 'pass-modal',
+      //   component: ChangePassModalComponent,
+      // },
+    ],
+  },
 
 ];
 
