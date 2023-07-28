59332aa3747932f1adbfaea23afaecb349cedd1a
Tunde Ogunjimi <tundereuben@yahoo.com>
2023-07-28 11:02:58 +0100

    VQ-571: Sharable OTP & Table component

--- src/app/features/auth/otp-verification/otp-verification.component.ts
+++ src/app/features/auth/otp-verification/otp-verification.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChildren} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../../shared/services/auth.service";
+import {GlobalMessagingService} from "../../../shared/services/messaging/global-messaging.service";
+import {Logger, UtilService} from "../../../shared/services";
+import {untilDestroyed} from "../../../shared/shared.module";
+
+const log = new Logger('OtpVerificationComponent');
 
 @Component({
   selector: 'app-otp-verification',
   templateUrl: './otp-verification.component.html',
   styleUrls: ['./otp-verification.component.css']
 })
-export class OtpVerificationComponent {
+export class OtpVerificationComponent implements OnInit, OnDestroy {
+
+  public otpProcess: string = '';
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.otpProcess = this.route.snapshot.queryParamMap.get('referrer');
+    log.info(`OTP Process: ${this.otpProcess}`);
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  verifyOtp(event: any) {
+    if(event === true){
+
+      switch(this.otpProcess) {
+        case 'password-reset': {
+          this.router.navigate(['/auth/change-password']).then(r => {});
+          break;
+        }
+        default: {
+          const details = JSON.parse(localStorage.getItem('details'));
+          this.authService.attemptAuth(details);
+          break;
+        }
+      }
+     }else{
+      return;
+    }
+  }
 
 }
--- src/app/features/base/components/dashboard/dashboard.component.ts
+++ src/app/features/base/components/dashboard/dashboard.component.ts
@@ -9,4 +9,77 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class DashboardComponent {
 
+  cols = [
+    { field: 'policyNumber', header: 'Policy Number' },
+    { field: 'type', header: 'Type' },
+    { field: 'insured', header: 'Insured' },
+    { field: 'status', header: 'Status' },
+    { field: 'premium', header: 'Premium' },
+  ];
+
+  rows = [
+    { policyNumber: 'TA823151', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00'},
+    { policyNumber: 'TA823152', type: 'Travel', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00'},
+    { policyNumber: 'TA823159', type: 'Marine', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00'},
+    { policyNumber: 'TA823158', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00'},
+    { policyNumber: 'TA823150', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00'},
+  ];
+
+  globalFilterFields = ['policyNumber', 'type', 'insured', 'status', 'premium'];
+
+  tableDetails: TableDetail = {
+    cols: this.cols,
+    rows: this.rows,
+    // rowsPerPage: 2,
+    globalFilterFields: this.globalFilterFields,
+    showFilter: false,
+    showSorting: false,
+    title: 'A list of policies transacted',
+    paginator: true,
+    url: 'entities',
+    urlIdentifier: 'policyNumber'
+  }
+
+  cols2 = [
+    { field: 'policyNumber', header: 'Policy Number' },
+    { field: 'type', header: 'Type' },
+    { field: 'insured', header: 'Insured' },
+    { field: 'status', header: 'Status' },
+    { field: 'premium', header: 'Premium' },
+    { field: 'options', header: 'Options' },
+  ];
+
+  rows2 = [
+    { policyNumber: 'TA823151', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00', options: 'View'},
+    { policyNumber: 'TA823152', type: 'Travel', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00',options: 'View' },
+    { policyNumber: 'TA823159', type: 'Marine', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00', options: 'View'},
+    { policyNumber: 'TA823158', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00', options: 'View'},
+    { policyNumber: 'TA823150', type: 'Motor', insured: 'John Doe', status: 'Active', premium: 'KSH 40,000.00', options: 'View'},
+  ];
+
+  globalFilterFields2 = ['policyNumber', 'type', 'insured', 'status', 'premium'];
+
+  tableDetails2: TableDetail = {
+    cols: this.cols2,
+    rows: this.rows2,
+    // rowsPerPage: 3,
+    globalFilterFields: this.globalFilterFields2,
+    showFilter: false,
+    showSorting: false,
+    title: 'A list of policies transacted',
+    paginator: true,
+  }
+}
+
+export interface TableDetail {
+  cols: {field: string, header: string}[],
+  rows: any[],
+  rowsPerPage?: number,
+  globalFilterFields: string[],
+  showFilter: boolean,
+  showSorting: boolean,
+  title: string,
+  paginator: boolean,
+  url?: string,
+  urlIdentifier?: string,
 }
