9f55406d6e93adc3e1689a696ce65d9d238770d3
Idah Gakii <idah.gakii@turnkeyafrica.com>
2023-07-31 14:26:08 +0300

    Initial Entities Setup

--- src/app/features/entities/entities.module.ts
+++ src/app/features/entities/entities.module.ts
@@ -2,10 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntitiesRoutingModule } from './entities-routing.module';
+import { NewClientComponent } from './components/client/new-client/new-client.component';
+import { ListClientComponent } from './components/client/list-client/list-client.component';
+import { NewEntityComponent } from './components/entity/new-entity/new-entity.component';
+import { ListEntityComponent } from './components/entity/list-entity/list-entity.component';
+import { NewIntermediaryComponent } from './components/intermediary/new-intermediary/new-intermediary.component';
+import { ListIntermediaryComponent } from './components/intermediary/list-intermediary/list-intermediary.component';
+import { NewServiceProviderComponent } from './components/service-provider/new-service-provider/new-service-provider.component';
+import { ListServiceProviderComponent } from './components/service-provider/list-service-provider/list-service-provider.component';
+import { NewStaffComponent } from './components/staff/new-staff/new-staff.component';
+import { ListStaffComponent } from './components/staff/list-staff/list-staff.component';
+import { EditComponent } from './components/edit/edit.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewClientComponent,
+    ListClientComponent,
+    NewEntityComponent,
+    ListEntityComponent,
+    NewIntermediaryComponent,
+    ListIntermediaryComponent,
+    NewServiceProviderComponent,
+    ListServiceProviderComponent,
+    NewStaffComponent,
+    ListStaffComponent,
+    EditComponent
+  ],
   imports: [
     CommonModule,
     EntitiesRoutingModule
