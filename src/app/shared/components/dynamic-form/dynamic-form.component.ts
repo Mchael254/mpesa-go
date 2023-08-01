4d4074dd02b41c62e65fc1f2c544a2045f4e5f50
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-08-01 13:27:22 +0100

    VQ-581 | Dynamic Form.

---
+++ src/app/shared/components/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { DynamicFormFields } from '../../utils/dynamic.form.fields';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.css']
+})
+export class DynamicFormComponent {
+
+  @Input() formFields: DynamicFormFields[];
+  @Output() formSubmitted: EventEmitter<any> = new EventEmitter<any>();
+  @Output() goBackButton: EventEmitter<any> = new EventEmitter<any>();
+  @Input() title: string = 'Dynamic Form';
+  @Input() fieldsPerRow: number = 1; // Input attribute to specify fields per row
+  @Input() actionButtonConfig: any = {
+    submit: { label: 'Save and Continue', visible: true, alignment: 'end' },
+    back: { label: 'Back', visible: true, alignment: 'start' }
+  };
+  rows: any[]; // To store rows of form fields
+  dynamicForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    // this.formFields = this.formConfigService.getFormFields();
+
+    this.rows = this.chunkArray(this.formFields, this.fieldsPerRow);
+
+    const formGroupConfig = {};
+    this.formFields.forEach(field => {
+      const validators = field.required ? [Validators.required] : [];
+      if (field.pattern) {
+        validators.push(Validators.pattern(field.pattern));
+      }
+
+      // formGroupConfig[field.name] = new FormControl(field.defaultValue || '', validators);
+      formGroupConfig[field.name] = new FormControl({ value: field.name || '', disabled: field?.disabled }, validators);
+
+    });
+
+    this.dynamicForm = this.formBuilder.group(formGroupConfig);
+  }
+
+  onSubmitForm() {
+    if (this.dynamicForm.valid) {
+      const formValues = this.dynamicForm.value;
+      this.formSubmitted.emit(formValues); // Emit the event with formValues
+    }
+  }
+
+  goBack() {
+    if (this.dynamicForm.valid) {
+      const formValues = this.dynamicForm.value;
+      this.formSubmitted.emit(formValues); // Emit the event with formValues
+    }
+  }
+
+  chunkArray(array: any[], chunkSize: number) {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+  }
+
+  onCenterAction(){}
+}
