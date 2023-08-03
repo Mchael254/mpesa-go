6c77900e92b68d5c4321acebae880e92a7c8c267
John Dylan <dilanjohn4@gmail.com>
2023-08-03 20:16:24 +0300

    VQ-127: ability to view clients list

---
+++ src/app/features/entities/data/ClientDTO.ts
@@ -0,0 +1,91 @@
+export interface ClientDTO {
+  branchCode: number,
+  category: string,
+  clientTitle: string,
+  clientType: ClientTypeDTO,
+  country: number,
+  createdBy: string,
+  dateOfBirth: string,
+  emailAddress: string,
+  firstName: string,
+  gender: string,
+  id: number,
+  idNumber: string,
+  lastName: string,
+  modeOfIdentity: string,
+  occupation: {
+    id: number,
+    name: string,
+    sector_id: number,
+    short_description: string
+  },
+  passportNumber: string,
+  phoneNumber: string,
+  physicalAddress: string,
+  pinNumber: string,
+  shortDescription: string,
+  status: string,
+  withEffectFromDate: string,
+  clientTypeName: string
+  clientFullName: string
+}
+export interface ClientTypeDTO {
+  category: string,
+  clientTypeName: string,
+  code: number,
+  description: string,
+  organizationId: number,
+  person: string,
+  type: string
+}
+export interface ClientBranchesDto {
+  id: number,
+  shortDescription: string,
+  name: string,
+  physicalAddress: null,
+  emailAddress: null,
+  postAddress: null,
+  town: null,
+  country: null,
+  contact: null,
+  manager: null,
+  telephoneNumber: null,
+  fax: null,
+  state: null,
+  account: null,
+  region: null,
+  postalCode: null,
+  logo: null,
+  emailSource: null,
+  sms_source: null,
+  organizationId: null
+}
+export interface ClientPostDTO {
+  createdBy: string;
+  firstName: string;
+  gender: string;
+  id: number;
+  lastName: string;
+  pinNumber: string;
+  effectiveDateFrom: string;
+  effectiveDateTo: string;
+  partyId: number;
+  status: string;
+  partyTypeShortDesc: string;
+  category: string;
+  countryId: number,
+  dateCreated: string,
+  accountType: number,
+  dateOfBirth: string,
+  organizationId: number,
+  modeOfIdentityid: number,
+  nextOfKinDetailsList: null,
+  // wealthAmlDetails:WealthDTO;
+  // address: AddressDTO;
+  // contactDetails:ContactsDTO;
+  // paymentDetails:PaymentDTO;
+  clientDetails:ClientDetailsDto;
+}
+export interface ClientDetailsDto {
+  clientBranchCode: number,
+}
--- src/app/features/entities/services/client/client.service.ts
+++ src/app/features/entities/services/client/client.service.ts
@@ -1,9 +1,109 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {AppConfigService} from "../../../../core/config/app-config-service";
+import {Observable} from "rxjs";
+import {Pagination} from "../../../../shared/data/common/pagination";
+import {ClientBranchesDto, ClientDTO, ClientTypeDTO} from "../../data/ClientDTO";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  baseUrl = this.appConfig.config.contextPath.accounts_services;
+
+  constructor(
+    private http: HttpClient,
+    private appConfig: AppConfigService
+  ) { }
+
+
+  getClients(
+    page: number = 0,
+    size: number = 5,
+    sortField: string = 'createdDate',
+    order: string = 'desc'
+  ): Observable<Pagination<ClientDTO>> {
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    })
+    const params = new HttpParams()
+      .set('page', `${page}`)
+      .set('size', `${size}`)
+      .set('organizationId', 2)
+      .set('sortListFields', `${sortField}`)
+      .set('order', `${order}`);
+
+    return this.http.get<Pagination<ClientDTO>>(`/${this.baseUrl}/accounts/clients`,
+      {
+        headers: headers,
+        params: params,
+      });
+  }
+
+  searchClients(
+    page: number = 0,
+    size: number = 5,
+    name: string,
+  ): Observable<Pagination<ClientDTO>> {
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+
+    const params = new HttpParams()
+      .set('page', `${page}`)
+      .set('size', `${size}`)
+      .set('name', `${name}`)
+      .set('organizationId', 2);
+
+    return this.http.get<Pagination<ClientDTO>>(`/${this.baseUrl}/accounts/clients`, {
+      headers: header,
+      params: params,
+    });
+  }
+
+  getClientType(organizationId: number): Observable<ClientTypeDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', `${organizationId}`);
+    return this.http.get<ClientTypeDTO[]>(`/${this.baseUrl}/accounts/client-types`,
+      {
+        headers:headers,
+        params:params
+      });
+  }
+
+  saveClientDetails(clientData: ClientDTO): Observable<ClientDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+
+    return this.http.post<ClientDTO[]>(`/${this.baseUrl}/accounts/accounts`, JSON.stringify(clientData), {headers:headers})
+
+  }
+
+  getClientById(id: number): Observable<ClientDTO> {
+    return this.http.get<ClientDTO>(`/${this.baseUrl}/accounts/clients/` + id);
+  }
+
+  getCLientBranches(): Observable<ClientBranchesDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', 2);
+    return this.http.get<ClientBranchesDto[]>(`/${this.baseUrl}/setups/organization-branches`,
+      {
+        headers:headers,
+        params:params
+      });
+  }
 }
