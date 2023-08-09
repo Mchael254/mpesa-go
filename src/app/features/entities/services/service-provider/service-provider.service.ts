3edda96e6b55e67466c8da27d6cee01cfc23e5d1
faithwasike <faith.wasike@turnkeyafrica.com>
2023-08-09 08:30:37 +0300

    VQ-136: Ability to add new service provider

--- src/app/features/entities/services/service-provider/service-provider.service.ts
+++ src/app/features/entities/services/service-provider/service-provider.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppConfigService } from '../../../../core/config/app-config-service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Pagination } from 'src/app/shared/data/common/pagination';
-import { ServiceProviderDTO } from '../../data/ServiceProviderDTO';
+import { ClientTitlesDto, CountryDto, CurrenciesDto, IdentityModeDto, MandatoryFieldsDTO, OccupationDto, ProviderTypeDto, SectorDto, ServiceProviderDTO, TownDto } from '../../data/ServiceProviderDTO';
+import { BankBranchDTO, BankDTO } from 'src/app/shared/data/common/bank-dto';
+import { StateDto } from 'src/app/shared/data/common/countryDto';
 
 
 @Injectable({
@@ -12,6 +14,23 @@ import { ServiceProviderDTO } from '../../data/ServiceProviderDTO';
 export class ServiceProviderService {
 
   baseUrl = this.appConfig.config.contextPath.accounts_services;
+  private entity$ = new BehaviorSubject({
+    partyTypeId: 0,
+    profilePicture: "",
+    categoryName: "",
+    countryId: 0,
+    dateOfBirth: "",
+    effectiveDateFrom: "",
+    effectiveDateTo: "",
+    id: 0,
+    modeOfIdentity: undefined,
+    identityNumber: 0,
+    name: "",
+    organizationId: 0,
+    pinNumber: "",
+    profileImage: ""
+  });
+  currentEntity$ = this.entity$.asObservable();
 
   constructor(
     private http: HttpClient,
@@ -41,5 +60,118 @@ export class ServiceProviderService {
         headers: headers,
         params: params,
       });
+  }
+
+  getMandatoryFieldsByGroupId(groupId: string): Observable<MandatoryFieldsDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    return this.http.get<MandatoryFieldsDTO[]>(`/${this.baseUrl}/setups/form-fields/group/${groupId}` ,{headers:headers});
+  }
+
+  getBanks(countryId: number): Observable<BankDTO[]> {
+    console.log('Fetching Banks');
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+    const params = new HttpParams()
+    .set('countryId', `${countryId}`);
+
+    return this.http.get<BankDTO[]>(`/${this.baseUrl}/setups/banks`, {headers:header, params:params})
+  }
+  getMainCityStatesByCountry(id: number): Observable<StateDto[]>{
+    console.log('Fetching city states');
+    return this.http.get<StateDto[]>(`/${this.baseUrl}/setups/countries/${id}/states`);
+  }
+  getTownsByMainCityState(id: number): Observable<TownDto[]>{
+    return this.http.get<TownDto[]>(`/${this.baseUrl}/setups/states/${id}/towns`);
+  }
+  getBankBranchesByBankId(bankId: number): Observable<BankBranchDTO[]> {
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+
+    return this.http.get<BankBranchDTO[]>(`/${this.baseUrl}/setups/bank-branches/${bankId}/bank-branches`, { headers: header });
+  }
+  saveServiceProvider(serviceProviderData: ServiceProviderDTO): Observable<ServiceProviderDTO[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+
+    return this.http.post<ServiceProviderDTO[]>(`/${this.baseUrl}/accounts/accounts`, JSON.stringify(serviceProviderData), {headers:headers})
+
+  }
+  getCountries(): Observable<CountryDto[]> {
+    return this.http.get<CountryDto[]>(`/${this.baseUrl}/setups/countries`);
+  }
+  getSectors(): Observable<SectorDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', 2);
+    return this.http.get<SectorDto[]>(`/${this.baseUrl}/setups/sectors`,
+      {
+        headers:headers,
+        params:params,
+      });
+  }
+  getCurrencies(): Observable<CurrenciesDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    return this.http.get<CurrenciesDto[]>(`/${this.baseUrl}/setups/currencies`,{headers:headers});
+  }
+  getClientTitles(): Observable<ClientTitlesDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', 2);
+    return this.http.get<ClientTitlesDto[]>(`/${this.baseUrl}/accounts/client-titles`,
+      {
+        headers:headers,
+        params:params,
+      });
+  }
+  getIdentityType(): Observable<IdentityModeDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', 2);
+    return this.http.get<IdentityModeDto[]>(`/${this.baseUrl}/accounts/identity-modes`,
+      {
+        headers:headers,
+        params:params,
+      });
+  }
+  getOccupation(): Observable<OccupationDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    const params = new HttpParams()
+      .set('organizationId', 2);
+    return this.http.get<OccupationDto[]>(`/${this.baseUrl}/setups/occupations`,
+      {
+        headers:headers,
+        params:params,
+      });
+  }
+  getServiceProviderType(): Observable<ProviderTypeDto[]> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    return this.http.get<ProviderTypeDto[]>(`/${this.baseUrl}/accounts/service-provider-types`,{headers:headers});
   }
 }
