5d44d3d8d8e31dcc1785c867f10d7a0633663343
faithwasike <faith.wasike@turnkeyafrica.com>
2023-08-09 08:26:56 +0300

    Added routing to new service provider

--- src/app/features/entities/components/service-provider/new-service-provider/new-service-provider.component.ts
+++ src/app/features/entities/components/service-provider/new-service-provider/new-service-provider.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BreadCrumbItem } from 'src/app/shared/data/common/BreadCrumbItem';
 
 @Component({
   selector: 'app-new-service-provider',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-service-provider.component.css']
 })
 export class NewServiceProviderComponent {
-
+  serviceProviderBreadCrumbItems: BreadCrumbItem[] = [
+    {
+      label: 'Home',
+      url: '/home/dashboard'
+    },
+    {
+      label: 'CRM',
+      url: '/home/dashboard',
+    },
+    {
+      label: 'New Service Provider',
+      url: '/home/entity/service-provider/new'
+    }
+  ];
 }
