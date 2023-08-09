2c226df176ec8c7ee2de28c4d9d41505d8e2f35d
John Dylan <dilanjohn4@gmail.com>
2023-08-09 13:18:26 +0300

    administration feature updates on routing

--- src/app/features/administration/administration.module.ts
+++ src/app/features/administration/administration.module.ts
@@ -3,18 +3,30 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
-import { AdministrationComponent } from './components/administration.component';
+import { AdministrationComponent } from './components/profile/administration.component';
+import { ViewEmployeeComponent } from './components/staff-performance/view-employee/view-employee.component';
+import {TableModule} from "primeng/table";
+import { ViewEmployeeTransactionsComponent } from './components/staff-performance/view-employee-transactions/view-employee-transactions.component';
+import { ViewTicketsComponent } from './components/tickets/view-tickets/view-tickets.component';
+import { RiskDetailsComponent } from './components/tickets/view-tickets/risk-details/risk-details.component';
+import { TaxDetailsComponent } from './components/tickets/view-tickets/tax-details/tax-details.component';
 
 
 @NgModule({
   declarations: [
-    AdministrationComponent
+    AdministrationComponent,
+    ViewEmployeeComponent,
+    ViewEmployeeTransactionsComponent,
+    ViewTicketsComponent,
+    RiskDetailsComponent,
+    TaxDetailsComponent
   ],
-  imports: [
-    CommonModule,
-    AdministrationRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        AdministrationRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TableModule
+    ]
 })
 export class AdministrationModule { }
