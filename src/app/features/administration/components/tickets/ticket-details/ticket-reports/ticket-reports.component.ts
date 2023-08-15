fa7cbc1a8f3186051150e8fbe09914152a6cb4b5
Tunde Ogunjimi <tundereuben@yahoo.com>
2023-08-15 09:17:47 +0100

    VQ-478: created ticket details layout structure

---
+++ src/app/features/administration/components/tickets/ticket-details/ticket-reports/ticket-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-reports',
+  templateUrl: './ticket-reports.component.html',
+  styleUrls: ['./ticket-reports.component.css']
+})
+export class TicketReportsComponent {
+
+}
