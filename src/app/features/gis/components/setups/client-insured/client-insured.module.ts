720840d8ba67ea8de969c66f21cf9294c078c894
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-08-16 21:29:44 +0100

    creating a folder structure for GIS/SETUP module

---
+++ src/app/features/gis/components/setups/client-insured/client-insured.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientInsuredRoutingModule } from './client-insured-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientInsuredRoutingModule
+  ]
+})
+export class ClientInsuredModule { }
--- src/app/features/gis/components/setups/setups-routing.module.ts
+++ src/app/features/gis/components/setups/setups-routing.module.ts
@@ -1,7 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'class-subclass',
+    loadChildren: () => import('./class-subclass/class-subclass.module').then(m => m.ClassSubclassModule),
+  },
+  {
+    path: 'clause',
+    loadChildren: () => import('./clause/clause.module').then(m => m.ClauseModule),
+  },
+  {
+    path: 'client-insured',
+    loadChildren: () => import('./client-insured/client-insured.module').then(m => m.ClientInsuredModule),
+  },
+  {
+    path: 'covertype-setup',
+    loadChildren: () => import('./covertype-setup/covertype-setup.module').then(m => m.CovertypeSetupModule),
+  },
+  {
+    path: 'general-parameter',
+    loadChildren: () => import('./general-parameter/general-parameter.module').then(m => m.GeneralParameterModule),
+  },
+  {
+    path: 'peril-territory',
+    loadChildren: () => import('./peril-territory/peril-territory.module').then(m => m.PerilTerritoryModule),
+  },
+  {
+    path: 'premium-rate',
+    loadChildren: () => import('./premium-rate/premium-rate.module').then(m => m.PremiumRateModule),
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+  },
+  {
+    path: 'schedule',
+    loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule),
+  },
+  {
+    path: 'short-period',
+    loadChildren: () => import('./short-period/short-period.module').then(m => m.ShortPeriodModule),
+  },
+  {
+    path: 'tax',
+    loadChildren: () => import('./tax/tax.module').then(m => m.TaxModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
