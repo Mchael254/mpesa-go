c31ae9041cfd45cb1c986f33cc7b63d43e1de917
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-08-16 21:29:44 +0100

    creating a folder structure for GIS/SETUP module

---
+++ src/app/features/gis/components/setups/premium-rate/premium-rate.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PremiumRateRoutingModule } from './premium-rate-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PremiumRateRoutingModule
+  ]
+})
+export class PremiumRateModule { }
