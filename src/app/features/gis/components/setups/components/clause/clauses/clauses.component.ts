286ed3553f1f609d03cefce2feb853bc3bf94c82
John Dylan <dilanjohn4@gmail.com>
2023-08-21 21:52:38 +0300

    VQ-345 conversion of clauses

---
+++ src/app/features/gis/components/setups/components/clause/clauses/clauses.component.ts
@@ -0,0 +1,301 @@
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Clause, Record1} from "../../../data/gisDTO";
+import {MessageService} from "primeng/api";
+import {DynamicFormFields} from "../../../../../../../shared/utils/dynamic.form.fields";
+import {DynamicFormButtons} from "../../../../../../../shared/utils/dynamic.form.button";
+import {ClauseService} from "../../../../../services/clause/clause.service";
+import {interval, retryWhen, tap} from "rxjs";
+
+enum Types {
+  clause = "CL",
+  warranty = "WT",
+  specialConditions = "SC",
+  excess = "E",
+  peril = "P",
+  exclusion = "EL",
+  extension = "ET",
+  memo = "M"
+}
+
+@Component({
+  selector: 'app-clauses',
+  templateUrl: './clauses.component.html',
+  styleUrls: ['./clauses.component.css']
+})
+export class ClausesComponent {
+  allClauses: any[] = [];
+  selected: any;
+  isupdate: boolean = true;
+  filterby: any;
+  selectedCode: any;
+  clauseForm: FormGroup
+  singleClauseDetails: any;
+  Types = Types;
+  isDisplayed: boolean;
+  dateEdited: any;
+  editedBy: any;
+  show: boolean = false;
+
+  formFields: DynamicFormFields[];
+  public buttonConfig: DynamicFormButtons;
+
+  constructor(
+    public cdr: ChangeDetectorRef,
+    public fb: FormBuilder,
+    private clauseService: ClauseService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllClauses();
+    this.getSingleClause();
+    this.createForm();
+    this.formFields = this.clausesForm();
+    this.buttonConfig = this.actionButtonConfig();
+  }
+
+  record: Record1 = {
+    id: 1,
+    name: 'Example Record',
+    dateEdited: new Date(),
+    editedBy: 'John Doe'
+  };
+
+
+  createForm() {
+
+    const randomString = Math.random().toString(36).substring(2, 5);
+    /*this.clauseForm = this.fb.group({
+      code: new FormControl(''),
+      isCurrent: new FormControl(''),
+      heading: new FormControl(''),
+      type: new FormControl(''),
+      isEditable: new FormControl(''),
+      wording: new FormControl(''),
+      short_description: "randomString",
+      is_lien: "string",
+      ins: "string",
+      merge: "string",
+      organization_code: 2,
+      version: 1,
+      updated_at: "2022-04-12",
+      updated_by: "TQUEST",
+    })*/
+
+  }
+
+
+  //get all the clauses
+  getAllClauses() {
+    this.clauseService.getClauses().pipe(retryWhen((_) => interval(1000)),
+      tap(() => (this.isDisplayed = true)),).subscribe(data => {
+      this.allClauses = data._embedded.clause_dto_list.map(data => {
+        let temp = data;
+        temp['name'] = data.heading;
+        return temp;
+      });
+      console.log("all clauses", this.allClauses)
+      this.isDisplayed = true;
+      this.cdr.detectChanges();
+    })
+  }
+  selectedClause() {
+    //console.log("this is the selected clause", this.selected.code)
+    this.clauseService.setClauseCode(this.selected.code)
+    const dateTimeStr = this.selected.updated_at;
+    const dateStr = dateTimeStr.substr(0, 10);
+    this.dateEdited = dateStr
+    this.editedBy = this.selected.updated_by
+  }
+  //get a single clause details
+  getSingleClause() {
+    this.clauseService.getClauseCode().subscribe(id => {
+      this.selectedCode = id;
+      this.clauseService.getSingleClause(this.selectedCode).subscribe(data => {
+        this.singleClauseDetails = data
+        console.log("this is the selected clause", this.singleClauseDetails)
+        // this.clauseForm.patchValue(this.singleClauseDetails)
+        this.cdr.detectChanges();
+      })
+    })
+  }
+  createNewClause() {
+    // this.clauseForm.reset();
+    this.isupdate = false;
+  }
+  save(data:any) {
+    if (this.isupdate) {
+      this.updateClause()
+    }
+    else {
+      this.createClause()
+      // this.clauseForm.reset();
+    }
+    console.log(data)
+  }
+  createClause() {
+    const randomString = Math.random().toString(36).substring(2, 5);
+    const requestBody: Clause = this.clauseForm.value;
+    requestBody.version = 2;
+    requestBody.short_description = randomString
+    requestBody.is_lien = "Y"
+    requestBody.ins = "Y"
+    requestBody.merge = "Y"
+    requestBody.organization_code = 2
+    try {
+      this.clauseService.createClause(requestBody).subscribe(data => {
+
+        this.showSuccess
+        console.log("Created Clause", data)
+        this.cdr.detectChanges();
+      })
+
+    } catch (error) {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error, try again later' });
+
+    }
+
+  }
+  /* update clauses */
+
+  updateClause() {
+    const randomString = Math.random().toString(36).substring(2, 5);
+    const requestBody: Clause = this.clauseForm.value;
+    requestBody.code = null;
+    requestBody.organization_code = 2;
+    requestBody.version = 3
+    requestBody.short_description = randomString
+    try {
+      this.clauseService.updateClause(requestBody, this.selectedCode).subscribe(data => {
+        console.log("Updated successfully here")
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully updated' });
+      })
+    } catch (error) {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error, try again later' });
+    }
+
+  }
+  /**REVISE CLAUSE */
+  reviseClause(){
+    const requestBody: Clause = this.clauseForm.value
+    requestBody.version = 2
+    this.clauseService.reviseClause(requestBody,requestBody.code).subscribe(res =>{
+      this.reviseSuccess()
+    })
+  }
+  /**DELETE CLAUSE */
+  deleteClause(){
+    const requestBody: Clause = this.clauseForm.value
+    this.clauseService.deleteClause(requestBody.code).subscribe(res =>{
+      this.deleteSuccess()
+    })
+  }
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully Created' });
+  }
+  reviseSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully Revised' });
+  }
+  deleteSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully Deleted' });
+  }
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error Occured' });
+  }
+
+  clausesForm(): DynamicFormFields[]{
+    return [
+      {
+        name: 'id',
+        label: 'ID',
+        type: 'text',
+        required: false,
+        disabled:true,
+        placeholder: '',
+        value: ""
+
+      },
+      {
+        name: 'current',
+        label: 'Current',
+        type: 'dropdown',
+        required: false,
+        disabled:false,
+        placeholder: '',
+        options: [
+          {value: 'Y', label: 'Yes' },
+          {value: 'N', label: 'No' },
+        ],
+      },
+      {
+        name: 'heading',
+        label: 'Heading',
+        type: 'text',
+        required: false,
+        disabled:false,
+        placeholder: ''
+      },
+      {
+        name: 'type',
+        label: 'Type',
+        type: 'dropdown',
+        required: false,
+        disabled:false,
+        placeholder: '',
+        options: [
+          {value: 'CL', label: 'Clause' },
+          {value: 'WA', label: 'Warranty' },
+          {value: 'SP', label: 'Special Conditions' },
+          {value: 'EX', label: 'Excess' },
+          {value: 'PE', label: 'Peril' },
+          {value: 'EC', label: 'Exclusion' },
+          {value: 'ET', label: 'Extension' },
+          {value: 'MM', label: 'Memo' },
+        ],
+      },
+      {
+        name: 'clauseEditable',
+        label: 'Clause Editable?',
+        type: 'dropdown',
+        required: false,
+        disabled:false,
+        placeholder: '',
+        options: [
+          {value: 'Y', label: 'Yes' },
+          {value: 'N', label: 'No' },
+        ],
+      },
+      {
+        name: 'wording',
+        label: 'Wording',
+        type: 'textarea',
+        required: false,
+        disabled:false,
+        placeholder: ''
+      },
+
+    ];
+  }
+
+  actionButtonConfig() : DynamicFormButtons{
+    return {
+      submit: { label: 'Save', visible: true, alignment: 'end' },
+      back: { label: 'Revise', visible: true, alignment: 'start' },
+      center: { label: 'Center', visible: false, alignment: 'center' },
+
+    };
+  }
+
+  receiveData(data) {
+
+    this.show = false;
+    let temp = this.formFields
+    temp[0].value = data.code;
+    temp[2].value = data.heading;
+    temp[5].value = data.wording;
+    this.formFields = temp;
+    this.cdr.detectChanges();
+    this.show = true;
+  }
+}
--- src/app/features/gis/gis.module.ts
+++ src/app/features/gis/gis.module.ts
@@ -1,21 +1,24 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GisRoutingModule } from './gis-routing.module';
 import { ListPoliciesComponent } from './components/policy/components/list-policies/list-policies.component';
 import { TaxRatesComponent } from './components/setups/components/tax/tax-rates/tax-rates.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { SharedModule } from 'primeng/api';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { StepsModule } from 'primeng/steps';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
+import { ClausesComponent } from './components/setups/components/clause/clauses/clauses.component';
+import {ListboxModule} from "primeng/listbox";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    TaxRatesComponent
+    TaxRatesComponent,
+    ClausesComponent
   ],
   imports: [
     CommonModule,
@@ -27,7 +30,9 @@ import { TabViewModule } from 'primeng/tabview';
     DropdownModule,
     StepsModule,
     TableModule,
-    FormsModule
+    FormsModule,
+    ListboxModule,
+    SharedModule,
 
   ],
   schemas: [
