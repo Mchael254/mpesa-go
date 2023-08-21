ccd05e42e66a0cdabf47e069871ab303e77370e3
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-08-21 12:04:41 +0100

    VQ-640 | Internationalization Implementation for V6 Application.

--- src/app/features/base/components/header/header.component.ts
+++ src/app/features/base/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +9,23 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 
 })
+@AutoUnsubscribe
 export class HeaderComponent {
+  defaultLanguage: string = 'fi fi-ng fis';
+  constructor(private translate:TranslateService){}
+
+  selectLanguage(value){
+    this.translate.use(value.code)
+    this.defaultLanguage = value.class
+
+  }
+
+  languages = [
+    { code: 'ng', class: 'fi fi-ng fis'},
+    { code: 'ke', class: 'fi fi-ke fis'},
+    { code: 'en', class: 'fi fi-gb fis'},
+    { code: 'fr', class: 'fi fi-fr fis'},
+    { code: 'cn', class: 'fi fi-cn fis'}
+  ];
 
 }
