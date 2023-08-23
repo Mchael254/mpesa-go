291042e475dc961a12868578b8b1e759466259b3
faithwasike <faith.wasike@turnkeyafrica.com>
2023-08-23 10:34:52 +0300

    Class Setup Wizard Form Content

--- src/app/features/gis/components/setups/components/class-subclass/class-subclass.module.ts
+++ src/app/features/gis/components/setups/components/class-subclass/class-subclass.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ClassSubclassRoutingModule } from './class-subclass-routing.module';
 import { DynamicSetupWizardWelcomeScreenComponent } from 'src/app/shared/components/dynamic-setup-wizard-welcome-screen/dynamic-setup-wizard-welcome-screen.component';
 import { ClassSetupWizardComponent } from './class-setup-wizard/class-setup-wizard.component';
-
+import { ClassesComponent } from './classes/classes.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+// import { DynamicFormComponent } from 'src/app/shared/components/dynamic-form/dynamic-form.component';
 @NgModule({
   declarations: [
-    DynamicSetupWizardWelcomeScreenComponent,
-    ClassSetupWizardComponent
+    // DynamicSetupWizardWelcomeScreenComponent,
+    ClassSetupWizardComponent,
+    ClassesComponent,
+    // DynamicFormComponent
   ],
   imports: [
     CommonModule,
     ClassSubclassRoutingModule,
-    
+    SharedModule
   ]
 })
 export class ClassSubclassModule { }
