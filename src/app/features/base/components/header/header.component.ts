0bc74308d02093afc7ff1ad0b010ae9827d8cd39
kimiti-R <raphael.kimiti@turnkeyafrica.com>
2023-09-01 11:59:18 +0300

    VQ-175:: Profile fixes

--- src/app/features/base/components/header/header.component.ts
+++ src/app/features/base/components/header/header.component.ts
@@ -1,7 +1,12 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
-import {AuthService} from "../../../../shared/services/auth.service";
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { LocalStorageService } from 'src/app/shared/services/local-storage/local-storage.service';
+import { Logger } from 'src/app/shared/services/logger.service';
+
+const log = new Logger("HeaderComponent");
 
 @Component({
   selector: 'app-header',
@@ -11,17 +16,58 @@ import {AuthService} from "../../../../shared/services/auth.service";
 
 })
 @AutoUnsubscribe
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   defaultLanguage: string = 'fi fi-gb fis';
+  user: any;
+
   constructor(
-    private translate:TranslateService,
+    private translate: TranslateService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private localStorageService: LocalStorageService,
     private authService: AuthService
-  ){}
+  ) { }
+  
+  ngOnInit(): void {
+    this.user = this.localStorageService.getItem("loginUserProfile")
+    this.getUserImage();
+  }
+
+    getUserImage() {
+    if (this.user.image) {
+      return this.user.image;
+    } else {
+      const initials = this.user.name.split(' ').map((name: string) => name.charAt(0)).join('');
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      canvas.width = 40;
+      canvas.height = 40;
+      if (context) {
+        const centerX = canvas.width / 2;
+        const centerY = canvas.height / 2;
+        const radius = Math.min(centerX, centerY);
+
+        context.fillStyle = '#00529b';
+        context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+        context.fill();
+
+        context.font = '16px Roboto';
+        context.fillStyle = '#000000';
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillText(initials, centerX, centerY);
+
+        context.strokeStyle = '#FFFFFF'; // Set the border color to white
+        context.lineWidth = 2; // Set the border width
+        context.stroke();
+      }
+      return canvas.toDataURL();
+    }
+  }
 
   selectLanguage(value){
     this.translate.use(value.code)
     this.defaultLanguage = value.class
-
   }
 
   languages = [
@@ -33,7 +79,14 @@ export class HeaderComponent {
     { code: 'es', class: 'fi fi-es fis', lang:"Espanyol"}
   ];
 
-  logout(): void {
-    this.authService.purgeAuth();
+  navigateAccount() {
+    this.router.navigate(['/home/administration'], { relativeTo: this.route, queryParams: { showTabs: 'false' } })
   }
+
+
+  logout() {
+    log.info('Logging out..');
+    this.authService.purgeAuth(true);
+  }
+
 }
