a28e0caf0614a7de800c6e1daf20af40eb5f632e
Idah Gakii <idah.gakii@turnkeyafrica.com>
2023-09-11 10:07:25 +0300

    VQ-313 Url Safe Sanitizer Pipe

---
+++ src/app/shared/pipes/safe-resource-url/safe-resource-url.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+
+/**
+ * Pipe to bypass security trust of a resource url
+ * @example
+ *   <iframe [src]="url | safeUrl"></iframe>
+ *   <img [src]="url | safeUrl">
+ *
+ *  <object [data]="url | safeUrl"></object>
+ *
+ *
+ */
+@Pipe({
+  name: 'safeUrl'
+})
+export class SafeResourceUrlPipe implements PipeTransform {
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  /**
+   * Bypass security trust of a resource url
+   * @param url {string} url to be sanitized
+   * @returns {SafeResourceUrl | string} sanitized url
+   */
+  transform(url: string): SafeResourceUrl | string {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+}
