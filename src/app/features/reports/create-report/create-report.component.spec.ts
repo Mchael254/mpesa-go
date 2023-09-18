e1010296bb58f91be36cdd41816b88d419abc1f6
Tunde Ogunjimi <tundeogunjimi@gmail.com>
2023-09-18 12:51:43 +0100

    VQ-340/471: calendar unit tests

--- src/app/features/reports/create-report/create-report.component.spec.ts
+++ src/app/features/reports/create-report/create-report.component.spec.ts
@@ -13,7 +13,7 @@ import {RouterTestingModule} from "@angular/router/testing";
 import {AppConfigService} from "../../../core/config/app-config-service";
 import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 import {ReactiveFormsModule} from "@angular/forms";
-import {ActivatedRoute, convertToParamMap, Params} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {createSpyObj} from "jest-createspyobj";
 import {SubjectArea} from "../../../shared/data/reports/subject-area";
 import {ReportService} from "../services/report.service";
