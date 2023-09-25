7d29413b8f285c5d0d2197c0b2a37a95280d71d6
HopeIbrahim <hope.ibrahim@turnkey.com>
2023-09-25 11:11:03 +0300

    VQ:733- Implementation of Dynamic Fields on Quick Qoute screen.

--- src/app/features/gis/services/quotations/quotations.service.ts
+++ src/app/features/gis/services/quotations/quotations.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams} from "@angular/common/http";
 import {AppConfigService} from '../../../../core/config/app-config-service'
-import {Observable} from "rxjs";
+import {Observable, catchError, retry, throwError} from "rxjs";
 import {Pagination} from "../../../../shared/data/common/pagination";
 import { QuotationsDTO } from '../../data/quotations-dto';
 
@@ -11,11 +11,26 @@ import { QuotationsDTO } from '../../data/quotations-dto';
 export class QuotationsService {
 
   baseUrl = this.appConfig.config.contextPath.gis_services;
+  setupsbaseurl = "setups/api/v1"
+
   constructor(
     private http: HttpClient,
     private appConfig: AppConfigService
   ) { }
 
+ // Error handling
+ errorHandl(error: HttpErrorResponse) {
+  let errorMessage = '';
+  if(error.error instanceof ErrorEvent) {
+    // Get client-side error
+    errorMessage = error.error.message;
+  } else {
+    // Get server-side error
+    errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  }
+  console.log(errorMessage);
+  return throwError(errorMessage);
+  }
   getQuotations(
     pageNo: number = 0,
     dateFrom: string,
@@ -37,5 +52,40 @@ export class QuotationsService {
         headers: headers,
         params: params,
       });
+  }
+  getAllQuotationSources(): Observable<any>{
+    let page = 0;
+    let size = 10;
+   const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    
+    })
+    const params = new HttpParams()
+    .set('pageNo', `${page}`)
+      .set('pageSize', `${size}`)
+    return this.http.get<any>(`/${this.baseUrl}/quotation/api/v2/quotation-sources`,{
+      headers:headers,
+      params:params
+    }).pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    ) 
+  }
+  getFormFields(shortDescription:any): Observable<any>{
+   
+   const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    
+    })
+    const params = new HttpParams()
+    return this.http.get<any>(`/${this.baseUrl}/${this.setupsbaseurl}/forms/${shortDescription}`,{
+      headers:headers,
+      params:params
+    }).pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    ) 
   }
 }
