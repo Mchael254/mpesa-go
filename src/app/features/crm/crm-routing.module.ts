0a4342edd96bdbcae64a234c168b2d7965eb0914
kimiti-R <raphael.kimiti@turnkeyafrica.com>
2023-10-17 11:25:20 +0300

    VQ-857: CRM Setups Landing Page

--- src/app/features/crm/crm-routing.module.ts
+++ src/app/features/crm/crm-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { BaseCrmComponent } from './components/base-crm/base-crm.component';
 import { CountryComponent } from './components/country/country.component';
 import { OrganizationComponent } from './components/organization/organization.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: BaseCrmComponent
+  },
   {
     path: 'organization',
     component: OrganizationComponent
