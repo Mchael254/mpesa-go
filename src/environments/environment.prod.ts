ddec6cc7ddae8bdafaf8694a8ea37d73653a28e9
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-10-30 22:06:29 +0100

    VQ-899 | Need Analysis screen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { API_CONFIG } from './api_service_config';
+import { API_CONFIG, API_TENANT_ID } from './api_service_config';
 
 export const environment = {
 
@@ -12,7 +12,9 @@ export const environment = {
 
   API_URLS: new Map<API_CONFIG, string>([
     [API_CONFIG.SETUPS_SERVICE_BASE_URL, '/lms/setups'],
-    [API_CONFIG.MARKETING_SERVICE_BASE_URL, '/lms/marketing'],
+    [API_CONFIG.GRP_QUOTATIONS_SERVICE_BASE_URL, '/lms/marketing'],
 
   ]),
+  TENANT_ID: API_TENANT_ID.MUTUAL,
+
 };
