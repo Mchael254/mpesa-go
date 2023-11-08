32f486bcda7e12e255acff8d9df4220d763ad6ed
kimiti-R <raphael.kimiti@turnkeyafrica.com>
2023-11-08 13:32:17 +0300

    VQ-658: Organization-Crm Journey Modification

--- src/app/features/crm/components/branch/branch.component.ts
+++ src/app/features/crm/components/branch/branch.component.ts
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { OrganizationService } from '../../services/organization.service';
 import { GlobalMessagingService } from '../../../../shared/services/messaging/global-messaging.service';
 import { untilDestroyed } from '../../../../shared/services/until-destroyed';
-import { OrganizationDTO, OrganizationRegionDTO } from '../../data/organization-dto';
+import { BranchContactDTO, BranchDivisionDTO, OrganizationBranchDTO, OrganizationDTO, OrganizationRegionDTO } from '../../data/organization-dto';
 import { Logger } from '../../../../shared/services/logger/logger.service';
 
 const log = new Logger( 'BranchComponent');
@@ -22,12 +22,14 @@ export class BranchComponent implements OnInit {
 
   public steps = stepData;
 
-  public organizationsData: OrganizationDTO[];
-  public regionData: OrganizationRegionDTO[];
-  public branchData: any;
-  public branchContacts: any;
-  public branchDivision: any;
+  public organizationsData: OrganizationDTO[] = [];
+  public regionData: OrganizationRegionDTO[] = [];
+  public BranchesData: OrganizationBranchDTO[] = [];
+  public BranchDivisionData: BranchDivisionDTO[] = [];
+  public branchContacts: BranchContactDTO[] = [];
   public selectedOrg: OrganizationDTO;
+  public selectedReg: OrganizationRegionDTO;
+  public selectedBranch: OrganizationBranchDTO;
 
   organizationBreadCrumbItems: BreadCrumbItem[] = [
     {
@@ -83,6 +85,19 @@ export class BranchComponent implements OnInit {
     this.fetchOrganizationRegion(this.selectedOrg.id);
   }
 
+  onRegionChange($event: Event) {
+    const selectedRegionId = (event.target as HTMLSelectElement).value;
+    const selectedRegIdAsNumber = parseInt(selectedRegionId, 10);
+    this.selectedReg = this.regionData.find(organization => organization.code === selectedRegIdAsNumber);
+    this.fetchOrganizationBranch(this.selectedReg.code);
+  }
+
+  onBranchRowSelect(branch: OrganizationBranchDTO) {
+    this.selectedBranch = branch;
+    this.fetchOrganizationBranchDivision(this.selectedBranch.id);
+    this.fetchOrganizationBranchContact(this.selectedBranch.id);
+  }
+
   fetchOrganizationRegion(organizationId: number) {
     this.organizationService.getOrganizationRegion(organizationId)
       .pipe(untilDestroyed(this))
@@ -90,6 +105,34 @@ export class BranchComponent implements OnInit {
         this.regionData = data;
         log.info('Region Data', this.regionData);
       })
+  }
+
+  fetchOrganizationBranch(regionId: number) {
+    this.organizationService.getOrganizationBranch(this.selectedOrg.id, regionId)
+      .pipe(untilDestroyed(this))
+      .subscribe((data) => {
+        this.BranchesData = data;
+        log.info('Branch Data', this.BranchesData);
+      })
+  }
+
+  fetchOrganizationBranchDivision(branchId: number) {
+    this.organizationService.getOrganizationBranchDivision(branchId)
+      .pipe(untilDestroyed(this))
+      .subscribe((data) => {
+        this.BranchDivisionData = data;
+        log.info('Branch Division Data', this.BranchDivisionData);
+      })
+  }
+
+  fetchOrganizationBranchContact(branchId: number) {
+    this.organizationService.getOrganizationBranchContact(branchId)
+      .pipe(untilDestroyed(this))
+      .subscribe((data) => {
+        this.branchContacts = data;
+        log.info('Branch Contact Data', this.branchContacts);
+      })
+
   }
 
   onNext() {}
