c9dfec803f9928c7420bdd5a01e789f17a8abd97
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-12-04 23:46:25 +0100

    VQ-1098 | Medical Underwriting Screen Development

---
+++ src/app/features/setups/components/forms/components/lms-group/lms-group.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lms-group',
+  templateUrl: './lms-group.component.html',
+  styleUrls: ['./lms-group.component.css']
+})
+export class LmsGroupComponent {
+
+}
---
+++ src/app/features/setups/setups.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SetupsRoutingModule } from './setups-routing.module';
+import { BaseComponent } from './base/base.component';
+import { SystemComponent } from './components/system/system.component';
+
+
+@NgModule({
+  declarations: [
+    BaseComponent,
+    SystemComponent
+  ],
+  imports: [
+    CommonModule,
+    SetupsRoutingModule
+  ]
+})
+export class SetupsModule { }
