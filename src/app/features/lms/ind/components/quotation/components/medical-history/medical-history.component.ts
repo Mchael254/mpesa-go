d252d06bcc9d7c9ffeb938b4c0078dc6337f3a81
Emmanuel Asunomeh <asunomeh.emmanuel@turnkeyafrica.com>
2023-12-11 01:02:35 +0100

    Correction on Setup Screen

--- src/app/features/lms/ind/components/quotation/components/medical-history/medical-history.component.ts
+++ src/app/features/lms/ind/components/quotation/components/medical-history/medical-history.component.ts
@@ -1,62 +1,78 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import stepData from '../../data/steps.json';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BreadCrumbItem } from 'src/app/shared/data/common/BreadCrumbItem';
 import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
-import { LifestyleService } from 'src/app/features/lms/service/lifestyle/lifestyle.service';
 import { SESSION_KEY } from 'src/app/features/lms/util/session_storage_enum';
 import { SessionStorageService } from 'src/app/shared/services/session-storage/session-storage.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { finalize } from 'rxjs';
-
+import { MedicalHistoryService } from 'src/app/features/lms/service/medical-history/medical-history.service';
+import { Router } from '@angular/router';
+import { environment } from '../../../../../../../../environments/environment';
 
 @Component({
   selector: 'app-medical-history',
   templateUrl: './medical-history.component.html',
-  styleUrls: ['./medical-history.component.css']
+  styleUrls: ['./medical-history.component.css'],
 })
 @AutoUnsubscribe
 export class MedicalHistoryComponent implements OnDestroy, OnInit {
-
-  steps = stepData
-  personalDetailFormfields: any[];
+  steps = stepData;
   buttonConfig: any;
-  searchForm: FormGroup;
-  escalationForm: FormGroup;
   breadCrumbItems: BreadCrumbItem[] = [
     {
       label: 'Home',
-      url: '/home/dashboard'
+      url: '/home/dashboard',
     },
     {
       label: 'Quotation',
-      url: '/home/lms/quotation/list'
+      url: '/home/lms/quotation/list',
     },
     {
       label: 'Medical History(Data Entry)',
-      url: '/home/lms/ind/quotation/medical-history'
+      url: '/home/lms/ind/quotation/medical-history',
     },
   ];
   medicalListOne: any[] = [];
   medicalListTwo: any[] = [];
   editEntity: boolean = false;
 
-
   medicalHistoryForm: FormGroup;
   clonedProducts: any;
   products: any;
-  constructor(private fb: FormBuilder,  
-    private lifestyle_service: LifestyleService, 
+  constructor(
+    private fb: FormBuilder,
+    private medical_history_service: MedicalHistoryService,
     private session_service: SessionStorageService,
     private spinner_service: NgxSpinnerService,
-    ){
+    private router: Router
+  ) {
     this.medicalHistoryForm = this.fb.group({
-      question1: ['N'],
-      question2: ['N'],
-      question3: ['N'],
-      question4: ['N'],
-    });
+      question1: [],
+      question2: [],
+      question3: [],
+      question4: [],
 
+      // code: ['', Validators.required],
+      // tenant_id: ['', Validators.required],
+      // client_code: ['', Validators.required],
+      medical_personnel: this.fb.group({
+        name: ['', Validators.required],
+        address: ['', Validators.required],
+        phone_no: ['', Validators.required],
+        email_address: ['', [Validators.required, Validators.email]],
+      }),
+      pregnancy_due_date: ['', Validators.required],
+      medication_intake: this.fb.array([]),
+      physical_challenge: [false, Validators.required],
+      health_history: this.fb.group({
+        individual_name: ['', Validators.required],
+        disease: ['', Validators.required],
+        diagnosed_date: ['', Validators.required],
+        description: [''],
+      }),
+    });
   }
   ngOnInit(): void {
     this.getMedicalHistoryByClientId();
@@ -66,71 +82,93 @@ export class MedicalHistoryComponent implements OnDestroy, OnInit {
     return this.medicalHistoryForm.get(name).value;
   }
 
-  getMedicalHistoryByClientId(){
-    this.spinner_service.show('medical_history_screen')
-    let client_code = this.session_service.get(SESSION_KEY.CLIENT_CODE);
-    this.lifestyle_service.getClientMedicalHistoryById(client_code)
-    .pipe(finalize(() => this.spinner_service.hide('medical_history_screen')
-    ))
-    .subscribe(data => {
-      console.log(data);
-      this.spinner_service.hide('medical_history_screen')
+  getMedicalHistoryByClientId() {
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>');
 
-
-    },
-    err => {
-      this.spinner_service.hide('medical_history_screen')
-    })
+    this.spinner_service.show('medical_history_screen');
+    let client_code = '123456'; // this.session_service.get(SESSION_KEY.CLIENT_CODE);
+    let tenant_id = environment.TENANT_ID;
+    this.medical_history_service
+      .getMedicalHistoryByTenantIdAndClientCode(tenant_id, client_code)
+      .pipe(finalize(() => this.spinner_service.hide('medical_history_screen')))
+      .subscribe(
+        (data) => {
+          console.log(data['data']);
+          let data_ = data['data'];
+          if(data_?.medical_personnel!=null){
+            data_['question1'] = 'Y'
+          }
+          if(data_?.pregnancy_due_date!=null){
+            data_['question2'] = 'Y'
+            data_['pregnancy_due_date'] = new Date(data_?.pregnancy_due_date)
+          }
+          if(data_?.physical_challenge){
+            data_['physical_challenge'] = 'Y'
+          }else{
+            data_['physical_challenge'] = 'N'
+          }
+          this.medicalHistoryForm.patchValue(data['data'])
+          this.spinner_service.hide('medical_history_screen');
+        },
+        (err) => {
+          this.spinner_service.hide('medical_history_screen');
+        }
+      );
   }
 
   onRowEditInit(product: any) {
     this.clonedProducts[product.id as string] = { ...product };
-}
+  }
 
-onRowEditSave(product: any) {
+  onRowEditSave(product: any) {
     if (product.price > 0) {
-        delete this.clonedProducts[product.id as string];
-        // this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Product is updated' });
+      delete this.clonedProducts[product.id as string];
+      // this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Product is updated' });
     } else {
-        // this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' });
+      // this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' });
     }
-}
+  }
 
-onRowEditCancel(product: any, index: number) {
+  onRowEditCancel(product: any, index: number) {
     this.products[index] = this.clonedProducts[product.id as string];
     delete this.clonedProducts[product.id as string];
-}
+  }
 
-editBeneficiary(i){}
-deleteMedicalListOne( i: number) {
-  this.medicalListOne = this.deleteEntity(this.medicalListOne, i);
-};
-deleteMedicalListTwo( i: number) {
-  this.medicalListTwo = this.deleteEntity(this.medicalListTwo, i);
-};
-updateBeneficiary(i){}
-addEmptyMedicalList(medicalList: any[]) {
-  this.addEntity(medicalList);
-}
+  editBeneficiary(i) {}
+  deleteMedicalListOne(i: number) {
+    this.medicalListOne = this.deleteEntity(this.medicalListOne, i);
+  }
+  deleteMedicalListTwo(i: number) {
+    this.medicalListTwo = this.deleteEntity(this.medicalListTwo, i);
+  }
+  updateBeneficiary(i) {}
+  addEmptyMedicalList(medicalList: any[]) {
+    this.addEntity(medicalList);
+  }
 
-private addEntity(d: any[]) {
-  this.editEntity = true;
-  d.push({ isEdit: true });
-  this.editEntity = false;
-  return d;
-};
-private deleteEntity(d: any[], i) {
-  this.editEntity = true;
-     d = d.filter((data, x) => {
+  private addEntity(d: any[]) {
+    this.editEntity = true;
+    d.push({ isEdit: true });
+    this.editEntity = false;
+    return d;
+  }
+  private deleteEntity(d: any[], i) {
+    this.editEntity = true;
+    d = d.filter((data, x) => {
       return i !== x;
     });
-    this.editEntity = false
+    this.editEntity = false;
     return d;
-};
+  }
 
-ngOnDestroy(): void {
-  console.log('MedicalHistoryComponent UNSUBSCRIBE');
+  nextPage() {
+    this.medical_history_service.saveMedicalHistory({}).subscribe((data) => {
+      console.log(data);
+      this.router.navigate(['/home/lms/ind/quotation/summary']);
+    });
+  }
 
-}
-
+  ngOnDestroy(): void {
+    console.log('MedicalHistoryComponent UNSUBSCRIBE');
+  }
 }
