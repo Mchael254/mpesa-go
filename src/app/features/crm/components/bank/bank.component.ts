920b7e903d682fd72add7ae62663e812fc27ae7a
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-01-24 10:04:24 +0300

    ability to view, create, update, delete bank charges

--- src/app/features/crm/components/bank/bank.component.ts
+++ src/app/features/crm/components/bank/bank.component.ts
@@ -3,7 +3,7 @@ import {BreadCrumbItem} from "../../../../shared/data/common/BreadCrumbItem";
 import {BankService} from "../../../../shared/services/setups/bank/bank.service";
 import {untilDestroyed} from "../../../../shared/services/until-destroyed";
 import {Logger} from "../../../../shared/services";
-import {BankBranchDTO, BankDTO, POSTBankBranchDTO} from "../../../../shared/data/common/bank-dto";
+import {BankBranchDTO, BankChargeDTO, BankDTO, POSTBankBranchDTO} from "../../../../shared/data/common/bank-dto";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CountryService} from "../../../../shared/services/setups/country/country.service";
 import {CountryDto, TownDto} from "../../../../shared/data/common/countryDto";
@@ -12,6 +12,18 @@ import {MandatoryFieldsService} from "../../../../shared/services/mandatory-fiel
 import {ReusableInputComponent} from "../../../../shared/components/reusable-input/reusable-input.component";
 import {OrganizationService} from "../../services/organization.service";
 import {OrganizationBranchDTO} from "../../data/organization-dto";
+import {SystemsDto} from "../../../../shared/data/common/systemsDto";
+import {SystemsService} from "../../../../shared/services/setups/systems/systems.service";
+import {ProductService} from "../../../gis/services/product/product.service";
+import {
+  BankChargeRateTypesService
+} from "../../../../shared/services/system-definitions/bank-charge-rate-types.service";
+import {BankChargeRateTypesDto} from "../../../../shared/data/common/bank-charge-rate-types-dto";
+import {ProductService as LmsProductService} from "../../../lms/service/product/product.service";
+import {take} from "rxjs";
+import {map} from "rxjs/internal/operators/map";
+import {Table} from "primeng/table";
+
 
 const log = new Logger('BankComponent');
 @Component({
@@ -35,6 +47,18 @@ export class BankComponent implements OnInit{
   selectedCountry: number;
   selectedBankBranch:BankBranchDTO;
   selectedBankBranchCountry: number;
+
+  createBankChargeForm: FormGroup;
+  bankChargeData: BankChargeDTO[];
+  selectedBankCharge: BankChargeDTO;
+
+  systemsData: SystemsDto[];
+  selectedSystem: SystemsDto;
+
+  productList: AllProduct[] = [];
+  lmsProducts: any[] = [];
+
+  bankChargeRateTypesData: BankChargeRateTypesDto[];
   bankBreadCrumbItems: BreadCrumbItem[] = [
     {
       label: 'Administration',
@@ -76,16 +100,29 @@ export class BankComponent implements OnInit{
     contactPersonPhone: 'Y',
     contactPersonEmail: 'Y',
     physicalAddress: 'Y',
-    postalAddress: 'Y'
+    postalAddress: 'Y',
+  //
+    rate: 'Y',
+    dateFrom: 'Y',
+    dateTo: 'Y',
+    rateType: 'Y',
+    system: 'Y',
+    product: 'Y',
   }
   groupId: string = 'bankTab';
   groupIdBankBranch: string = 'bankBranchTab';
+  groupIdBankCharge: string = 'bankChargeTab';
+
+  @ViewChild('bankTable') bankTable: Table;
 
   @ViewChild('bankConfirmationModal')
   bankConfirmationModal!: ReusableInputComponent;
 
   @ViewChild('bankBranchConfirmationModal')
   bankBranchConfirmationModal!: ReusableInputComponent;
+
+  @ViewChild('bankChargeConfirmationModal')
+  bankChargeConfirmationModal!: ReusableInputComponent;
 
   constructor(
     private fb: FormBuilder,
@@ -94,6 +131,10 @@ export class BankComponent implements OnInit{
     private mandatoryFieldsService: MandatoryFieldsService,
     private globalMessagingService: GlobalMessagingService,
     private organizationService: OrganizationService,
+    private systemsService: SystemsService,
+    private gisProductService: ProductService,
+    private lmsProductService: LmsProductService,
+    private bankChargeRateTypesService: BankChargeRateTypesService,
     private cdr: ChangeDetectorRef
   ) {}
 
@@ -103,6 +144,9 @@ export class BankComponent implements OnInit{
     this.bankCreateForm();
     this.fetchOrganizationBranchNames();
     this.bankBranchCreateForm();
+    this.bankChargeCreateForm();
+    this.fetchAllSystems();
+    this.fetchBankChargeRateTypes();
   }
 
   ngOnDestroy(): void {}
@@ -116,6 +160,7 @@ export class BankComponent implements OnInit{
     this.selectedBank = bank;
     log.info('bank select', this.selectedBank)
     this.fetchBankBranches(this.selectedBank.id);
+    this.fetchBankCharges(this.selectedBank.id);
   }
 
   /**
@@ -124,6 +169,15 @@ export class BankComponent implements OnInit{
   onBankBranchRowSelect(bankBranch: BankBranchDTO) {
     this.selectedBankBranch = bankBranch;
     log.info('bank branch select', this.selectedBankBranch)
+  }
+
+  /**
+   * The function "onBankChargeRowSelect" assigns the selected bank charge to a variable and logs the selected bank charge
+   * information.
+   */
+  onBankChargeRowSelect(bankCharge: BankChargeDTO) {
+    this.selectedBankCharge = bankCharge;
+    log.info('bank charge select', this.selectedBankCharge)
   }
 
   /**
@@ -220,6 +274,45 @@ export class BankComponent implements OnInit{
   }
 
   /**
+   * The function `bankChargeCreateForm()` creates a form with various fields and adds validators and asterisks to the
+   * required fields based on the response from an API call.
+   */
+  bankChargeCreateForm() {
+    this.createBankChargeForm = this.fb.group({
+      rate: [''],
+      dateFrom: [''],
+      dateTo: [''],
+      rateType: [''],
+      system: [''],
+      product: [''],
+    });
+    this.mandatoryFieldsService.getMandatoryFieldsByGroupId(this.groupIdBankCharge).pipe(
+      untilDestroyed(this)
+    )
+      .subscribe((response) =>{
+        response.forEach((field) =>{
+          for (const key of Object.keys(this.createBankChargeForm.controls)) {
+            this.visibleStatus[field.frontedId] = field.visibleStatus;
+            if (field.visibleStatus === 'Y') {
+              if (key === field.frontedId && field.mandatoryStatus === 'Y'){
+                this.createBankChargeForm.controls[key].addValidators(Validators.required);
+                this.createBankChargeForm.controls[key].updateValueAndValidity();
+                const label = document.querySelector(`label[for=${field.frontedId}]`);
+                if (label) {
+                  const asterisk = document.createElement('span');
+                  asterisk.innerHTML = ' *';
+                  asterisk.style.color = 'red';
+                  label.appendChild(asterisk);
+                }
+              }
+            }
+          }
+        })
+        this.cdr.detectChanges();
+      });
+  }
+
+  /**
    * The function returns the controls of a bank form.
    */
   get f() {
@@ -234,12 +327,18 @@ export class BankComponent implements OnInit{
   }
 
   /**
+   * The function returns the controls of a bank charge form.
+   */
+  get h() {
+    return this.createBankChargeForm.controls;
+  }
+
+  /**
    * The fetchBanks function retrieves bank data using the bankService and logs the data.
    */
   fetchBanks() {
     this.bankService
       .getBanks(1100)
-      .pipe(untilDestroyed(this))
       .subscribe((data) => {
         this.bankData = data;
         log.info('Bank Data', this.bankData);
@@ -295,6 +394,116 @@ export class BankComponent implements OnInit{
   }
 
   /**
+   * The function fetches bank charges based on a bank ID and assigns the retrieved data to a variable.
+   * @param {number} bankId - The bankId parameter is a number that represents the ID of a bank.
+   */
+  fetchBankCharges(bankId: number) {
+    if (bankId) {
+      this.bankService.getBankChargesByBankId(bankId).subscribe((bankCharges) => {
+        this.bankChargeData = bankCharges;
+      });
+    } else {
+      this.bankChargeData = [];
+    }
+  }
+
+  /**
+   * The function fetches all systems data from the systems service and assigns it to the systemsData variable.
+   */
+  fetchAllSystems() {
+    this.systemsService.getSystems()
+      .subscribe(
+        (data) => {
+          this.systemsData = data;
+      })
+  }
+
+  /**
+   * The function fetches bank charge rate types from a service and assigns the data to a variable.
+   */
+  fetchBankChargeRateTypes() {
+    this.bankChargeRateTypesService.getBankChargeRateTypes()
+      .subscribe(
+        (data) => {
+          this.bankChargeRateTypesData = data;
+        })
+  }
+
+  /**
+   * The fetchGisProducts function retrieves all products from the GIS product service and stores them in the productList
+   * array.
+   */
+  fetchGisProducts() {
+    this.productList = [];
+    this.gisProductService.getAllProducts()
+      .pipe(
+        take(1),
+        map(data => {
+          const allProducts: AllProduct[] = [];
+          data.forEach(product => {
+            const combinedProduct: AllProduct = {
+              code: product.code,
+              description: product.description
+            }
+            allProducts.push(combinedProduct);
+          })
+          return allProducts;
+        })
+      )
+      .subscribe(
+        (data) => {
+         this.productList = data;
+          log.info('gis products:', this.productList);
+        })
+  }
+
+  /**
+   * The fetchLmsProducts function retrieves a list of products from an LMS service and assigns it to the productList
+   * variable.
+   */
+  fetchLmsProducts() {
+    this.productList = [];
+    this.lmsProductService.getListOfProduct()
+      .pipe(
+        take(1),
+        map(data => {
+          const allProducts: AllProduct[] = [];
+          data.forEach(product => {
+            const combinedProduct: AllProduct = {
+              code: product.code,
+              description: product.description
+            }
+            allProducts.push(combinedProduct);
+          })
+          return allProducts;
+        })
+      )
+      .subscribe(
+        (data) => {
+          this.productList = data;
+          log.info('lms products:', this.productList);
+        })
+  }
+
+  /**
+   * The function checks the short description of a selected system and fetches corresponding products based on the
+   * description.
+   */
+  onSystemChange(event: any) {
+    const shortDescription = this.selectedSystem.shortDesc;
+
+    if (shortDescription === 'GIS') {
+      this.fetchGisProducts();
+
+    } else if (shortDescription === 'LMS') {
+      this.fetchLmsProducts();
+
+    } else {
+      this.productList = [];
+    }
+
+  }
+  /**
    * The function "openBankModal" opens a modal by adding the "show" class and setting the display property to "block".
    */
   openBankModal() {
@@ -336,6 +545,33 @@ export class BankComponent implements OnInit{
    */
   closeBankBranchModal() {
     const modal = document.getElementById('bankBranchModal');
+    if (modal) {
+      modal.classList.remove('show');
+      modal.style.display = 'none';
+    }
+  }
+
+  /**
+   * The function opens a modal to display bank charges if a bank is selected, otherwise it displays an error message.
+   */
+  openBankChargeModal() {
+    const modal = document.getElementById('bankChargeModal');
+    if (modal && this.selectedBank) {
+      modal.classList.add('show');
+      modal.style.display = 'block';
+    } else {
+      this.globalMessagingService.displayErrorMessage(
+        'Error',
+        'No bank is selected!'
+      );
+    }
+  }
+
+  /**
+   * The function "closeBankChargeModal" hides and removes the "bankChargeModal" element from the DOM.
+   */
+  closeBankChargeModal() {
+    const modal = document.getElementById('bankChargeModal');
     if (modal) {
       modal.classList.remove('show');
       modal.style.display = 'none';
@@ -385,7 +621,7 @@ export class BankComponent implements OnInit{
   saveBank() {
     this.createBankForm.markAllAsTouched();
     if (this.createBankForm.invalid) {
-      this.globalMessagingService.displayErrorMessage('Error', 'Fill required fields');
+      // this.globalMessagingService.displayErrorMessage('Error', 'Fill required fields');
       return;
     }
 
@@ -512,8 +748,10 @@ export class BankComponent implements OnInit{
         adminCharge: this.selectedBank.administrativeCharge,
         pesaLink: this.selectedBank.allowPesalink,
         status: this.selectedBank.status,
-        logo: this.selectedBank.bankLogo
+        // logo: this.selectedBank.bankLogo
       });
+      this.url = this.selectedBank.bankLogo ?
+        'data:image/jpeg;base64,' + this.selectedBank.bankLogo : '';
     } else {
       this.globalMessagingService.displayErrorMessage(
         'Error',
@@ -564,7 +802,7 @@ export class BankComponent implements OnInit{
   saveBankBranch() {
     this.createBankBranchForm.markAllAsTouched();
     if (this.createBankBranchForm.invalid) {
-      this.globalMessagingService.displayErrorMessage('Error', 'Fill required fields');
+      // this.globalMessagingService.displayErrorMessage('Error', 'Fill required fields');
       return;
     }
 
@@ -705,4 +943,154 @@ export class BankComponent implements OnInit{
       );
     }
   }
+
+  /**
+   * The `saveBankCharge()` function is responsible for saving or updating a bank charge based on the form inputs and
+   * displaying success or error messages accordingly.
+   */
+  saveBankCharge() {
+    this.createBankChargeForm.markAllAsTouched();
+    if (this.createBankChargeForm.invalid) {
+      // this.globalMessagingService.displayErrorMessage('Error', 'Fill required fields');
+      return;
+    }
+
+    if(!this.selectedBankCharge) {
+      const bankChargeFormValues = this.createBankChargeForm.getRawValue();
+
+      const saveBankCharge: BankChargeDTO = {
+        bankCode: this.selectedBank.id,
+        bankName: this.selectedBank.name,
+        dateFrom: bankChargeFormValues.dateFrom,
+        dateTo: bankChargeFormValues.dateTo,
+        id: null,
+        productCode: bankChargeFormValues.product.code,
+        productName: bankChargeFormValues.product.description,
+        rate: bankChargeFormValues.rate,
+        rateType: bankChargeFormValues.rateType,
+        systemCode: this.selectedSystem?.id,
+        systemName: this.selectedSystem?.systemName
+
+      }
+      log.info('bank charge create', saveBankCharge);
+      this.bankService.createBankCharge(saveBankCharge)
+        .subscribe((data) => {
+            this.globalMessagingService.displaySuccessMessage('Success', 'Successfully Created a bank charge');
+
+            this.createBankChargeForm.reset();
+            this.fetchBankCharges(this.selectedBank.id);
+            this.closeBankChargeModal();
+          },
+          error => {
+            this.globalMessagingService.displayErrorMessage('Error', error.error.message);
+          })
+    }
+    else {
+      const bankChargeFormValues = this.createBankChargeForm.getRawValue();
+      const bankChargeId = this.selectedBankCharge.id;
+      const bankId = this.selectedBank.id;
+
+      const saveBankCharge: BankChargeDTO = {
+        bankCode: bankId,
+        bankName: this.selectedBank.name,
+        dateFrom: bankChargeFormValues.dateFrom,
+        dateTo: bankChargeFormValues.dateTo,
+        id: bankChargeId,
+        productCode: bankChargeFormValues.product.code,
+        productName: bankChargeFormValues.product.description,
+        rate: bankChargeFormValues.rate,
+        rateType: bankChargeFormValues.rateType,
+        systemCode: this.selectedSystem?.id,
+        systemName: this.selectedSystem?.systemName
+      }
+
+      log.info('bank charge update', saveBankCharge);
+      this.bankService.updateBankCharge(bankChargeId, saveBankCharge)
+        .subscribe((data) => {
+            this.globalMessagingService.displaySuccessMessage('Success', 'Successfully updated a bank charge');
+
+            this.createBankChargeForm.reset();
+            this.fetchBankCharges(this.selectedBank.id);
+            this.closeBankChargeModal();
+          },
+          error => {
+            this.globalMessagingService.displayErrorMessage('Error', error.error.message);
+          })
+    }
+  }
+
+  /**
+   * The function `editBankCharge()` opens a modal and populates it with data from the selected bank charge, or displays an
+   * error message if no bank charge is selected.
+   */
+  editBankCharge() {
+    if (this.selectedBankCharge) {
+      this.openBankChargeModal();
+      const selectedDateFrom = new Date(this.selectedBankCharge.dateFrom).toISOString().split('T')[0];
+
+      const selectedDateTo = new Date(this.selectedBankCharge.dateTo).toISOString().split('T')[0];
+
+      this.createBankChargeForm.patchValue({
+        dateFrom: selectedDateFrom,
+        dateTo: selectedDateTo,
+        product: this.selectedBankCharge.productName,
+        rate: this.selectedBankCharge.rate,
+        rateType: this.selectedBankCharge.rateType,
+        system: this.selectedBankCharge.systemName
+      });
+    } else {
+      this.globalMessagingService.displayErrorMessage(
+        'Error',
+        'No bank charge is selected!'
+      );
+    }
+  }
+
+  /**
+   * The function "deleteBankCharge" displays a confirmation modal for deleting a bank charge.
+   */
+  deleteBankCharge() {
+    this.bankChargeConfirmationModal.show();
+  }
+
+  /**
+   * The function `confirmBankChargeDelete()` deletes a selected bank charge and displays a success message if the deletion
+   * is successful, or an error message if there is an error.
+   */
+  confirmBankChargeDelete() {
+    if (this.selectedBankCharge) {
+      const bankChargeId = this.selectedBankCharge.id;
+      this.bankService.deleteBankCharge(bankChargeId).subscribe((data) => {
+          this.globalMessagingService.displaySuccessMessage(
+            'success',
+            'Successfully deleted a bank charge'
+          );
+          this.fetchBankCharges(this.selectedBank.id);
+        },
+        error => {
+          this.globalMessagingService.displayErrorMessage('Error', error.error.message);
+        });
+    } else {
+      this.globalMessagingService.displayErrorMessage(
+        'Error',
+        'No bank charge is selected.'
+      );
+    }
+  }
+
+  /**
+   * The function filters a bank table based on a user input value.
+   * @param {Event} event - The event parameter is an object that represents an event that has occurred, such as a user
+   * input event or a button click event. It contains information about the event, such as the target element that
+   * triggered the event.
+   */
+  filterBanks(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.bankTable.filterGlobal(filterValue, 'contains');
+  }
+}
+
+interface AllProduct {
+  code: number,
+  description: string
 }
