551254bfdbf5e1a0cb2d49c801a4c2bdb5fe2bbd
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-01-24 10:04:24 +0300

    ability to view, create, update, delete bank charges

--- src/app/shared/services/setups/bank/bank.service.ts
+++ src/app/shared/services/setups/bank/bank.service.ts
@@ -3,7 +3,7 @@ import { AppConfigService } from '../../../../core/config/app-config-service';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import {
-  BankBranchDTO,
+  BankBranchDTO, BankChargeDTO,
   BankDTO,
   BankRegionDTO,
   CurrencyDTO,
@@ -191,6 +191,12 @@ export class BankService {
     );
   }
 
+  /**
+   * The function `createBank` sends a POST request to create a new bank using the provided data.
+   * @param {BankDTO} data - The `data` parameter is of type `BankDTO`, which is an object containing the information
+   * needed to create a bank.
+   * @returns an Observable of type BankDTO.
+   */
   createBank(
     data: BankDTO
   ): Observable<BankDTO> {
@@ -205,6 +211,14 @@ export class BankService {
     );
   }
 
+  /**
+   * The function `updateBank` sends a PUT request to update a bank record with the specified ID using the provided data.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank you want to
+   * update in the system.
+   * @param {BankDTO} data - The `data` parameter is of type `BankDTO`, which is an object containing the updated
+   * information for a bank.
+   * @returns an Observable of type BankDTO.
+   */
   updateBank(id: number, data: BankDTO): Observable<BankDTO> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -217,6 +231,13 @@ export class BankService {
     );
   }
 
+  /**
+   * The `deleteBank` function sends an HTTP DELETE request to delete a bank with the specified ID.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank that you want to
+   * delete.
+   * @returns an HTTP DELETE request to delete a bank with the specified ID. The response is expected to be of type
+   * `BankDTO`.
+   */
   deleteBank(id: number) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -228,6 +249,12 @@ export class BankService {
     );
   }
 
+  /**
+   * The function `createBankBranch` sends a POST request to create a new bank branch using the provided data.
+   * @param {POSTBankBranchDTO} data - The `data` parameter is of type `POSTBankBranchDTO`, which is an object containing
+   * the information needed to create a bank branch.
+   * @returns an Observable of type POSTBankBranchDTO.
+   */
   createBankBranch(
     data: POSTBankBranchDTO
   ): Observable<POSTBankBranchDTO> {
@@ -242,6 +269,15 @@ export class BankService {
     );
   }
 
+  /**
+   * The function `updateBankBranch` sends a PUT request to update a bank branch with the specified ID using the provided
+   * data.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank branch that
+   * needs to be updated. It is used to identify the specific bank branch in the database.
+   * @param {POSTBankBranchDTO} data - The `data` parameter is of type `POSTBankBranchDTO`, which is an object containing
+   * the data to be updated for a bank branch.
+   * @returns an Observable of type POSTBankBranchDTO.
+   */
   updateBankBranch(id: number, data: POSTBankBranchDTO): Observable<POSTBankBranchDTO> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -254,6 +290,13 @@ export class BankService {
     );
   }
 
+  /**
+   * The function `deleteBankBranch` sends a DELETE request to the server to delete a bank branch with the specified ID.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank branch that you
+   * want to delete.
+   * @returns an HTTP DELETE request to delete a bank branch with the specified ID. The response is expected to be of type
+   * `POSTBankBranchDTO`.
+   */
   deleteBankBranch(id: number) {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -261,6 +304,87 @@ export class BankService {
     });
     return this.http.delete<POSTBankBranchDTO>(
       `/${this.baseUrl}/setups/bank-branches/${id}`,
+      { headers: headers }
+    );
+  }
+
+  /**
+   * The function `getBankChargesByBankId` retrieves bank charges based on a given bank ID.
+   * @param {number} bankId - The `bankId` parameter is a number that represents the ID of a bank. It is used to retrieve
+   * bank charges associated with a specific bank.
+   * @returns an Observable of type BankChargeDTO[].
+   */
+  getBankChargesByBankId(bankId: number): Observable<BankChargeDTO[]> {
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    });
+    const params = new HttpParams().set('bankId', `${bankId}`);
+    return this.http.get<BankChargeDTO[]>(
+      `/${this.baseUrl}/setups/bank-charges`,
+      {
+        headers: header,
+        params: params
+      }
+    );
+  }
+
+  /**
+   * The function `createBankCharge` sends a POST request to a specific endpoint with the provided data and returns an
+   * Observable of the response.
+   * @param {BankChargeDTO} data - The `data` parameter is of type `BankChargeDTO`, which is an object containing the
+   * necessary information to create a bank charge.
+   * @returns an Observable of type BankChargeDTO.
+   */
+  createBankCharge(
+    data: BankChargeDTO
+  ): Observable<BankChargeDTO> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+    return this.http.post<BankChargeDTO>(
+      `/${this.baseUrl}/setups/bank-charges`,
+      JSON.stringify(data),
+      { headers: headers }
+    );
+  }
+
+  /**
+   * The function `updateBankCharge` sends a PUT request to update a bank charge with the specified ID using the provided
+   * data.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank charge you want
+   * to update. It is used to specify which bank charge should be updated.
+   * @param {BankChargeDTO} data - The `data` parameter is of type `BankChargeDTO`, which is an object containing the
+   * updated bank charge information.
+   * @returns an Observable of type BankChargeDTO.
+   */
+  updateBankCharge(id: number, data: BankChargeDTO): Observable<BankChargeDTO> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+    return this.http.put<BankChargeDTO>(
+      `/${this.baseUrl}/setups/bank-charges/${id}`,
+      data,
+      { headers: headers }
+    );
+  }
+
+  /**
+   * The function `deleteBankCharge` sends a DELETE request to the server to delete a bank charge with the specified ID.
+   * @param {number} id - The `id` parameter is a number that represents the unique identifier of the bank charge that
+   * needs to be deleted.
+   * @returns an HTTP DELETE request to delete a bank charge with the specified ID. The response is expected to be of type
+   * `BankChargeDTO`.
+   */
+  deleteBankCharge(id: number) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    });
+    return this.http.delete<BankChargeDTO>(
+      `/${this.baseUrl}/setups/bank-charges/${id}`,
       { headers: headers }
     );
   }
