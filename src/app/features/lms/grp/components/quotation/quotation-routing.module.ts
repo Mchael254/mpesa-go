405c1ec0ac4947e3a5887e74f464d49110308046
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-02-19 12:21:36 +0300

    VQ-1279/Reinsurance data display

--- src/app/features/lms/grp/components/quotation/quotation-routing.module.ts
+++ src/app/features/lms/grp/components/quotation/quotation-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuickComponent } from './components/quick/quick.component';
 import { CoverageDetailsComponent } from './components/coverage-details/coverage-details.component';
 import { SummaryComponent } from './components/summary/summary.component';
+import { ClientCreationComponent } from './components/client-creation/client-creation.component';
 
 const routes: Routes = [
   {path:'quick', component: QuickComponent},
   {path:'coverage', component: CoverageDetailsComponent},
-  {path:'summary', component: SummaryComponent}
+  {path:'summary', component: SummaryComponent},
+  {path:'client', component: ClientCreationComponent}
 ];
 
 @NgModule({
