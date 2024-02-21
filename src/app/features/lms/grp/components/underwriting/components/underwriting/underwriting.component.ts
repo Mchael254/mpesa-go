c9c229ff45793af14856f71ce07d231940f5a7b5
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-02-21 08:08:23 +0300

    VQ-1469/New client details capture

--- src/app/features/lms/grp/components/underwriting/components/underwriting/underwriting.component.ts
+++ src/app/features/lms/grp/components/underwriting/components/underwriting/underwriting.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import stepData from '../../data/steps.json';
+import { UnderwritingService } from '../../service/underwriting.service';
 
 @Component({
   selector: 'app-underwriting',
@@ -10,9 +11,11 @@ import stepData from '../../data/steps.json';
 export class UnderwritingComponent implements OnInit, OnDestroy {
   quoteSummary = 'underwriting'
   steps = stepData;
+  quotationCode = 20247640;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private underwritingService: UnderwritingService
   ) { }
 
   ngOnInit(): void {
@@ -24,7 +27,10 @@ export class UnderwritingComponent implements OnInit, OnDestroy {
   }
 
   onConvert() {
+  this.underwritingService.convertQuoteToPolicy(this.quotationCode).subscribe((policySummary) => {
+    console.log("policySummary", policySummary);
     this.router.navigate(['/home/lms/grp/underwriting/endorsement']);
+  });
   }
 
 }
