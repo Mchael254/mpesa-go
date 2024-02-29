35392cfc8bc5fccb1bfad5050858756b778cb81a
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-02-29 10:12:59 +0300

    VQ-1543/VQ-1544/VQ-1545/Dashboard landing screen

--- src/app/features/lms/grp/components/dashboard/dashboard-routing.module.ts
+++ src/app/features/lms/grp/components/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingScreenComponent } from './components/landing-screen/landing-screen.component';
+import { LandingDashboardComponent } from './components/landing-dashboard/landing-dashboard.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: LandingScreenComponent }
+  { path: 'dashboard', component: LandingScreenComponent },
+  { path: 'dashboard-screen', component: LandingDashboardComponent },
 ];
 
 @NgModule({
