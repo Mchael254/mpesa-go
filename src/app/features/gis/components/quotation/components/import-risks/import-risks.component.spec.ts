7e3cab9a5a28bb3e2c89fc729a7a589ad6295c32
faithwasike <faith.wasike@turnkeyafrica.com>
2024-03-05 11:37:30 +0300

    VQ-1501:ability to import risks

--- src/app/features/gis/components/quotation/components/import-risks/import-risks.component.spec.ts
+++ src/app/features/gis/components/quotation/components/import-risks/import-risks.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImportRisksComponent } from './import-risks.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SubclassesService } from '../../../setups/services/subclasses/subclasses.service';
+import { of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http'; 
 
+class MockAppConfig {
+  config = {
+    contextPath: {
+      gis_services: 'String' // Provide the expected value for gis_services
+    }
+  };
+}
 describe('ImportRisksComponent', () => {
   let component: ImportRisksComponent;
   let fixture: ComponentFixture<ImportRisksComponent>;
+  let subclassService: SubclassesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ImportRisksComponent]
+      declarations: [ImportRisksComponent],
+      imports: [RouterTestingModule,HttpClientModule],
+      providers: [SubclassesService] 
     });
     fixture = TestBed.createComponent(ImportRisksComponent);
     component = fixture.componentInstance;
+    subclassService = TestBed.inject(SubclassesService);
     fixture.detectChanges();
   });
 
