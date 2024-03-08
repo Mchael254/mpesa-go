d590191d5dd366763525828837476c4cea4f988d
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-03-08 18:57:49 +0300

    Fetching and displaying claim details

--- src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
+++ src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {TicketsDTO} from "../../../../data/ticketsDTO";
+import {Logger} from "../../../../../../shared/services";
+import {untilDestroyed} from "../../../../../../shared/services/until-destroyed";
+import {ViewClaimService} from "../../../../../gis/services/claims/view-claim.service";
+import {Pagination} from "../../../../../../shared/data/common/pagination";
+import {ClaimsDTO} from "../../../../../gis/data/claims-dto";
 
+const log = new Logger('ClaimDetailsComponent');
 @Component({
   selector: 'app-claim-details',
   templateUrl: './claim-details.component.html',
   styleUrls: ['./claim-details.component.css']
 })
-export class ClaimDetailsComponent {
+export class ClaimDetailsComponent implements OnInit {
+  @Input() selectedSpringTickets: TicketsDTO;
 
+  claimsData: Pagination<ClaimsDTO> = <Pagination<ClaimsDTO>>{};
+  claim: ClaimsDTO;
+
+  constructor(private claimsService: ViewClaimService) {
+  }
+  ngOnInit(): void {
+    log.info('claim>>', this.selectedSpringTickets);
+    this.fetchClaimDetails(this.selectedSpringTickets?.ticket?.claimNo);
+  }
+
+  fetchClaimDetails(code:any) {
+    this.claimsService.getClaimByClaimNo(code)
+      .pipe(
+        untilDestroyed(this),
+      )
+      .subscribe(
+        (data: Pagination<ClaimsDTO>) => {
+          data.content.forEach( claim => {
+            this.claim = claim
+          });
+          this.claimsData = data;
+
+          log.info('claimsdata>>', this.claimsData, this.claim)
+        })
+  }
+
+  ngOnDestroy(): void {
+  }
 }
