208eed271dfac01270c002f0608b16ad08b55383
HopeIbrahim <hope.ibrahim@turnkey.com>
2024-03-11 17:30:22 +0300

    VQ-1586:Ability to View Policy Product Details.

--- src/app/features/gis/components/policy/policy-routing.module.ts
+++ src/app/features/gis/components/policy/policy-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPoliciesComponent } from './components/list-policies/list-policies.component';
-import { NewBusinessComponent } from './components/new-business/new-business.component';
+import { UnderwritingComponent } from './components/underwriting/underwriting.component';
+import { PolicyProductComponent } from './components/policy-product/policy-product.component';
 
 const routes: Routes = [
   {
     path: 'list/:id', component:ListPoliciesComponent
   },
   {
-    path: 'new-business', component:NewBusinessComponent
+    path: 'underwriting', component:UnderwritingComponent
+  },
+  {
+    path: 'policy-product', component:PolicyProductComponent
   },
 ];
 
