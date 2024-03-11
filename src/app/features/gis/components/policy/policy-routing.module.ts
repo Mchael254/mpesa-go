7a962618992e68cbb23c000d970e3c6a0e23df6e
HopeIbrahim <hope.ibrahim@turnkey.com>
2024-03-11 14:37:41 +0300

    VQ-1585:Ability to start New Business Transaction.

--- src/app/features/gis/components/policy/policy-routing.module.ts
+++ src/app/features/gis/components/policy/policy-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPoliciesComponent } from './components/list-policies/list-policies.component';
+import { NewBusinessComponent } from './components/new-business/new-business.component';
 
 const routes: Routes = [
   {
     path: 'list/:id', component:ListPoliciesComponent
+  },
+  {
+    path: 'new-business', component:NewBusinessComponent
   },
 ];
 
