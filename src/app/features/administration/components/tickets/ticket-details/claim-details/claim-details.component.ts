e0ea92d3692efb188de88b631cd9b58350c2fcf5
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-03-18 14:24:53 +0300

    Updated claim exceptions table

--- src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
+++ src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
@@ -18,11 +18,19 @@ export class ClaimDetailsComponent implements OnInit {
   claimsData: Pagination<ClaimsDTO> = <Pagination<ClaimsDTO>>{};
   claim: ClaimsDTO;
 
+  public pageSize: 5;
+  claimExceptionData: any[];
+  selectedClaimException: any[] = [];
+
+  completionRemarksData: any[];
+  selectedRemark: string = '';
+
   constructor(private claimsService: ViewClaimService) {
   }
   ngOnInit(): void {
     log.info('claim>>', this.selectedSpringTickets);
     this.fetchClaimDetails(this.selectedSpringTickets?.ticket?.claimNo);
+    // this.fetchClaimExceptions();
   }
 
   fetchClaimDetails(code:any) {
@@ -41,6 +49,43 @@ export class ClaimDetailsComponent implements OnInit {
         })
   }
 
+  /*fetchClaimExceptions() {
+    this.claimsService.getListOfExceptionsByClaimNo('C/2018/NBI/MON/021176', 19263658, 'CP')
+      .subscribe({
+        next: (data) => {
+          this.claimExceptionData = data.embedded && data.embedded.length > 0 ? data.embedded[0] : [];
+          log.info('Receipts>>', this.claimExceptionData);
+        }
+      })
+  }*/
+
+  onDropdownChange(event, exception) {
+    log.info('>>>>', event.value, exception.code)
+
+    /*if (event.value) {
+      const payload: any = {
+        exceptionCode: exception?.code,
+        exceptionUnderwritingDecision: event.value
+      }
+      this.claimsService.saveExceptionRemark(payload)
+        .subscribe({
+          next: (data) => {
+            this.authoriseExceptionsData = data;
+            this.globalMessagingService.displaySuccessMessage('Success', 'Successfully saved remark');
+            this.getAuthorizationExceptionDetails();
+            this.cdr.detectChanges();
+          },
+          error: err => {
+            this.globalMessagingService.displayErrorMessage('Error', err.error.message);
+          }
+        })
+    } else {
+      this.globalMessagingService.displayErrorMessage(
+        'Error',
+        'Exception remark not selected.'
+      );
+    }*/
+  }
   ngOnDestroy(): void {
   }
 }
