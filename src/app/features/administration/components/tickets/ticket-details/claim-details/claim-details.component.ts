ae4172195cbe4bd66f8550c6227ca1f404a02c65
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-03-28 16:31:36 +0300

    Claims details mapping, consuming revision details endpoints

--- src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
+++ src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
@@ -1,10 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TicketsDTO} from "../../../../data/ticketsDTO";
 import {Logger} from "../../../../../../shared/services";
-import {untilDestroyed} from "../../../../../../shared/services/until-destroyed";
 import {ViewClaimService} from "../../../../../gis/services/claims/view-claim.service";
-import {Pagination} from "../../../../../../shared/data/common/pagination";
 import {ClaimsDTO} from "../../../../../gis/data/claims-dto";
+import {GlobalMessagingService} from "../../../../../../shared/services/messaging/global-messaging.service";
+import {AuthService} from "../../../../../../shared/services/auth.service";
 
 const log = new Logger('ClaimDetailsComponent');
 @Component({
@@ -15,8 +15,7 @@ const log = new Logger('ClaimDetailsComponent');
 export class ClaimDetailsComponent implements OnInit {
   @Input() selectedSpringTickets: TicketsDTO;
 
-  claimsData: Pagination<ClaimsDTO> = <Pagination<ClaimsDTO>>{};
-  claim: ClaimsDTO;
+  @Input() claim: ClaimsDTO;
 
   public pageSize: 5;
   claimExceptionData: any[];
@@ -24,40 +23,33 @@ export class ClaimDetailsComponent implements OnInit {
 
   completionRemarksData: any[];
   selectedRemark: string = '';
+  claimTransactionData: any[];
+  claimTransaction: any;
 
-  constructor(private claimsService: ViewClaimService) {
+  claimPaymentTransactionData: any[];
+  claimPaymentTransaction: any;
+
+  isLoadingAuthExc: boolean = false;
+
+  constructor(private claimsService: ViewClaimService,
+              private globalMessagingService: GlobalMessagingService,
+              private authService: AuthService) {
   }
   ngOnInit(): void {
     log.info('claim>>', this.selectedSpringTickets);
-    this.fetchClaimDetails(this.selectedSpringTickets?.ticket?.claimNo);
-    // this.fetchClaimExceptions();
+    // this.fetchClaimDetails(this.selectedSpringTickets?.ticket?.claimNo);
+    this.fetchClaimTransactionDetails();
   }
 
-  fetchClaimDetails(code:any) {
-    this.claimsService.getClaimByClaimNo(code)
-      .pipe(
-        untilDestroyed(this),
-      )
-      .subscribe(
-        (data: Pagination<ClaimsDTO>) => {
-          data.content.forEach( claim => {
-            this.claim = claim
-          });
-          this.claimsData = data;
-
-          log.info('claimsdata>>', this.claimsData, this.claim)
-        })
-  }
-
-  /*fetchClaimExceptions() {
-    this.claimsService.getListOfExceptionsByClaimNo('C/2018/NBI/MON/021176', 19263658, 'CP')
+  fetchClaimExceptions() {
+    this.claimsService.getListOfExceptionsByClaimNo(this.selectedSpringTickets?.ticket?.claimNo, this.claimTransaction?.transactionNo, this.claimTransaction?.transactionCode)
       .subscribe({
         next: (data) => {
           this.claimExceptionData = data.embedded && data.embedded.length > 0 ? data.embedded[0] : [];
-          log.info('Receipts>>', this.claimExceptionData);
+          log.info('exceptions>>', this.claimExceptionData);
         }
       })
-  }*/
+  }
 
   onDropdownChange(event, exception) {
     log.info('>>>>', event.value, exception.code)
@@ -86,6 +78,77 @@ export class ClaimDetailsComponent implements OnInit {
       );
     }*/
   }
+
+  authoriseExceptions() {
+    this.isLoadingAuthExc = true;
+    const selectedExceptions = this.selectedClaimException.map(data=> data.code);
+
+    log.info('selected exceptions', selectedExceptions);
+    const assignee = this.authService.getCurrentUserName();
+    /*if (selectedExceptions.length > 0) {
+      const payload: any = {
+        code: selectedExceptions,
+        userName: assignee
+      }
+      this.policiesService.authoriseExceptions(payload)
+        .subscribe({
+          next: (data) => {
+            this.authoriseExceptionsData = data;
+            this.globalMessagingService.displaySuccessMessage('Success', 'Successfully authorized exception');
+            this.isLoadingAuthExc = false;
+          },
+          error: err => {
+            this.globalMessagingService.displayErrorMessage('Error', err.error.message);
+            this.isLoadingAuthExc = false;
+          }
+        })
+    } else {
+      this.globalMessagingService.displayErrorMessage(
+        'Error',
+        'No exception is selected.'
+      );
+      this.isLoadingAuthExc = false;
+    }*/
+  }
+
+  fetchClaimTransactionDetails() {
+    this.claimsService.getClaimsTransactionsDetails(this.selectedSpringTickets?.ticket?.claimNo)
+      .subscribe({
+        next: (data) => {
+          data.embedded[0].forEach( transaction => {
+            this.claimTransaction = transaction
+          });
+          this.claimTransactionData = data.embedded[0];
+          if (this.claimTransaction) {
+            this.fetchClaimPaymentsTransactionDetails();
+            this.fetchClaimExceptions();
+            this.claimsService.claimTransactionObject.set({...this.claimTransaction, fromClaimTransaction: true});
+          }
+          log.info('claim transaction data>>', this.claimTransactionData, this.claimTransaction);
+          // transactionNo
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.message);
+        }
+      })
+  }
+
+  fetchClaimPaymentsTransactionDetails() {
+    this.claimsService.getClaimsPaymentTransactionsDetails(this.selectedSpringTickets?.ticket?.claimNo, this.claimTransaction?.code)
+      .subscribe({
+        next: (data) => {
+          data.embedded[0].forEach( transaction => {
+            this.claimPaymentTransaction = transaction
+          });
+          this.claimPaymentTransactionData = data.embedded[0];
+          log.info('claim payment transaction data>>', this.claimPaymentTransactionData, this.claimPaymentTransaction);
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.message);
+        }
+      })
+  }
+
   ngOnDestroy(): void {
   }
 }
