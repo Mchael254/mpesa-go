31a96f196a731116f521727adfb971e3c54276fe
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-04-05 18:12:07 +0300

    VQ-1792/Quotation process completion

---
+++ src/app/features/lms/grp/components/quotation/models/IdentityTypes/IdentityTypeDTO.ts
@@ -0,0 +1,8 @@
+export interface IdentityTypeDTO {
+    id: number;
+    identityFormat?: string;
+    identityFormatError?: string;
+    name: string;
+    organizationId: number;
+    value: string;
+}
