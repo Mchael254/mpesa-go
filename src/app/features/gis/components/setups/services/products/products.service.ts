c3172c362bdd61f9b92c7d395dd17a3c887c9a8c
HopeIbrahim <hope.ibrahim@turnkey.com>
2024-04-12 09:57:02 +0300

    VQ-1733:Ability to Capture Risk details for Subclass Motor.

--- src/app/features/gis/components/setups/services/products/products.service.ts
+++ src/app/features/gis/components/setups/services/products/products.service.ts
@@ -67,27 +67,27 @@ export class ProductsService {
     
     })
 
-    return this.api.GET<Products[]>(`products?page=${page}&pageSize=${size}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<Products[]>(`api/v1/products?page=${page}&pageSize=${size}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl)
     )  }
 
   createProductgroup(data: any): Observable<Product_group> {
     console.log(JSON.stringify(data))
-    return this.api.POST<Product_group>(`product-groups`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
+    return this.api.POST<Product_group>(`api/v1/product-groups`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
       .pipe(
     )
   }
   createProducts(data: any): Observable<Products> {
     console.log(JSON.stringify(data))
-    return this.api.POST<Products>(`products`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
+    return this.api.POST<Products>(`api/v1/products`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
       .pipe(
     )
   }
   editProducts (data:any,code:any): Observable<Products> {
 
     console.log(JSON.stringify(data))
-    return this.api.PUT<Products>(`products/${code}`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
+    return this.api.PUT<Products>(`api/v1/products/${code}`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
     .pipe(
       retry(1),
       catchError(this.errorHandl)
@@ -95,26 +95,26 @@ export class ProductsService {
   }
   createProductSubclasses(data: any): Observable<SubclassesDTO> {
     console.log(JSON.stringify(data))
-    return this.api.POST<SubclassesDTO>(`product-subclasses`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
+    return this.api.POST<SubclassesDTO>(`api/v1/product-subclasses`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
       .pipe(
     )
   }
   getASubclasses (): Observable<SubclassesDTO>{
 
-    return this.api.GET<SubclassesDTO>(`product-subclasses?pageNo=0&pageSize=90`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<SubclassesDTO>(`api/v1/product-subclasses?pageNo=0&pageSize=90`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl) 
     );
   }
   getsubclassByCode(code: any): Observable<SubclassesDTO>{
-    return this.api.GET<SubclassesDTO>(`product-subclasses/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<SubclassesDTO>(`api/v1/product-subclasses/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       
     )
   }
   getGroupedData(): Observable<any[]> {
     return forkJoin([
-      this.api.GET(`products?pageNo=0&pageSize=100000`,API_CONFIG.GIS_SETUPS_BASE_URL),
-      this.api.GET(`product-groups?pageNo=0&pageSize=100000`,API_CONFIG.GIS_SETUPS_BASE_URL),
+      this.api.GET(`api/v1/products?pageNo=0&pageSize=100000`,API_CONFIG.GIS_SETUPS_BASE_URL),
+      this.api.GET(`api/v1/product-groups?pageNo=0&pageSize=100000`,API_CONFIG.GIS_SETUPS_BASE_URL),
     ]).pipe(
       map(([products, productGroups]) => {
         return Object.values(products).map(product => {
@@ -125,7 +125,7 @@ export class ProductsService {
     );
   }
   updateProductGroupbyCode(data:any,code:any): Observable<Product_group>{
-    return this.api.PUT<Product_group>(`product-groups/${code}`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
+    return this.api.PUT<Product_group>(`api/v1/product-groups/${code}`, JSON.stringify(data), API_CONFIG.GIS_SETUPS_BASE_URL)
     .pipe(
       retry(1),
       catchError(this.errorHandl)
@@ -133,7 +133,7 @@ export class ProductsService {
   }
   getProductGroupByCode(code: number): Observable<Product_group[]>{
 
-    return this.api.GET<Product_group[]>(`product-groups/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<Product_group[]>(`api/v1/product-groups/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl)
     )
@@ -141,27 +141,27 @@ export class ProductsService {
   }
   getProductByCode(code: number): Observable<Products[]>{
     
-    return this.api.GET<Products[]>(`products/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<Products[]>(`api/v1/products/${code}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl)
     )
   
   }
   getCoverToDate(coverFromDate:string,productCode :Number):Observable<any>{
-    return this.api.GET<any>(`products/coverToDate?coverFromDate=${coverFromDate}&productCode=${productCode}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<any>(`api/v1/products/coverToDate?coverFromDate=${coverFromDate}&productCode=${productCode}`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl)
     )
   }
   getYearOfManufacture():Observable<any>{
-    return this.api.GET<any>(`products/retrieveYearofManufacture?`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<any>(`api/v1/products/retrieveYearofManufacture?`,API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl)
     )
   }
   getProductSubclasses (productCode): Observable<SubclassesDTO>{
 
-    return this.api.GET<SubclassesDTO>(`product-subclasses?productCode=${productCode}`, API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
+    return this.api.GET<SubclassesDTO>(`api/v1/product-subclasses?productCode=${productCode}`, API_CONFIG.GIS_SETUPS_BASE_URL).pipe(
       retry(1),
       catchError(this.errorHandl) 
     );
