b0f7e9b1526418bd248f6fff01bacfe287638ea9
HopeIbrahim <hope.ibrahim@turnkey.com>
2024-04-17 14:29:59 +0300

    VQ-1815:Unit tests on Product and Risk detail screens

--- src/app/features/gis/components/setups/services/sub-class-cover-types/sub-class-cover-types.service.ts
+++ src/app/features/gis/components/setups/services/sub-class-cover-types/sub-class-cover-types.service.ts
@@ -5,11 +5,12 @@ import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {AppConfigService} from "../../../../../../core/config/app-config-service";
 import {catchError, retry} from "rxjs/operators";
 import { environment } from '../../../../../../../environments/environment';
-import { SESSION_KEY } from 'src/app/features/lms/util/session_storage_enum';
-import { StringManipulation } from 'src/app/features/lms/util/string_manipulation';
-import { SessionStorageService } from 'src/app/shared/services/session-storage/session-storage.service';
-import { ApiService } from 'src/app/shared/services/api/api.service';
-import { API_CONFIG } from 'src/environments/api_service_config';
+
+import { SESSION_KEY } from '../../../../../../../app/features/lms/util/session_storage_enum';
+import { StringManipulation } from '../../../../../../../app/features/lms/util/string_manipulation';
+import { SessionStorageService } from '../../../../../../shared/services/session-storage/session-storage.service';
+import { API_CONFIG } from '../../../../../../../environments/api_service_config';
+import { ApiService } from '../../../../../../../app/shared/services/api/api.service';
 @Injectable({
   providedIn: 'root'
 })
