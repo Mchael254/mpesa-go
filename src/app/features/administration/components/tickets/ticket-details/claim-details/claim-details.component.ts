02ca49a7ad07ba59e314aa1f9fea538a144ab586
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-04-23 17:34:05 +0300

    Updated missing documentation

--- src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
+++ src/app/features/administration/components/tickets/ticket-details/claim-details/claim-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 import {TicketsDTO} from "../../../../data/ticketsDTO";
 import {Logger} from "../../../../../../shared/services";
 import {ViewClaimService} from "../../../../../gis/services/claims/view-claim.service";
@@ -6,6 +6,11 @@ import {ClaimsDTO} from "../../../../../gis/data/claims-dto";
 import {GlobalMessagingService} from "../../../../../../shared/services/messaging/global-messaging.service";
 import {AuthService} from "../../../../../../shared/services/auth.service";
 import {LocalStorageService} from "../../../../../../shared/services/local-storage/local-storage.service";
+import {
+  CompletionRemarksService
+} from "../../../../../gis/components/setups/services/completion-remarks/completion-remarks.service";
+import {PoliciesService} from "../../../../../gis/services/policies/policies.service";
+import {untilDestroyed} from "../../../../../../shared/services/until-destroyed";
 
 const log = new Logger('ClaimDetailsComponent');
 @Component({
@@ -26,25 +31,41 @@ export class ClaimDetailsComponent implements OnInit {
   selectedRemark: string = '';
   claimTransactionData: any[];
   claimTransaction: any;
+  claimRevisionData: any[];
 
   claimPaymentTransactionData: any[];
   claimPaymentTransaction: any;
 
+  authoriseExceptionsData: any;
+  showDropdownForException: any;
+
+  authorizationLevelsData: any[];
+  selectedAuthorizationLevel: any[] = [];
+
   isLoadingAuthExc: boolean = false;
   isLoadingTransactionData: boolean = false;
   isLoadingMakeUndo: boolean = false;
+  isLoadingAuthClaim: boolean = false;
 
   constructor(private claimsService: ViewClaimService,
               private globalMessagingService: GlobalMessagingService,
               private authService: AuthService,
-              private localStorageService: LocalStorageService) {
+              private localStorageService: LocalStorageService,
+              private completionRemarksService: CompletionRemarksService,
+              private policiesService: PoliciesService,
+              private cdr: ChangeDetectorRef,) {
   }
   ngOnInit(): void {
     log.info('claim>>', this.selectedSpringTickets);
     // this.fetchClaimDetails(this.selectedSpringTickets?.ticket?.claimNo);
     this.fetchClaimTransactionDetails();
+    this.fetchCompletionRemarks();
+    this.getAuthorizationLevels();
   }
 
+  /**
+   * The `fetchClaimExceptions` function fetches a list of exceptions related to a specific claim and logs the data.
+   */
   fetchClaimExceptions() {
     this.claimsService.getListOfExceptionsByClaimNo(this.selectedSpringTickets?.ticket?.claimNo, this.claimTransaction?.transactionNo, this.claimTransaction?.transactionCode)
       .subscribe({
@@ -55,20 +76,24 @@ export class ClaimDetailsComponent implements OnInit {
       })
   }
 
+  /**
+   * The function `onDropdownChange` handles the change event of a dropdown, saves exception remark data, and displays
+   * success or error messages accordingly.
+   */
   onDropdownChange(event, exception) {
     log.info('>>>>', event.value, exception.code)
 
-    /*if (event.value) {
+    if (event.value) {
       const payload: any = {
         exceptionCode: exception?.code,
         exceptionUnderwritingDecision: event.value
       }
-      this.claimsService.saveExceptionRemark(payload)
+      this.policiesService.saveExceptionRemark(payload)
         .subscribe({
           next: (data) => {
             this.authoriseExceptionsData = data;
             this.globalMessagingService.displaySuccessMessage('Success', 'Successfully saved remark');
-            this.getAuthorizationExceptionDetails();
+            this.fetchClaimExceptions();
             this.cdr.detectChanges();
           },
           error: err => {
@@ -80,16 +105,20 @@ export class ClaimDetailsComponent implements OnInit {
         'Error',
         'Exception remark not selected.'
       );
-    }*/
+    }
   }
 
+  /**
+   * The `authoriseExceptions` function handles the authorization of selected claim exceptions, displaying success or error
+   * messages accordingly.
+   */
   authoriseExceptions() {
     this.isLoadingAuthExc = true;
     const selectedExceptions = this.selectedClaimException.map(data=> data.code);
 
     log.info('selected exceptions', selectedExceptions);
     const assignee = this.authService.getCurrentUserName();
-    /*if (selectedExceptions.length > 0) {
+    if (selectedExceptions.length > 0) {
       const payload: any = {
         code: selectedExceptions,
         userName: assignee
@@ -112,9 +141,13 @@ export class ClaimDetailsComponent implements OnInit {
         'No exception is selected.'
       );
       this.isLoadingAuthExc = false;
-    }*/
+    }
   }
 
+  /**
+   * The `fetchClaimTransactionDetails` function fetches claim transaction details, processes the data, and handles any
+   * errors that may occur.
+   */
   fetchClaimTransactionDetails() {
     this.claimsService.getClaimsTransactionsDetails(this.selectedSpringTickets?.ticket?.claimNo)
       .subscribe({
@@ -126,6 +159,7 @@ export class ClaimDetailsComponent implements OnInit {
           if (this.claimTransaction) {
             this.fetchClaimPaymentsTransactionDetails();
             this.fetchClaimExceptions();
+            this.fetchClaimRevision();
             this.localStorageService.setItem('claimTransactionDetails', this.claimTransaction);
           }
           log.info('claim transaction data>>', this.claimTransactionData, this.claimTransaction);
@@ -137,6 +171,10 @@ export class ClaimDetailsComponent implements OnInit {
       })
   }
 
+  /**
+   * The function `fetchClaimPaymentsTransactionDetails` fetches claim payment transaction details and handles loading and
+   * error states.
+   */
   fetchClaimPaymentsTransactionDetails() {
     this.isLoadingTransactionData = true;
     this.claimsService.getClaimsPaymentTransactionsDetails(this.selectedSpringTickets?.ticket?.claimNo, this.claimTransaction?.claimVoucherCode)
@@ -159,6 +197,10 @@ export class ClaimDetailsComponent implements OnInit {
   ngOnDestroy(): void {
   }
 
+  /**
+   * The `makeReady` function  makes a claim transaction ready and displays success or error messages
+   * accordingly.
+   */
   makeReady() {
     this.isLoadingMakeUndo = true;
     const assignee = this.authService.getCurrentUserName();
@@ -181,5 +223,117 @@ export class ClaimDetailsComponent implements OnInit {
             this.isLoadingMakeUndo = false;
           }
         })
+  }
+
+  /**
+   * The function fetches completion remarks data from a service and logs it, displaying an error message if there is an
+   * error.
+   */
+  fetchCompletionRemarks() {
+    this.completionRemarksService.getCompletionRemarks()
+      .subscribe({
+        next: (data) => {
+          this.completionRemarksData = data._embedded;
+          log.info('completionRemarks>>', this.completionRemarksData);
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.message);
+        }
+      })
+  }
+
+  /**
+   * The `fetchClaimRevision` fetches claim revision details using the `claimsService` and displays error messages
+   * if any.
+   */
+  fetchClaimRevision() {
+    this.claimsService.getClaimRevisionDetails(this.selectedSpringTickets?.ticket?.claimNo, this.claimTransaction?.transactionNo)
+      .subscribe({
+        next: (data) => {
+          this.claimRevisionData = data.embedded[0];
+          log.info('revision data>>', this.claimRevisionData);
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.message);
+        }
+      })
+  }
+
+  /**
+   * The function `getAuthorizationLevels` retrieves policy authorization levels based on the selected Spring ticket's
+   * policy code.
+   */
+  getAuthorizationLevels() {
+    this.policiesService.getPolicyAuthorizationLevels(this.selectedSpringTickets?.ticket?.policyCode)
+      .pipe(
+        untilDestroyed(this),
+      )
+      .subscribe({
+        next: (data) => {
+          this.authorizationLevelsData = data.embedded && data.embedded.length > 0 ? data.embedded[0] : [];
+          log.info('AuthorizationLevels>>', this.authorizationLevelsData);
+        },
+      })
+  }
+
+  /**
+   * The function `authorizeAuthorizationLevels` authorizes a selected authorization level and displays success or error
+   * messages accordingly.
+   */
+  authorizeAuthorizationLevels() {
+    const selectedAuthLevel = this.selectedAuthorizationLevel;
+    log.info("select", this.selectedAuthorizationLevel);
+
+    this.policiesService.authorizeAuthorizationLevels(selectedAuthLevel[0]?.code)
+      .subscribe({
+        next: (data) => {
+          this.authorizationLevelsData = data;
+          this.globalMessagingService.displaySuccessMessage('Success', 'Successfully authorized level');
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.error.message);
+        }
+      })
+  }
+
+  /**
+   * The `authorizeClaim` function authorizes a claim by sending a payload to a service and displaying
+   * success or error messages accordingly.
+   */
+  authorizeClaim() {
+    this.isLoadingAuthClaim = true;
+    const assignee = this.authService.getCurrentUserName();
+    const payload: any = {
+      claimNo: this.selectedSpringTickets?.ticket?.claimNo,
+      claimRevisionCode: this.claimRevisionData[0]?.code,
+      ignoreClaimants: "N",
+      transactionNo: this.claimTransaction?.transactionNo,
+      transactionType: this.claimTransaction?.transactionCode,
+      user: assignee
+    }
+    log.info('pay', payload);
+    this.claimsService.authorizeClaims(payload)
+      .subscribe({
+        next: (data) => {
+
+          this.globalMessagingService.displaySuccessMessage('Success', 'Successfully authorized claim');
+          this.isLoadingAuthClaim = false;
+        },
+        error: err => {
+          this.globalMessagingService.displayErrorMessage('Error', err.error.message);
+          this.isLoadingAuthClaim = false;
+        }
+      })
+  }
+
+  /**
+   * The `toggleDropdown` function toggles the visibility of a dropdown based on the exception parameter.
+   */
+  toggleDropdown(exception: any) {
+    if (this.showDropdownForException === exception) {
+      this.showDropdownForException = null; // Hide the dropdown if already shown
+    } else {
+      this.showDropdownForException = exception; // Show the dropdown for the clicked exception
+    }
   }
 }
