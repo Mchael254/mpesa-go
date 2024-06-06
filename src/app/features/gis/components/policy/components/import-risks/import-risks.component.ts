c608d3f1090857e54e428cae170bbe8218212729
faithwasike <faith.wasike@turnkeyafrica.com>
2024-06-06 04:42:01 +0300

    VQ-1983:Ability to upload risks' csv template

--- src/app/features/gis/components/policy/components/import-risks/import-risks.component.ts
+++ src/app/features/gis/components/policy/components/import-risks/import-risks.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as Papa from 'papaparse';
+import { GlobalMessagingService } from '../../../../../../shared/services/messaging/global-messaging.service';
 @Component({
   selector: 'app-import-risks',
   templateUrl: './import-risks.component.html',
@@ -7,7 +8,16 @@ import * as Papa from 'papaparse';
 })
 export class ImportRisksComponent {
 
+  fileSelected: boolean = false;
+  uploadedFileName: string = '';
+  uploading: string = '';
+  importedRisk:any;
 
+  constructor(
+    public globalMessagingService: GlobalMessagingService,
+  ){
+
+  }
 
   downloadCSVTemplate(): void {
     console.log("TEST")
@@ -20,5 +30,61 @@ export class ImportRisksComponent {
     link.click();
     document.body.removeChild(link);
   }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+
+      this.uploading = 'progress';
+
+      Papa.parse(file, {
+        complete: (result: any) => {
+         
+          const expectedHeaders = ['clientId', 'clientName', 'WEF','WET', 'riskDuplicated', 'Class','coverType', 'Premium', 'Taxes'];
+          const actualHeaders = result.meta.fields;
+          console.log(actualHeaders)
+          // Assuming CSV has header row, you can access data with result.data
+          if (this.validateHeaders(expectedHeaders, actualHeaders)) {
+            this.importedRisk = result.data;
+            console.log(result.data);
   
+            try {
+              this.uploadedFileName = file.name;
+              sessionStorage.setItem('uploadedFileName', this.uploadedFileName);
+              this.uploading = 'success';
+            } catch (e) {
+              console.log(`file upload failed >>> `, e);
+              this.uploading = 'error';
+            }
+          }else {
+            this.globalMessagingService.displayErrorMessage(
+              'Error',
+              'CSV headers do not match the required format.'
+            );
+          
+            this.uploading = 'error';
+          }
+         
+        },
+        header: true // Set to true if CSV file has a header row
+      });
+    }else{
+      this.fileSelected = false;
+      this.uploadedFileName = '';
+      this.uploading = '';
+    }
+  }
+  validateHeaders(expectedHeaders: string[], actualHeaders: string[]): boolean {
+    if (expectedHeaders.length !== actualHeaders.length) {
+      return false;
+    }
+  
+    for (let i = 0; i < expectedHeaders.length; i++) {
+      if (expectedHeaders[i] !== actualHeaders[i]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
 }
