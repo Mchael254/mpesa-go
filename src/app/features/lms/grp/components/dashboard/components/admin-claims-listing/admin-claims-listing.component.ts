6fc2dbd662595077a96c03f47c402271c0db1f1f
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-06-14 12:55:29 +0300

    VQ-1945, VQ-1978/Policies, pension summary and listing API consumption

--- src/app/features/lms/grp/components/dashboard/components/admin-claims-listing/admin-claims-listing.component.ts
+++ src/app/features/lms/grp/components/dashboard/components/admin-claims-listing/admin-claims-listing.component.ts
@@ -2,7 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SelectItem } from 'primeng/api';
 import { BreadCrumbItem } from 'src/app/shared/data/common/BreadCrumbItem';
+import { DashboardService } from '../../services/dashboard.service';
+import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
+import { Logger } from 'src/app/shared/services';
+import { ClaimsListingDTO } from '../../models/admin-policies';
 
+const log = new Logger("AdminClaimsListingComponent")
+@AutoUnsubscribe
 @Component({
   selector: 'app-admin-claims-listing',
   templateUrl: './admin-claims-listing.component.html',
@@ -12,14 +18,18 @@ export class AdminClaimsListingComponent implements OnInit, OnDestroy {
   columnOptions: SelectItem[];
   selectedColumns: string[];
   breadCrumbItems: BreadCrumbItem[] = [];
+  clientCode: number = 2422853;
+  claimListing: ClaimsListingDTO[];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private dashboardService: DashboardService,
   ) { }
 
   ngOnInit(): void {
     this.populateBreadCrumbItems();
     this.policyListingColumns();
+    this.getClaimsListing();
 
   }
 
@@ -27,44 +37,7 @@ export class AdminClaimsListingComponent implements OnInit, OnDestroy {
 
   }
 
-  claimListing = [
-    {
-      product: "Health Insurance",
-      claim_number: "CLM123456",
-      member: "John Doe",
-      mem_no: "M12345",
-      claim_amnt: 1200.50
-    },
-    {
-      product: "Life Insurance",
-      claim_number: "CLM123457",
-      member: "Jane Smith",
-      mem_no: "M12346",
-      claim_amnt: 2500.75
-    },
-    {
-      product: "Auto Insurance",
-      claim_number: "CLM123458",
-      member: "James Brown",
-      mem_no: "M12347",
-      claim_amnt: 980.00
-    },
-    {
-      product: "Home Insurance",
-      claim_number: "CLM123459",
-      member: "Emily Davis",
-      mem_no: "M12348",
-      claim_amnt: 4300.60
-    },
-    {
-      product: "Travel Insurance",
-      claim_number: "CLM123460",
-      member: "Michael Johnson",
-      mem_no: "M12349",
-      claim_amnt: 600.20
-    }
-  ];
-
+  
   populateBreadCrumbItems(): void {
     this.breadCrumbItems = [
       { label: 'Dashboard', url: '/home/lms/grp/dashboard/admin' },
@@ -83,6 +56,13 @@ export class AdminClaimsListingComponent implements OnInit, OnDestroy {
     ];
 
     this.selectedColumns = this.columnOptions.map(option => option.value);
+  }
+
+  getClaimsListing() {
+    this.dashboardService.getClaimsListing(this.clientCode).subscribe((res: ClaimsListingDTO[]) => {
+      this.claimListing = res;
+      log.info("getClaimsListing", this.claimListing)
+    });
   }
 
 }
