7e48ccdbc314c692922a6212a835451c8747fd69
christine aoko <christine.aoko@turnkeyafrica.com>
2024-06-24 18:31:32 +0300

    VQ-2166: Investigation Details Screen

--- src/app/features/lms/ind/components/claims/components/claims-investigation/claims-investigation.component.ts
+++ src/app/features/lms/ind/components/claims/components/claims-investigation/claims-investigation.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import stepData from '../../data/steps.json';
 
 @Component({
   selector: 'app-claims-investigation',
   templateUrl: './claims-investigation.component.html',
   styleUrls: ['./claims-investigation.component.css']
 })
-export class ClaimsInvestigationComponent {
 
+export class ClaimsInvestigationComponent implements OnInit, OnDestroy {
+  steps = stepData;
+ 
+  constructor () {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {} 
 }
