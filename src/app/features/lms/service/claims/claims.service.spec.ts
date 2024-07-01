9bdaa198f6661ed2a8ec4e8e7c33ea25b80e285c
komenAnna <komenriana@gmail.com>
2024-07-01 08:23:20 +0300

    VQ-2165-B: VQ-1610 API consumption

--- src/app/features/lms/service/claims/claims.service.spec.ts
+++ src/app/features/lms/service/claims/claims.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed } from '@angular/core/testing';
 
-import { ClaimsService } from "./claims.service";
+import { ClaimsService } from './claims.service';
 
-describe ('ClaimsService', () => {
-    let service: ClaimsService;
+describe('ClaimsService', () => {
+  let service: ClaimsService;
 
-    beforeEach (() => {
-        TestBed.configureTestingModule ({});
-        service = TestBed.inject (ClaimsService);
-    });
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ClaimsService);
+  });
 
-    it('should be created', () => {
-        expect (service).toBeTruthy();
-    });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
