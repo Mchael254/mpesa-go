046f08d97d4496638839975e86f1f2b401a46ec4
christine aoko <christine.aoko@turnkeyafrica.com>
2024-07-04 16:07:34 +0300

    VQ-1607: Fetching Claims Causation

---
+++ src/app/features/lms/ind/components/claims/models/causation-causes.ts
@@ -0,0 +1,9 @@
+export interface CausationCausesDTO {
+    caus_code: number,
+    caus_sht_desc: string,
+    caus_desc: string,
+    caus_type: string,
+    csc_code : number,
+    ddc_code : number,
+    ddc_desc : string,  
+}
