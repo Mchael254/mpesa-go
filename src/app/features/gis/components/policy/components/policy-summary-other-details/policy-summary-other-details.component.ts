73dbbc12349f0cf57e18d5f2b6b975fe014c2633
faithwasike <faith.wasike@turnkeyafrica.com>
2024-07-26 07:14:26 +0300

    VQ-2356:Ability to view risk perils

--- src/app/features/gis/components/policy/components/policy-summary-other-details/policy-summary-other-details.component.ts
+++ src/app/features/gis/components/policy/components/policy-summary-other-details/policy-summary-other-details.component.ts
@@ -133,6 +133,7 @@ export class PolicySummaryOtherDetailsComponent {
 
 
 
+
   @ViewChild('dt1') dt1: Table | undefined;
   @ViewChild('dt2') dt2: Table | undefined;
 
@@ -171,6 +172,7 @@ export class PolicySummaryOtherDetailsComponent {
   public isRequiredDocDetailOpen = false;
   public isCertificatesDetailOpen = false;
 
+  public riskPerils = false;
 
   ngOnInit(): void {
     this.getUtil();
@@ -1196,6 +1198,10 @@ openPremiumDeleteModal() {
     document.getElementById("openModalPremiumButtonDelete").click();
 
   }
+}
+
+toggleRiskPerils(){
+  this.riskPerils = !this.riskPerils
 }
 
 
