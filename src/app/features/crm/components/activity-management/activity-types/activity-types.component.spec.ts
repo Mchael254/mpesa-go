b1975a4646ba69ba1700cd05fc47661696a248ec
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-08-09 11:28:13 +0300

    Activity management screens

---
+++ src/app/features/crm/components/activity-management/activity-types/activity-types.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActivityTypesComponent } from './activity-types.component';
+
+describe('ActivityTypesComponent', () => {
+  let component: ActivityTypesComponent;
+  let fixture: ComponentFixture<ActivityTypesComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ActivityTypesComponent]
+    });
+    fixture = TestBed.createComponent(ActivityTypesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
