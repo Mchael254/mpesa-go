00fd2b7853a65306e67a8fd295562947fc870823
John Gachoki <33230732+john-dylan@users.noreply.github.com>
2024-08-09 11:27:31 +0300

    campaign management mandatory fields and analytics metrics

--- src/app/features/crm/components/campaigns/client-attributes/client-attributes.component.ts
+++ src/app/features/crm/components/campaigns/client-attributes/client-attributes.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MandatoryFieldsService} from "../../../../../shared/services/mandatory-fields/mandatory-fields.service";
+import {GlobalMessagingService} from "../../../../../shared/services/messaging/global-messaging.service";
+import {untilDestroyed} from "../../../../../shared/services/until-destroyed";
 
 @Component({
   selector: 'app-client-attributes',
@@ -12,13 +16,80 @@ export class ClientAttributesComponent implements OnInit {
 
   editMode: boolean = false;
 
-  constructor() {
+  createClientAttributesForm: FormGroup;
 
+  visibleStatus: any = {
+    clientAttributeName: 'Y',
+    clientAttributeDescription: 'Y',
+    clientAttributePrompt: 'Y',
+    clientAttributeRange: 'Y',
+    clientAttribute: 'Y',
   }
+
+  groupIdClientAttributes: string = 'campaignClientAttributesTab';
+
+  constructor(
+    private fb: FormBuilder,
+    private mandatoryFieldsService: MandatoryFieldsService,
+    private globalMessagingService: GlobalMessagingService,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
+    this.campaignDefinitionCreateForm();
   }
 
+  /**
+   * The function `campaignDefinitionCreateForm` creates a form for client attributes, sets validators
+   * for mandatory fields, and updates the form's visibility based on the response from a service call.
+   */
+  campaignDefinitionCreateForm() {
+    this.createClientAttributesForm = this.fb.group({
+      clientAttributeName: [''],
+      clientAttributeDescription: [''],
+      clientAttributePrompt: [''],
+      clientAttributeRange: [''],
+      clientAttribute: ['']
+    });
+    this.mandatoryFieldsService.getMandatoryFieldsByGroupId(this.groupIdClientAttributes).pipe(
+      untilDestroyed(this)
+    )
+      .subscribe((response) =>{
+        response.forEach((field) =>{
+          for (const key of Object.keys(this.createClientAttributesForm.controls)) {
+            this.visibleStatus[field.frontedId] = field.visibleStatus;
+            if (field.visibleStatus === 'Y') {
+              if (key === field.frontedId && field.mandatoryStatus === 'Y'){
+                this.createClientAttributesForm.controls[key].addValidators(Validators.required);
+                this.createClientAttributesForm.controls[key].updateValueAndValidity();
+                const label = document.querySelector(`label[for=${field.frontedId}]`);
+                if (label) {
+                  const asterisk = document.createElement('span');
+                  asterisk.innerHTML = ' *';
+                  asterisk.style.color = 'red';
+                  label.appendChild(asterisk);
+                }
+              }
+            }
+          }
+        })
+        this.cdr.detectChanges();
+      });
+  }
+
+  /**
+   * The function returns the controls of a form named createClientAttributesForm.
+   * @returns The function `f()` is returning the controls of the `createClientAttributesForm` form
+   * group.
+   */
+  get f() {
+    return this.createClientAttributesForm.controls;
+  }
+
+  /**
+   * The function `openDefineClientAttributesModal` displays a modal for defining client attributes if
+   * it exists in the DOM.
+   */
   openDefineClientAttributesModal() {
     const modal = document.getElementById('campaignClientAttribute');
     if (modal) {
@@ -27,6 +98,10 @@ export class ClientAttributesComponent implements OnInit {
     }
   }
 
+  /**
+   * The function `closeDefineClientAttributesModal` sets `editMode` to false and hides the modal with
+   * the id 'campaignClientAttribute'.
+   */
   closeDefineClientAttributesModal() {
     this.editMode = false;
     const modal = document.getElementById('campaignClientAttribute');
@@ -35,4 +110,15 @@ export class ClientAttributesComponent implements OnInit {
       modal.style.display = 'none';
     }
   }
+
+  /**
+   * The function `editClientAttribute` toggles the edit mode and opens a modal for defining client
+   * attributes.
+   */
+  editClientAttribute() {
+    this.editMode = !this.editMode;
+    this.openDefineClientAttributesModal();
+  }
+
+  ngOnDestroy(): void {}
 }
--- src/app/features/crm/components/campaigns/product-attributes/product-attributes.component.ts
+++ src/app/features/crm/components/campaigns/product-attributes/product-attributes.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {untilDestroyed} from "../../../../../shared/services/until-destroyed";
+import {MandatoryFieldsService} from "../../../../../shared/services/mandatory-fields/mandatory-fields.service";
+import {GlobalMessagingService} from "../../../../../shared/services/messaging/global-messaging.service";
 
 @Component({
   selector: 'app-product-attributes',
@@ -14,13 +18,131 @@ export class ProductAttributesComponent implements OnInit{
 
   editMode: boolean = false;
 
+  createProductAttributesForm: FormGroup;
+  createProductForm: FormGroup;
 
-  constructor() {
+  visibleStatus: any = {
+    selectAttribute: 'Y',
+    productAttributeCheck: 'Y',
+    minValue: 'Y',
+    maxValue: 'Y',
+  //
+    sysName: 'Y',
+    productName: 'Y',
+    shortDescription: 'Y',
+    description: 'Y',
+    narration: 'Y'
   }
+  groupIdProductAttributes: string = 'campaignProductAttributesTab';
+
+  constructor(
+    private fb: FormBuilder,
+    private mandatoryFieldsService: MandatoryFieldsService,
+    private globalMessagingService: GlobalMessagingService,
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
+    this.campaignProductAttrCreateForm();
+    this.campaignProductCreateForm();
   }
 
+  /**
+   * The function `campaignProductAttrCreateForm` creates a form with specific attributes and sets
+   * validators based on mandatory fields retrieved from an API response.
+   */
+  campaignProductAttrCreateForm() {
+    this.createProductAttributesForm = this.fb.group({
+      selectAttribute: [''],
+      productAttributeCheck: [''],
+      minValue: [''],
+      maxValue: ['']
+    });
+    this.mandatoryFieldsService.getMandatoryFieldsByGroupId(this.groupIdProductAttributes).pipe(
+      untilDestroyed(this)
+    )
+      .subscribe((response) =>{
+        response.forEach((field) =>{
+          for (const key of Object.keys(this.createProductAttributesForm.controls)) {
+            this.visibleStatus[field.frontedId] = field.visibleStatus;
+            if (field.visibleStatus === 'Y') {
+              if (key === field.frontedId && field.mandatoryStatus === 'Y'){
+                this.createProductAttributesForm.controls[key].addValidators(Validators.required);
+                this.createProductAttributesForm.controls[key].updateValueAndValidity();
+                const label = document.querySelector(`label[for=${field.frontedId}]`);
+                if (label) {
+                  const asterisk = document.createElement('span');
+                  asterisk.innerHTML = ' *';
+                  asterisk.style.color = 'red';
+                  label.appendChild(asterisk);
+                }
+              }
+            }
+          }
+        })
+        this.cdr.detectChanges();
+      });
+  }
+
+  /**
+   * The function `campaignProductCreateForm` creates a form with specific fields, sets validators for
+   * mandatory fields based on response data, and updates the form accordingly.
+   */
+  campaignProductCreateForm() {
+    this.createProductForm = this.fb.group({
+      sysName: [''],
+      productName: [''],
+      shortDescription: [''],
+      description: [''],
+      narration: ['']
+    });
+    this.mandatoryFieldsService.getMandatoryFieldsByGroupId(this.groupIdProductAttributes).pipe(
+      untilDestroyed(this)
+    )
+      .subscribe((response) =>{
+        response.forEach((field) =>{
+          for (const key of Object.keys(this.createProductForm.controls)) {
+            this.visibleStatus[field.frontedId] = field.visibleStatus;
+            if (field.visibleStatus === 'Y') {
+              if (key === field.frontedId && field.mandatoryStatus === 'Y'){
+                this.createProductForm.controls[key].addValidators(Validators.required);
+                this.createProductForm.controls[key].updateValueAndValidity();
+                const label = document.querySelector(`label[for=${field.frontedId}]`);
+                if (label) {
+                  const asterisk = document.createElement('span');
+                  asterisk.innerHTML = ' *';
+                  asterisk.style.color = 'red';
+                  label.appendChild(asterisk);
+                }
+              }
+            }
+          }
+        })
+        this.cdr.detectChanges();
+      });
+  }
+
+  /**
+   * The function returns the controls of a form for creating product attributes.
+   * @returns The `get f()` function is returning the controls of the `createProductAttributesForm`
+   * form group.
+   */
+  get f() {
+    return this.createProductAttributesForm.controls;
+  }
+
+  /**
+   * The function returns the controls of a createProductForm in TypeScript.
+   * @returns The `g()` function is returning the controls of the `createProductForm` form group.
+   */
+  get g() {
+    return this.createProductForm.controls;
+  }
+
+  /**
+   * The function `openDefineProductModal` displays a modal with the id 'campaignClientProduct' by
+   * adding the 'show' class and setting the display style to 'block'.
+   */
   openDefineProductModal() {
     const modal = document.getElementById('campaignClientProduct');
     if (modal) {
@@ -29,6 +151,10 @@ export class ProductAttributesComponent implements OnInit{
     }
   }
 
+  /**
+   * The function `closeDefineProductModal` sets `editMode` to false and hides the modal with the id
+   * 'campaignClientProduct'.
+   */
   closeDefineProductModal() {
     this.editMode = false;
     const modal = document.getElementById('campaignClientProduct');
@@ -38,6 +164,10 @@ export class ProductAttributesComponent implements OnInit{
     }
   }
 
+  /**
+   * The function `openDefineProductAttributesModal` displays a modal for defining product attributes
+   * if it exists in the DOM.
+   */
   openDefineProductAttributesModal() {
     const modal = document.getElementById('campaignClientProductAttributes');
     if (modal) {
@@ -46,6 +176,10 @@ export class ProductAttributesComponent implements OnInit{
     }
   }
 
+  /**
+   * The function `closeDefineProductAttributesModal` sets `editMode` to false and hides the modal with
+   * the id 'campaignClientProductAttributes'.
+   */
   closeDefineProductAttributesModal() {
     this.editMode = false;
     const modal = document.getElementById('campaignClientProductAttributes');
@@ -54,4 +188,23 @@ export class ProductAttributesComponent implements OnInit{
       modal.style.display = 'none';
     }
   }
+
+  /**
+   * The `editProduct` function toggles the edit mode and opens a modal to define a product.
+   */
+  editProduct() {
+    this.editMode = !this.editMode;
+    this.openDefineProductModal();
+  }
+
+  /**
+   * The function `editProductAttribute` toggles the edit mode and opens a modal for defining product
+   * attributes.
+   */
+  editProductAttribute() {
+    this.editMode = !this.editMode;
+    this.openDefineProductAttributesModal();
+  }
+
+  ngOnDestroy(): void {}
 }
