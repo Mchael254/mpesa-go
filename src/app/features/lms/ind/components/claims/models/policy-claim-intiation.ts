da557c7b3a9919fe9b69a116d79c8378a5116555
unknown <rosemary.ogbonna@turnkeyafrica.com>
2024-08-16 08:28:12 +0100

    VQ-2469 API to Initiate a Claim

---
+++ src/app/features/lms/ind/components/claims/models/policy-claim-intiation.ts
@@ -0,0 +1,17 @@
+export interface PolicyClaimIntiation {
+  pol_code: number;
+  clm_date_death_accident: string;
+  place_of_incident: string;
+  clm_exgratia_checked: string;
+  caus_code: number;
+  caus_sht_desc: string;
+  csc_code: number;
+  ddc_code: number;
+  ddc_desc: string;
+  place_of_death: string;
+  caus_type: string;
+  delete_existing_trans: string;
+  clm_pod_code: number;
+  pol_policy_no: string;
+  prp_code: number;
+}
