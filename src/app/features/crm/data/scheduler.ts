84abe2cee60afe21f6527cbfe07c73df9b5528a9
kimiti-R <raphael.kimiti@turnkeyafrica.com>
2024-08-20 16:32:25 +0300

    VQ-2536-n-VQ-2537:: Scheduled Occurrence Modal

--- src/app/features/crm/data/scheduler.ts
+++ src/app/features/crm/data/scheduler.ts
@@ -32,3 +32,43 @@ export interface ScheduledJobsDto {
   thresholdType: string;
   thresholdValue: number;
 }
+
+export interface ScheduledJobsPostDto {
+  code: number;
+  description: string;
+  endTime: string;
+  failNotifyTemplate: number;
+  jobAssignee: number;
+  jobName: string;
+  jobTemplate: number;
+  jobType: string;
+  nextFireTime: string;
+  notifiedFailUser: number;
+  notifiedSuccUser: number;
+  prevFireTime: string;
+  recurrence: string;
+  recurrenceInterval: string;
+  recurrenceType: string;
+  startTime: string;
+  status: string;
+  succNotifyTemplate: number;
+  sysCode: number;
+  thresholdType: string;
+  thresholdValue: number;
+}
+
+export interface Recurrence {
+  type: string;
+  interval: number;
+  daysOfWeek?: string[];
+  dayOfMonthMonthly?: number;
+  repeatOnTheMonthly?: string;
+  monthlyDays?: string[];
+  yearlyDays?: string[];
+  yearlyMonths?: string[];
+  dayOfMonthYearly?: number;
+  repeatOnTheYearly?: string;
+  end: string;
+  endOn?: string;
+  occurrences?: number;
+}
