4cea01caaa5d7cbf31384f431dc9c79d3429eb8a
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-09-06 18:47:30 +0300

    VQ-2756/Claim Processing, admission  API consumption

--- src/app/features/lms/grp/components/claims/components/claim-admission/claim-admission.component.ts
+++ src/app/features/lms/grp/components/claims/components/claim-admission/claim-admission.component.ts
@@ -6,7 +6,9 @@ import { ClaimsService } from '../../service/claims.service';
 import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
 import { Logger } from 'src/app/shared/services';
 import { ClaimDetailsDTO } from '../../models/claim-models';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { untilDestroyed } from 'src/app/shared/shared.module';
+import { SessionStorageService } from 'src/app/shared/services/session-storage/session-storage.service';
 
 const log = new Logger("ClaimAdmissionComponent");
 @AutoUnsubscribe
@@ -27,12 +29,13 @@ export class ClaimAdmissionComponent implements OnInit, OnDestroy {
     private fb: FormBuilder,
     private claimsService: ClaimsService,
     private cdr: ChangeDetectorRef,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private session_storage: SessionStorageService,
+    private router: Router
   ) {}
   
   ngOnInit(): void {
     this.getParams();
-    // this.getClaimDetails();
   }
 
   ngOnDestroy(): void {
@@ -54,22 +57,72 @@ export class ClaimAdmissionComponent implements OnInit, OnDestroy {
     { label: 'Social Security Card', path: 'assets/documents/ssc.pdf', name: 'card.pdf' }
   ];
 
+  formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
   getParams() {
-    this.claimNumber = this.activatedRoute.snapshot.queryParams['claimNumber'];
+    // Get the claim number from the query parameters
+    let claimNumberFromRoute = this.activatedRoute.snapshot.queryParams['claimNumber'];
+  
+    // If the claim number from the route is null, empty, or undefined, use the session storage value
+    this.claimNumber = claimNumberFromRoute || this.session_storage.get('claimNumber');
+  
     this.getClaimDetails();
     this.cdr.detectChanges();
   }
+  
 
   onAdmitClaim() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'claim admitted successfully'
-    });
-    this.isClaimAdmitted = true;
+    if (this.claimDetails && this.claimDetails.length > 0) {
+      const claimDetail = this.claimDetails[0];
+      
+      const admittedClaimDetails = {
+        claim_no: this.claimNumber,
+        age: claimDetail.death_disability_age,
+        date_reported: this.formatDate(new Date(claimDetail.claim_date)),
+        date_of_accident: this.formatDate(new Date(claimDetail.date_of_accident)),
+        caus_code: claimDetail.cause_code,
+        cause_short_description: claimDetail.cause_type,
+        retention: claimDetail.retention_percentage,
+        remarks: claimDetail.remarks,
+        installment_payment: claimDetail.pay_in_installments,
+        date_admitted: this.formatDate(new Date()), // Always today's date
+        user: "ADMIN", //to add user dynamically
+        admitted: "Y",
+      };
+  
+      this.claimsService.admitClaim(this.claimNumber, admittedClaimDetails).pipe(untilDestroyed(this)).subscribe((res) => {
+        if(res) {
+          this.isClaimAdmitted = true;
+
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Claim admitted successfully'
+          });
+          this.cdr.detectChanges();
+        }
+      });
+  
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Warning',
+        detail: 'Claim NOT admitted!'
+      });
+    }
   }
 
   onProcessClaim() {
+    this.router.navigate(['/home/lms/grp/claims/processing'], {
+      queryParams: {
+        claimNumber: this.claimNumber,
+      }
+    });
 
   }
 
