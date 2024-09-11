f0c7c73bd478498e7292aac4496ea8669db5526c
ChristyAoko <christy.aoko@gmail.com>
2024-09-11 16:55:10 +0300

    VQ-1320: Product Selection Screen

---
+++ src/app/features/lms/ind/components/quotation/models/escalation-rate.ts
@@ -0,0 +1,11 @@
+export interface EscalationRateDTO {
+    prod_code: number,
+	pop_code: number,
+	rate: number,
+	div_factor: number,
+	wef: string,
+	wet: string,
+	sa_rate: number,
+	rate_type: null,
+	code: number
+}
