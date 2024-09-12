111706f3759f0fe6632dda4e256e2662d2682111
Tunde Ogunjimi <tundeogunjimi@gmail.com>
2024-09-12 22:32:24 +0100

    VQ-2677: updated chart-service to use tenant API

--- src/app/features/base/services/calendar.service.ts
+++ src/app/features/base/services/calendar.service.ts
@@ -1,43 +1,48 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
-import {AppConfigService} from "../../../core/config/app-config-service";
-import {CalendarEventReqDTO, SaveCalendarEventDTO} from "../../../shared/data/base/dashboard/calendar-dto";
-import {Observable} from "rxjs";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { AppConfigService } from '../../../core/config/app-config-service';
+import {
+  CalendarEventReqDTO,
+  SaveCalendarEventDTO,
+} from '../../../shared/data/base/dashboard/calendar-dto';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalendarService {
-
   baseUrl = this.appConfig.config.contextPath.users_services;
 
-  constructor(
-    private http: HttpClient,
-    private appConfig: AppConfigService
-  ) { }
+  constructor(private http: HttpClient, private appConfig: AppConfigService) {}
 
-  getCalendarEvent(username:string): Observable<CalendarEventReqDTO[]> {
+  getCalendarEvent(username: string): Observable<CalendarEventReqDTO[]> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Accept: 'application/json',
     });
-    const params = new HttpParams()
-      .set('user', `${username}`);
+    const params = new HttpParams().set('user', `${username}`);
 
-    return this.http.get<CalendarEventReqDTO[]>(`/${this.baseUrl}/administration/calendar-activities`, {
-      headers:headers,
-      params:params
-    })
+    return this.http.get<CalendarEventReqDTO[]>(
+      `/${this.baseUrl}/administration/calendar-activities`,
+      {
+        headers: headers,
+        params: params,
+      }
+    );
   }
 
-  saveCalendarEvent(createEventData: SaveCalendarEventDTO): Observable<SaveCalendarEventDTO[]> {
+  saveCalendarEvent(
+    createEventData: SaveCalendarEventDTO
+  ): Observable<SaveCalendarEventDTO[]> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Accept: 'application/json',
     });
 
-    return this.http.post<SaveCalendarEventDTO[]>(`/${this.baseUrl}/administration/calendar-activities`, JSON.stringify(createEventData), {headers:headers})
-
+    return this.http.post<SaveCalendarEventDTO[]>(
+      `/${this.baseUrl}/administration/calendar-activities`,
+      JSON.stringify(createEventData),
+      { headers: headers }
+    );
   }
-
 }
