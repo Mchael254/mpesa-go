e9ec5df63f0a26278b59d75b8be2650db9d7dd90
Tunde Ogunjimi <tundeogunjimi@gmail.com>
2024-09-12 22:32:24 +0100

    VQ-2677: updated chart-service to use tenant API

--- src/app/features/reports-v2/services/report.service.ts
+++ src/app/features/reports-v2/services/report.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {AppConfigService} from "../../../core/config/app-config-service";
-import {Observable} from "rxjs";
-import {ReportV2} from "../../../shared/data/reports/report";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppConfigService } from '../../../core/config/app-config-service';
+import { Observable } from 'rxjs';
+import { ReportV2 } from '../../../shared/data/reports/report';
+import { ApiService } from 'src/app/shared/services/api/api.service';
+import { API_CONFIG } from 'src/environments/api_service_config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportServiceV2 {
-
   constructor(
     private http: HttpClient,
-    private appConfig: AppConfigService
-  ) { }
+    private appConfig: AppConfigService,
+    private apiService: ApiService
+  ) {}
 
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Accept': 'application/json'
+    Accept: 'application/json',
   });
 
   /**
@@ -24,9 +26,14 @@ export class ReportServiceV2 {
    * @param report
    */
   createReport(report: ReportV2): Observable<ReportV2> {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.post<ReportV2>(
-      `/${baseUrl}/chart/chart-reports`, JSON.stringify(report), {headers: this.headers});
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.post<ReportV2>(
+    //   `/${baseUrl}/chart/chart-reports`, JSON.stringify(report), {headers: this.headers});
+    return this.apiService.POST<ReportV2>(
+      `chart-reports`,
+      JSON.stringify(report),
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
 
   /**
@@ -34,84 +41,110 @@ export class ReportServiceV2 {
    * @param id
    */
   getReportById(id: number): Observable<ReportV2> {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.get<ReportV2>(
-      `/${baseUrl}/chart/chart-reports/${id}`, {headers: this.headers});
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.get<ReportV2>(`/${baseUrl}/chart/chart-reports/${id}`, {
+    //   headers: this.headers,
+    // });
+    return this.apiService.GET<ReportV2>(
+      `chart-reports/${id}`,
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
-
 
   /**
    * Get all reports from DB
-   * @param page 
-   * @param folder 
-   * @returns 
+   * @param page
+   * @param folder
+   * @returns
    */
-  getReports(
-    page = 0,
-    folder = null
-  ): Observable<any> {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.get<any>(
-      `/${baseUrl}/chart/chart-reports?page=${page}`, {headers: this.headers});
+  getReports(page = 0, folder = null): Observable<any> {
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.get<any>(`/${baseUrl}/chart/chart-reports?page=${page}`, {
+    //   headers: this.headers,
+    // });
+    return this.apiService.GET<any>(
+      `chart-reports?page=${page}`,
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
 
   findUserById(id: number): Observable<any> {
     const baseUrl = this.appConfig.config.contextPath.users_services;
-    return this.http.get<any>(
-      `/${baseUrl}/administration/users/${id}`, {headers: this.headers});
+    return this.http.get<any>(`/${baseUrl}/administration/users/${id}`, {
+      headers: this.headers,
+    });
   }
 
   updateReport(report: ReportV2): Observable<ReportV2> {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.put<ReportV2>(
-      `/${baseUrl}/chart/chart-reports/${report.id}`, JSON.stringify(report), {headers: this.headers});
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.put<ReportV2>(
+    //   `/${baseUrl}/chart/chart-reports/${report.id}`,
+    //   JSON.stringify(report),
+    //   { headers: this.headers }
+    // );
+    return this.apiService.PUT<ReportV2>(
+      `chart-reports/${report.id}`,
+      JSON.stringify(report),
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
 
   deleteReportCharts(id: number) {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.delete<ReportV2>(
-      `/${baseUrl}/chart/chart-reports/${id}/charts`, {headers: this.headers});
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.delete<ReportV2>(
+    //   `/${baseUrl}/chart/chart-reports/${id}/charts`,
+    //   { headers: this.headers }
+    // );
+    return this.apiService.DELETE<ReportV2>(
+      `charts/${id}/charts`,
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
 
   deleteReport(id: number) {
-    const baseUrl = this.appConfig.config.contextPath.setup_services;
-    return this.http.delete(
-      `/${baseUrl}/chart/chart-reports/${id}`, {headers: this.headers});
+    // const baseUrl = this.appConfig.config.contextPath.setup_services;
+    // return this.http.delete(`/${baseUrl}/chart/chart-reports/${id}`, {
+    //   headers: this.headers,
+    // });
+    return this.apiService.DELETE<string>(
+      `chart-reports/${id}`,
+      API_CONFIG.CHART_SERVICE_BASE_URL
+    );
   }
 
   aiBotQuestion(question: string): Observable<any> {
     return this.http.post<any>(
       // `/${baseUrl}?user_input_area=${question}`, {}
-      `http://192.0.0.183:9090/answer_question?user_input_area=${question}`, {}
-    )
+      `http://192.0.0.183:9090/answer_question?user_input_area=${question}`,
+      {}
+    );
   }
 
   fetchFilterConditions() {
     const metricConditions = [
-      {label: 'Greater than', value: 'gt'},
-      {label: 'Greater than or equal', value: 'gte'},
-      {label: 'Lower than', value: 'lt'},
-      {label: 'Lower than or equal', value: 'lte'},
-      {label: 'Equals', value: 'equals'},
-      {label: 'Not equals', value: 'notEquals'},
-      {label: 'Between', value: 'between'},
+      { label: 'Greater than', value: 'gt' },
+      { label: 'Greater than or equal', value: 'gte' },
+      { label: 'Lower than', value: 'lt' },
+      { label: 'Lower than or equal', value: 'lte' },
+      { label: 'Equals', value: 'equals' },
+      { label: 'Not equals', value: 'notEquals' },
+      { label: 'Between', value: 'between' },
     ];
 
     const dimensionConditions = [
-      {label: 'Starts with', value: 'startsWith'},
-      {label: 'Contains', value: 'contains'},
-      {label: 'Not contains', value: 'notContains'},
-      {label: 'Ends with', value: 'endsWith'},
+      { label: 'Starts with', value: 'startsWith' },
+      { label: 'Contains', value: 'contains' },
+      { label: 'Not contains', value: 'notContains' },
+      { label: 'Ends with', value: 'endsWith' },
     ];
 
-  const dateConditions = [
-      {label: 'In date range', value: 'inDateRange'},
-      {label: 'Not in date range', value: 'notInDateRange'},
-      {label: 'Before date', value: 'beforeDate'},
-      {label: 'After date', value: 'afterDate'},
+    const dateConditions = [
+      { label: 'In date range', value: 'inDateRange' },
+      { label: 'Not in date range', value: 'notInDateRange' },
+      { label: 'Before date', value: 'beforeDate' },
+      { label: 'After date', value: 'afterDate' },
     ];
 
-  return { metricConditions, dimensionConditions, dateConditions };
+    return { metricConditions, dimensionConditions, dateConditions };
   }
-
 }
