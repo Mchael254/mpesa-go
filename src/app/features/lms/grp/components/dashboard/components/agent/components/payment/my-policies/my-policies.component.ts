ccdc7666c693f3aba1c5895b6e28a9a334222832
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2024-10-01 08:33:45 +0300

    VQ-2916/Agent individual policies and Payment UI

--- src/app/features/lms/grp/components/dashboard/components/agent/components/payment/my-policies/my-policies.component.ts
+++ src/app/features/lms/grp/components/dashboard/components/agent/components/payment/my-policies/my-policies.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { BreadCrumbItem } from 'src/app/shared/data/common/BreadCrumbItem';
 
 @Component({
   selector: 'app-my-policies',
   templateUrl: './my-policies.component.html',
   styleUrls: ['./my-policies.component.css']
 })
-export class MyPoliciesComponent {
+export class MyPoliciesComponent implements OnInit, OnDestroy {
+  columnOptions: SelectItem[];
+  selectedColumns: string[];
+  breadCrumbItems: BreadCrumbItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.populateBreadCrumbItems();
+    this.myPolicyListingColumns();
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  populateBreadCrumbItems(): void {
+    this.breadCrumbItems = [
+      { label: 'Dashboard', url: '/home/lms/grp/dashboard/agent' },
+      { label: 'My policies', url: '/home/lms/grp/dashboard/agent-individual-policies' },
+    ];
+  }
+
+  myPolicyListingColumns() {
+    this.columnOptions = [
+      { label: 'Quote no.', value: 'policy_no' },
+      { label: 'Client', value: 'client' },
+      { label: 'Product', value: 'product' },
+      { label: 'Status', value: 'status' },
+      { label: 'Effective date', value: 'effective_date' },
+      { label: 'Premium', value: 'premium' }, 
+      { label: 'Sum assured', value: 'sum_assured' },
+    ];
+  
+    this.selectedColumns = this.columnOptions.map(option => option.value);
+  }
+  
+
+  myPolicyDetails = [
+    {
+      policy_no: 'Q1001',
+      client: 'John Doe',
+      product: 'Life Insurance',
+      status: 'Active',
+      effective_date: '2024-01-01',
+      premium: 4500,
+      sum_assured: 1000000
+    },
+    {
+      policy_no: 'Q1002',
+      client: 'Jane Smith',
+      product: 'Health Insurance',
+      status: 'Pending',
+      effective_date: '2023-12-15',
+      premium: 3500,
+      sum_assured: 500000
+    },
+    {
+      policy_no: 'Q1003',
+      client: 'XYZ Corporation',
+      product: 'Corporate Health Insurance',
+      status: 'Active',
+      effective_date: '2023-10-25',
+      premium: 12000,
+      sum_assured: 3000000
+    },
+    {
+      policy_no: 'Q1004',
+      client: 'Emily Brown',
+      product: 'Auto Insurance',
+      status: 'Lapsed',
+      effective_date: '2023-11-10',
+      premium: 800,
+      sum_assured: 150000
+    },
+    {
+      policy_no: 'Q1005',
+      client: 'Michael Green',
+      product: 'Travel Insurance',
+      status: 'Expired',
+      effective_date: '2023-09-01',
+      premium: 600,
+      sum_assured: 50000
+    }
+  ];
+  
 
 }
--- src/app/features/lms/grp/components/dashboard/components/agent/components/payment/policy-summary/policy-summary.component.ts
+++ src/app/features/lms/grp/components/dashboard/components/agent/components/payment/policy-summary/policy-summary.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-policy-summary',
   templateUrl: './policy-summary.component.html',
   styleUrls: ['./policy-summary.component.css']
 })
-export class PolicySummaryComponent {
+export class PolicySummaryComponent implements OnInit, OnDestroy {
+  selectedOption: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  policySummary = {
+    policyNumber: 'PN1234567',
+    clientName: 'Jane Doe',
+    product: 'Life Insurance',
+    coverageDuration: '20 Years',
+    covers: 'Death, Total Disability',
+    frequencyOfPayment: 'Monthly',
+    coverFrom: '01-Jan-2024',
+    coverTo: '31-Dec-2044',
+    sumAssured: '$200,000',
+    premium: '$1,500',
+    commission: '$150'
+};
+
+selectPaymentOption(option: string) {
+  this.selectedOption = option;
+}
+
+showPushPopUpModal() {
+  const modal = document.getElementById('pushPopUpModal');
+  if (modal) {
+    modal.classList.add('show');
+    modal.style.display = 'block';
+  }
+}
+
+closePushPopUpModal() {
+  const modal = document.getElementById('pushPopUpModal');
+  if (modal) {
+    modal.classList.remove('show')
+    modal.style.display = 'none';
+  }
+}
 
 }
