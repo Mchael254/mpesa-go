48726c8bfbc55b66decefeaafd0646cd8d560e7d
franklinenthamburi <frankline.nthamburi@turnkeyafrica.com>
2025-01-08 18:01:36 +0300

    added optional fields in currencyDTO

--- src/app/shared/data/common/currency-dto.ts
+++ src/app/shared/data/common/currency-dto.ts
@@ -1,19 +1,23 @@
 export interface CurrencyDTO {
   decimalWord: string,
+  currencyDefault?:'Y' | 'N' ,
   id: number,
   name: string,
+  nameAndSymbol?: string,
   numberWord: string,
   roundingOff: number,
   symbol: string
 }
 
 export interface CurrencyRateDTO {
+  baseCurrency?: string,
   baseCurrencyId: number,
   date: string,
   id: number,
   organizationId: number,
   rate: number,
   targetCurrencyId: number,
+  targetCurrency?: string,
   withEffectFromDate: string,
   withEffectToDate: string
 }
