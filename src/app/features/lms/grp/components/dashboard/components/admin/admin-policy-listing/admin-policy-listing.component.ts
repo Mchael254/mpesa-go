724f69c81eda8452fd261f0f08a88813c5d51c58
benard.kipkemoi <benard.kipkemoi@turnkeyafrica.com>
2025-01-09 15:37:33 +0300

    Admin policy listing, pension listing and pension summary unit tests

--- src/app/features/lms/grp/components/dashboard/components/admin/admin-policy-listing/admin-policy-listing.component.ts
+++ src/app/features/lms/grp/components/dashboard/components/admin/admin-policy-listing/admin-policy-listing.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { BreadCrumbItem } from 'src/app/shared/data/common/BreadCrumbItem';
+import { BreadCrumbItem } from '../../../../../../../../shared/data/common/BreadCrumbItem';
 import { DashboardService } from '../../../services/dashboard.service';
-import { Logger } from 'src/app/shared/services';
-import { AutoUnsubscribe } from 'src/app/shared/services/AutoUnsubscribe';
+import { AutoUnsubscribe } from '../../../../../../../../shared/services/AutoUnsubscribe';
+import { Logger } from '../../../../../../../../shared/services';
 import { PoliciesListingDTO } from '../../../models/admin-policies';
 
 const log = new Logger("AdminPolicyListingComponent")
