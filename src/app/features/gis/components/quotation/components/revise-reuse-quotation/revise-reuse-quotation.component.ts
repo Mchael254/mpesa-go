50647fe825583af495bfc81e5f78cbe7e363f7ea
Hope Ibrahim <hope.ibrahim@turnkeyafrica.com>
2025-01-15 19:06:13 +0300

    VQ-3536,VQ-3268,VQ-3603: ABILITY TO VIEW QUOTATION INQUIRY AND QUOTATION REVISION SCREEN

---
+++ src/app/features/gis/components/quotation/components/revise-reuse-quotation/revise-reuse-quotation.component.ts
@@ -0,0 +1,104 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { SharedQuotationsService } from '../../services/shared-quotations.service';
+import { GlobalMessagingService } from 'src/app/shared/services/messaging/global-messaging.service';
+import { Logger, untilDestroyed } from 'src/app/shared/shared.module';
+import { QuotationList, Sources, UserDetails } from '../../data/quotationsDTO';
+import { FormBuilder } from '@angular/forms';
+import { QuotationsService } from 'src/app/features/gis/services/quotations/quotations.service';
+
+const log = new Logger('ReviseReuseQuotationComponent');
+
+@Component({
+  selector: 'app-revise-reuse-quotation',
+  templateUrl: './revise-reuse-quotation.component.html',
+  styleUrls: ['./revise-reuse-quotation.component.css']
+})
+export class ReviseReuseQuotationComponent {
+
+  user: string;
+  userDetails: any;
+  dateFormat: string;
+  selectedDateFrom: string;
+  sourceList: any;
+  sourceDetail: Sources[] = [];
+  gisQuotationList: QuotationList[] = [];
+
+  constructor(
+    public authService: AuthService,
+    public cdr: ChangeDetectorRef,
+    public sharedService: SharedQuotationsService,
+    public globalMessagingService: GlobalMessagingService,
+    public fb: FormBuilder,
+    public quotationService: QuotationsService,
+
+
+
+  ) { }
+
+  ngOnInit(): void {
+    this.getuser();
+    this.loadAllQoutationSources();
+    this.fetchGISQuotations();
+  }
+  ngOnDestroy(): void { }
+
+
+
+  /**
+  * Retrieves user information from the authentication service.
+  * - Sets the 'user' property with the current user's name.
+  * - Sets the 'userDetails' property with the current user's details.
+  * - Logs the user details for debugging purposes.
+  * - Retrieves and sets the 'userBranchId' property with the branch ID from user details.
+  * @method getUser
+  * @return {void}
+  */
+  getuser() {
+    this.user = this.authService.getCurrentUserName();
+    this.userDetails = this.authService.getCurrentUser();
+    log.debug('User Details:', this.userDetails);
+    log.debug('User:', this.user);
+
+    this.dateFormat = this.userDetails?.orgDateFormat;
+    log.debug('Organization Date Format:', this.dateFormat);
+
+  }
+  onDateInputChange(date: any) {
+  }
+  /**
+ * Loads all quotation sources.
+ * - Subscribes to 'getAllQuotationSources' from QuotationService.
+ * - Populates 'sourceList' and assigns 'sourceDetail'.
+ * - Logs source details.
+ * @method loadAllQuotationSources
+ * @return {void}
+ */
+  loadAllQoutationSources() {
+    this.quotationService.getAllQuotationSources().subscribe((data) => {
+      this.sourceList = data;
+      this.sourceDetail = data.content;
+      log.debug(this.sourceDetail, 'Source list');
+      log.debug(this.sourceList, 'Source list');
+    });
+  }
+
+  fetchGISQuotations() {
+    this.quotationService
+      .searchQuotations(0, 10)
+      .pipe(untilDestroyed(this))
+      .subscribe({
+        next: (response: any) => {
+
+          this.gisQuotationList = response._embedded
+          log.debug("LIST OF GIS QUOTATIONS ", this.gisQuotationList);
+
+        },
+        error: (error) => {
+
+          this.globalMessagingService.displayErrorMessage('Error', 'Failed to fetch quotation list. Try again later');
+        }
+      });
+  }
+
+}
