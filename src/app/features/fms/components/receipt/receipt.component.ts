65393de3cecca70971b1a68d1a22591d6e3268c1
franklinenthamburi <frankline.nthamburi@turnkeyafrica.com>
2025-01-24 12:38:12 +0300

    made a correction on allocations table

--- src/app/features/fms/components/receipt/receipt.component.ts
+++ src/app/features/fms/components/receipt/receipt.component.ts
@@ -2195,7 +2195,7 @@ this.receiptingDetailsForm.reset();
     this.isClientSelected = false;
     //this.allocation = false;
     //this.getAllocationStatus = false;
-    //this.isAllocationCompleted = false;
+    this.isAllocationCompleted = false;
 
 // Clear client-related states
 this.selectedClient = null;
@@ -2364,11 +2364,13 @@ GetReceipt(){
       const blob = new Blob([response], {type: blobType});
       this.filePath = window.URL.createObjectURL(blob);
 
+
       // this.fileName = report?.description;
       this.download(this.filePath,'test.pdf');
       //console.log('Report Response>>',response);
       // Disable the print button after successful download
 
+      
          // Reset states
          this.isReceiptSaved = false; // Disable the Print Receipt button
          this.isReceiptDownloading = false;
