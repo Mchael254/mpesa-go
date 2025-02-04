f0c204d318c9382bcb46819a9e4d733d3e352d22
franklinenthamburi <frankline.nthamburi@turnkeyafrica.com>
2025-02-05 02:17:32 +0300

    added routing to new components and registered them in the module file

--- src/app/features/fms/components/base-fms/base-fms.component.ts
+++ src/app/features/fms/components/base-fms/base-fms.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OrganizationDTO } from 'src/app/features/crm/data/organization-dto';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { BranchDTO } from '../../data/receipting-dto';
+import { OrganizationService } from 'src/app/features/crm/services/organization.service';
+import { StaffService } from 'src/app/features/entities/services/staff/staff.service';
+import { GlobalMessagingService } from 'src/app/shared/services/messaging/global-messaging.service';
+import { ReceiptService } from '../../services/receipt.service';
 
 @Component({
   selector: 'app-base-fms',
@@ -6,6 +14,216 @@ import { Component } from '@angular/core';
   styleUrls: ['./base-fms.component.css']
 })
 export class BaseFmsComponent {
+  receiptingDetailsForm: FormGroup;
+    // 1.2 User & Organization and Branch Details
+    loggedInUser: any;
+    GlobalUser: any;
+    organization: OrganizationDTO[];
+    defaultOrgId: number;
+    users:any;
+    branches:BranchDTO[]=[];  
+    organizationId: number;
+    selectedCountryId: number | null = null;
+    defaultCountryId: number;
+    selectedOrganization: string | null = null; // Currently selected organization
+    selectedBranchId:any;
+  defaultBranchId:any;
+  defaultBranchName:string;
+  selectedOrgId:number;
+  //control flags
+  backdatingEnabled = true; // Adjust this based on your logic
+  isNarrationFromLov = false; 
+  isSaveBtnActive=true;
+  isSubmitButtonVisible: boolean = false;
+  ngOnInit(): void {
+    this.captureReceiptForm();
+      this.loggedInUser = this.authService.getCurrentUser();
+      localStorage.setItem('user',this.loggedInUser);
+      
+    
+   
+       this.fetchUserDetails();
+      this.fetchOrganization();
+      
+    
+
+}
+constructor(
+  private authService:AuthService,
+  private fb:FormBuilder,
+  private organizationService:OrganizationService,
+  private staffService:StaffService,
+  private globalMessagingService:GlobalMessagingService,
+  private receiptService:ReceiptService
+
+
+
+){
+  
+}
+captureReceiptForm(){
+  
+  this.receiptingDetailsForm = this.fb.group({
+    selectedBranch:['',Validators.required],
+    organization: ['', Validators.required], // Set default value here as well
+    amountIssued: ['', Validators.required],
+    receiptingPoint:['',Validators.required],
+    openCheque: [''],
+    receiptNumber:['',Validators.required],
+    // receiptNumber: [{ value: '', disabled: true }],
+    ipfFinancier: [''],
+    grossReceiptAmount: [''],
+    receivedFrom: ['', Validators.required],
+    drawersBank: [ '',[Validators.required, Validators.minLength(1)]], // Drawers bank required if not cash
+    receiptDate: ['', Validators.required],
+    narration: ['', [Validators.required, Validators.maxLength(255)]],
+    paymentRef: ['', Validators.required],
+    otherRef: [''],
+    documentDate: ['',Validators.required],
+    manualRef: [''],
+    currency: ['', Validators.required], // Default currency is KES
+    paymentMode: ['', Validators.required],
+    chequeType: [{ value: '', disabled: true }],
+    bankAccount: ['',Validators.required],
+ exchangeRate:['',[Validators.required, Validators.min(0)]],
+      exchangeRates:[''],
+     manualExchangeRate: ['', [Validators.required, Validators.min(0.01)]],
+     
+     charges: ['no', Validators.required],
+     chargeAmount: [ ''],
+     selectedChargeType:['', Validators.required],
+   
+  });
+}
+fetchUserDetails(){
+  this.staffService.getStaffById(this.loggedInUser.code).subscribe({
+    next:(data)=>{
+this.users=data;
+this.GlobalUser=this.users;
+this.organizationId = this.GlobalUser.organizationId;
+localStorage.setItem('OrgId',this.organizationId.toString());
+localStorage.setItem('UserId',this.GlobalUser.id.toString());
+    },
+    error:(err)=>{
+      this.globalMessagingService.displayErrorMessage('Error',err.err.err);
+    }
+  })
+}
+fetchOrganization(){
+  this.organizationService.getOrganization().subscribe({
+    next:(data)=>{
+      this.organization=data;
+     // Set the default organization if it exists
+     
+     const defaultOrg = this.organization.find(org => org.id === 2);
+     if (defaultOrg) {
+    
+       this.selectedOrganization = defaultOrg.name; // Set default organization
+       this.defaultOrgId =  defaultOrg.id;
+       this.defaultCountryId=defaultOrg.country.id;
+      this.fetchBranches(this.defaultOrgId);
+      //this.fetchPayments(this.defaultOrgId);
+      
+       //this.fetchDrawersBanks(this.defaultCountryId);
+       //this.orgId=this.selectedOrganization.name;
+       // Patch the form control with the default organization ID
+       this.receiptingDetailsForm.patchValue({ organization: this.defaultOrgId });
+       //this.receiptingDetailsForm.patchValue({organization:this.selectedOrganization});
+      
+     } else {
+       this.selectedOrganization = null; // Allow user to select from the list
+     }
+
+    },
+    error:(err)=>{
+      this.globalMessagingService.displayErrorMessage('Error',err.error.error);
+    }
+  })
+}
+
+onOrganizationChange(event: any) {
+
+  // Get the selected organization ID
+
+  const selectedOrgId = Number(event.target.value);
+  //console.log('Selected Organization ID:', selectedOrgId);
+
+  if (selectedOrgId) {
+    // Find the selected organization object
+    const selectedOrg = this.organization.find(org => org.id === selectedOrgId);
+    this.selectedOrgId=selectedOrgId;
+    localStorage.setItem('selectedOrgId',this.selectedOrgId.toString());
+    console.log('selected org id>>',this.selectedOrgId);
+    if (selectedOrg && selectedOrg.country) {
+      // Store the country ID
+      this.selectedCountryId = selectedOrg.country.id;
+     // console.log('Selected Country ID:', this.selectedCountryId);
+
+      // Update the form control value
+      this.receiptingDetailsForm.patchValue({
+        organization: selectedOrgId
+      });
+
+      // Fetch branches for the selected organization
+      this.fetchBranches(selectedOrgId);
+      //this.fetchPayments(this.defaultOrgId);
+      //this.fetchPaymentModes(selectedOrgId);
+    
+      // Now you can fetch drawer banks using the country ID
+      if (this.selectedCountryId) {
+       // this.fetchDrawersBanks(this.selectedCountryId);
+      }
+    }
+     // Patch the form control with the default organization ID
+     //this.receiptingDetailsForm.patchValue({ organization: this.defaultOrgId });
+  } else {
+    // Clear dependent fields if no organization is selected
+    this.selectedCountryId = null;
+    this.receiptingDetailsForm.patchValue({
+      selectedBranch: '',
+      drawersBank: ''
+      // ... any other dependent fields
+    });
+    this.branches = []; // Clear branches array
+    //this.drawersBanks = []; // Clear banks array
+  }
+}
+fetchBranches(organizationId: number){
+  this.receiptService.getBranches(organizationId
+  ).subscribe({
+    next:(data)=>{
+      this.branches = data;
+      const defaultBranch= this.branches.find(branch=>branch.id===1);
+      if(defaultBranch){
+        this.defaultBranchName=defaultBranch.name;
+        this.defaultBranchId=defaultBranch.id;
+        
+        this.receiptingDetailsForm.patchValue({ selectedBranch:  this.defaultBranchId });
+      localStorage.setItem('defaultBranchId',this.defaultBranchId.toString());
+      }else{
+        this.defaultBranchName= null;
+      }
+      //this.fetchCurrencies();
+    },
+    error:(err)=>{
+      this.globalMessagingService.displayErrorMessage('Error',err.error.error);
+    }
+  })
+}
+
+onBranchChange(event:any){
+const selectedBranch=event.target.value;
+if(selectedBranch){
+  this.selectedBranchId=selectedBranch;
+  localStorage.setItem('selectedBranch',this.selectedBranchId.toString());
+
+}else{
+  this.selectedBranchId=null;
+}
+// this.fetchAccountTypes(this.GlobalUser.organizationId, this.selectedBranchId,this.GlobalUser.id);
+//this.fetchBanks(this.selectedBranchId,this.defaultCurrencyId);
+
+}
 /* The `items` property is an array of objects. Each object represents a menu item in a CRM (Customer
 Relationship Management) application. Each object has the following properties: */
 GLledger: Array<{
@@ -232,9 +450,7 @@ debtors: Array<{
     ],
   },
 ];
-constructor() {}
 
-ngOnInit(): void {}
 
 /**
  * The function toggles the visibility of sub-items for a given item.
