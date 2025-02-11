adb07a7a6d97bea553d43dd5ab0d9e44d8102258
franklinenthamburi <frankline.nthamburi@turnkeyafrica.com>
2025-02-12 00:40:02 +0300

    redesigned fms receipting screens

--- src/app/features/fms/fms-routing.module.ts
+++ src/app/features/fms/fms-routing.module.ts
@@ -4,6 +4,11 @@ import {ChequeAuthorizationComponent} from "./cheque-authorization/cheque-author
 import { ReceiptComponent } from './components/receipt/receipt.component';
 import { ReceiptAuthorizationComponent } from './components/receipt-authorization/receipt-authorization.component';
 import { BaseFmsComponent } from './components/base-fms/base-fms.component';
+import { ReceiptCaptureComponent } from './components/receipt-capture/receipt-capture.component';
+import { ClientSearchComponent } from './components/client-search/client-search.component';
+import { ClientAllocationComponent } from './components/client-allocation/client-allocation.component';
+
+
 const routes: Routes = [
   {
     path:'',
@@ -20,7 +25,20 @@ const routes: Routes = [
   {
     path:'authorize',
     component:ReceiptAuthorizationComponent
-  }
+  },
+  {
+    path:'client',
+    component:ClientSearchComponent
+  },
+  {
+    path:'screen1',
+    component:ReceiptCaptureComponent
+  },
+  {
+    path:'allocation',
+    component:ClientAllocationComponent
+  },
+ 
 ];
 
 @NgModule({
