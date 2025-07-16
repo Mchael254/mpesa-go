da18f7a8c8cbd4ed0dc3147a13bd6a4e25360dd1
briankipchirchir <kbrianngetich@gmail.com>
2025-07-16 11:51:59 +0300

    edit section popu

--- src/app/features/gis/components/quotation/components/risk-details/risk-details.component.ts
+++ src/app/features/gis/components/quotation/components/risk-details/risk-details.component.ts
@@ -56,6 +56,10 @@ const log = new Logger('RiskClausesDetailsComponent');
 })
 
 export class RiskDetailsComponent {
+freeLimitValue: any;
+getFreeLimitLabel(arg0: any) {
+throw new Error('Method not implemented.');
+}
   @Input() selectedProduct!: any;
   @ViewChild('editSectionModal') editSectionModal!: ElementRef;
   @ViewChild('sectionTable') sectionTable!: Table;
@@ -192,6 +196,13 @@ export class RiskDetailsComponent {
   dynamicRegexPattern: string;
   clientsData: ClientDTO[] = [];
   sectionToDelete: any = null;
+  declaration1: boolean | null = null;
+  declaration2: boolean | null = null;
+  premiumTypes: any[] = [];
+isEditable: boolean = false;
+quotationIsAuthorised: boolean = true; // or false
+
+
 
 
 
@@ -283,6 +294,7 @@ export class RiskDetailsComponent {
       nullable: true,
       align: 'left',
     };
+    this.loadDummyFreeLimit();
     
   }
   ngOnDestroy(): void { }
@@ -292,6 +304,33 @@ export class RiskDetailsComponent {
       keyboard: false
     });
   }
+  
+  
+  setDeclaration1(value: boolean) {
+    this.declaration1 = value;
+    this.riskDetailsForm.get('declarationField')?.setValue(value ? 'Yes' : 'No'); // Optional
+  }
+  
+  setDeclaration2(value: boolean) {
+    this.declaration2 = value;
+    this.riskDetailsForm.get('computeField')?.setValue(value ? 'Yes' : 'No'); // Optional
+  }
+  
+loadDummyFreeLimit() {
+  
+  const dummyResponse = {
+    value: 100000,        
+    editable: false      
+  };
+
+  this.freeLimitValue = dummyResponse.value;
+  this.isEditable = dummyResponse.editable;
+
+  if (this.isEditable) {
+    this.riskDetailsForm.get('freeLimit')?.setValue(this.freeLimitValue);
+  }
+}
+
 
 setSectionToDelete(section: any) {
   this.sectionToDelete = section;
