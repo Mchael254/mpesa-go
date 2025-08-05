0a30f70f5c5e3464c393271fc593e0d812170f51
Tunde Ogunjimi <tundeogunjimi@gmail.com>
2025-08-05 11:21:29 +0100

    VQ-4543: updated unit tests

--- src/app/features/entities/components/entity/view-entity/address/address.component.spec.ts
+++ src/app/features/entities/components/entity/view-entity/address/address.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { AddressComponent } from './address.component';
 import {of} from "rxjs";
@@ -7,7 +7,10 @@ import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {MessageService} from "primeng/api";
 import {ClientService} from "../../../../services/client/client.service";
 import {CountryService} from "../../../../../../shared/services/setups/country/country.service";
-import {CountryDto} from "../../../../../../shared/data/common/countryDto";
+import {CountryDto, PostalCodesDTO, StateDto, TownDto} from "../../../../../../shared/data/common/countryDto";
+import {UtilService} from "../../../../../../shared/services";
+import {GlobalMessagingService} from "../../../../../../shared/services/messaging/global-messaging.service";
+import {ElementRef} from "@angular/core";
 
 const countries: CountryDto[] = [
   {
@@ -31,11 +34,11 @@ const countries: CountryDto[] = [
   drugWetDate: "",
   highRiskWefDate: "",
   highRiskWetDate: "",
-  id: 0,
+  id: 165,
   isShengen: "",
   mobilePrefix: 0,
-  name: "",
-  nationality: "",
+  name: "nigeria",
+  nationality: "nigerian",
   risklevel: "",
   short_description: "",
   subAdministrativeUnit: "",
@@ -47,7 +50,13 @@ const countries: CountryDto[] = [
   zipCode: 0,
   zipCodeString: ""
   }
-]
+];
+
+const states: StateDto[] = [{country: countries[0], id: 1, name: "nigeria", shortDescription: "nig"}];
+
+const towns: TownDto[] = [{country: undefined, id: 3, name: "", shortDescription: "", state: undefined}];
+
+const postalCodes: PostalCodesDTO[] = [{description: "", id: 0, townCode: 0, townName: 0, zipCode: 0}];
 
 describe('AddressComponent', () => {
   let component: AddressComponent;
@@ -58,7 +67,7 @@ describe('AddressComponent', () => {
   }
 
   const mockCountryService = {
-    getCountries: jest.fn().mockReturnValue(of(countries)),
+    getCountries: jest.fn(),
     getMainCityStatesByCountry: jest.fn(),
     getTownsByMainCityState: jest.fn(),
     getPostalCodes: jest.fn()
@@ -68,6 +77,11 @@ describe('AddressComponent', () => {
     getClientTitles: jest.fn().mockReturnValue(of([])),
     updateClient: jest.fn().mockReturnValue(of({}))
   }
+
+  let mockMessagingService = {
+    displaySuccessMessage: jest.fn(),
+    displayErrorMessage: jest.fn()
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -81,6 +95,8 @@ describe('AddressComponent', () => {
         FormBuilder,
         { provide: ClientService, useValue: mockClientService },
         { provide: CountryService, useValue: mockCountryService },
+        { provide: UtilService, useValue: mockUtilService },
+        { provide: GlobalMessagingService, useValue: mockMessagingService },
       ]
     });
     fixture = TestBed.createComponent(AddressComponent);
@@ -282,8 +298,13 @@ describe('AddressComponent', () => {
 
     component.addressDetails = {
       countryId: "165",
-    }
+      physicalAddress: '123 Main St',
+    } as any;
 
+    jest.spyOn(mockCountryService, 'getCountries').mockReturnValue(of(countries));
+    jest.spyOn(mockCountryService, 'getMainCityStatesByCountry').mockReturnValue(of(states));
+    jest.spyOn(mockCountryService, 'getTownsByMainCityState').mockReturnValue(of(towns));
+    jest.spyOn(mockCountryService, 'getPostalCodes').mockReturnValue(of(postalCodes));
 
     fixture.detectChanges();
   });
@@ -291,4 +312,196 @@ describe('AddressComponent', () => {
   test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('should set clientCountry based on addressDetails.countryId', () => {
+    jest.spyOn(mockCountryService, 'getCountries').mockReturnValue(of(countries));
+
+    component.addressDetails = {
+      countryId: 165
+    } as any;
+
+    component.fetchCountries();
+
+    expect(component.clientCountry).toEqual(countries[0]);
+  });
+
+  test('should patch form values correctly', () => {
+    component.editForm = new FormBuilder().group({
+      address: [''],
+      country: [''],
+      county: [''],
+      city: [''],
+      physicalAddress: [''],
+      postalAddress: [''],
+      postalCode: [''],
+      town: [''],
+      road: [''],
+      houseNo: [''],
+    });
+
+    component.clientCountry = { id: 165 } as any;
+    component.clientState = { id: 1 } as any;
+    component.clientTown = { id: 3 } as any;
+
+    component.patchFormValues();
+
+    expect(component.editForm.value.physicalAddress).toBe('123 Main St');
+    expect(component.editForm.value.town).toBe(3);
+  });
+
+  test('should call editButton click, setSelectOptions, and patchFormValues', fakeAsync(() => {
+    const setSelectOptionsSpy = jest.spyOn(component, 'setSelectOptions');
+    const patchFormValuesSpy = jest.spyOn(component, 'patchFormValues');
+
+    component.clientCountry = { id: 165 } as any;
+    component.clientState = { id: 1 } as any;
+    component.clientTown = { id: 3 } as any;
+
+    component.editButton = {
+      nativeElement: {
+        click: jest.fn()
+      }
+    } as any;
+
+    component.openEditAddressDialog();
+
+    expect(component.editButton.nativeElement.click).toHaveBeenCalled();
+    expect(setSelectOptionsSpy).toHaveBeenCalled();
+
+    tick(500); // simulate the 500ms delay
+    expect(patchFormValuesSpy).toHaveBeenCalled();
+  }));
+
+
+  test('should set options for country, county, town, and postalCode fields', () => {
+    component.countries = countries;
+    component.states = states;
+    component.towns = towns;
+    component.postalCodes = postalCodes;
+
+    component.formFieldsConfig = {
+      fields: [
+        { fieldId: 'country', options: [] },
+        { fieldId: 'county', options: [] },
+        { fieldId: 'town', options: [] },
+        { fieldId: 'postalCode', options: [] }
+      ]
+    };
+
+    component.setSelectOptions();
+
+    expect(component.formFieldsConfig.fields.find(f => f.fieldId === 'country')?.options).toEqual(component.countries);
+    expect(component.formFieldsConfig.fields.find(f => f.fieldId === 'county')?.options).toEqual(component.states);
+    expect(component.formFieldsConfig.fields.find(f => f.fieldId === 'town')?.options).toEqual(component.towns);
+    expect(component.formFieldsConfig.fields.find(f => f.fieldId === 'postalCode')?.options).toEqual(component.postalCodes);
+  });
+
+  test('should fetch and set states when a country is selected', () => {
+    const mockStates = states;
+    const mockEvent = { target: { value: 'kenya-id' } };
+
+    // Spy on country service
+    jest.spyOn(mockCountryService, 'getMainCityStatesByCountry').mockReturnValue(of(mockStates));
+
+    // Setup form fields
+    component.formFieldsConfig = {
+      fields: [
+        { fieldId: 'country', options: [] },
+        { fieldId: 'county', options: [] }
+      ]
+    };
+
+    component.processSelectOption(mockEvent, 'country');
+
+    expect(mockCountryService.getMainCityStatesByCountry).toHaveBeenCalledWith('kenya-id');
+    expect(component.formFieldsConfig.fields.find(f => f.fieldId === 'county')?.options).toEqual(mockStates);
+  });
+
+  test('should fetch and set towns when a county is selected', () => {
+    const mockTowns = towns;
+    const mockEvent = { target: { value: 'lagos-id' } };
+
+    // Prepare formFieldsConfig with city and town fields
+    component.formFieldsConfig = {
+      fields: [
+        { fieldId: 'city', options: [] },
+        { fieldId: 'town', options: [] }
+      ]
+    };
+
+    // Spy on getTownsByMainCityState
+    const getTownsSpy = jest
+      .spyOn(mockCountryService, 'getTownsByMainCityState')
+      .mockReturnValue(of(mockTowns));
+
+    component.processSelectOption(mockEvent, 'county');
+
+    expect(getTownsSpy).toHaveBeenCalledWith('lagos-id');
+
+    const cityField = component.formFieldsConfig.fields.find(f => f.fieldId === 'city');
+    const townField = component.formFieldsConfig.fields.find(f => f.fieldId === 'town');
+
+    expect(cityField?.options).toEqual(mockTowns);
+    expect(townField?.options).toEqual(mockTowns);
+  });
+
+  test('should fetch and set postal codes when a city is selected', () => {
+    const mockPostalCodes = postalCodes;
+    const mockEvent = { target: { value: 'ikeja-id' } };
+
+    // Prepare formFieldsConfig with postalCode field
+    component.formFieldsConfig = {
+      fields: [
+        { fieldId: 'postalCode', options: [] }
+      ]
+    };
+
+    // Spy on getPostalCodes
+    const getPostalCodesSpy = jest
+      .spyOn(mockCountryService, 'getPostalCodes')
+      .mockReturnValue(of(mockPostalCodes));
+
+    component.processSelectOption(mockEvent, 'city');
+
+    expect(getPostalCodesSpy).toHaveBeenCalledWith('ikeja-id');
+
+    const postalCodeField = component.formFieldsConfig.fields.find(f => f.fieldId === 'postalCode');
+    expect(postalCodeField?.options).toEqual(mockPostalCodes);
+  });
+
+
+  test('should call updateClient', () => {
+    component.editForm = new FormBuilder().group({
+      country: ['165'],
+      county: ['2'],
+      city: ['3'],
+      postalCode: ['00100'],
+      physicalAddress: ['123 Main St'],
+      postalAddress: ['P.O. Box 123'],
+      road: ['1st Ave'],
+      houseNo: ['12B']
+    });
+
+    component.addressDetails = {
+      id: 100,
+      countryId: 165,
+      stateId: 2,
+      townId: 3
+    };
+
+    component.accountCode = 42;
+
+    component.closeButton = {
+      nativeElement: {
+        click: jest.fn()
+      }
+    } as unknown as ElementRef;
+
+    component.editAddressDetails();
+
+    expect(mockClientService.updateClient).toHaveBeenCalled();
+    expect(mockMessagingService.displaySuccessMessage).toHaveBeenCalled();
+  });
+
+
 });
