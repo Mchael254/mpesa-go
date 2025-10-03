e1eec9f029e5dfe830046e4b8762a441fa954151
franklinenthamburi <frankline.nthamburi@turnkeyafrica.com>
2025-10-03 12:53:47 +0300

    Fix compilation errors and implement new banking process component

--- src/app/features/fms/components/new-banking-process/new-banking-process.component.ts
+++ src/app/features/fms/components/new-banking-process/new-banking-process.component.ts
@@ -1,23 +1,22 @@
-import { GlobalMessagingService } from './../../../../shared/services/messaging/global-messaging.service';
-import { GlobalMessagingService } from './../../../../shared/services/messaging/global-messaging.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { GlobalMessagingService } from './../../../../shared/services/messaging/global-messaging.service';
 import { Logger } from 'src/app/shared/services';
- const log = new Logger('NewBankingProcessComponent');
-import { Logger } from 'src/app/shared/services';
- const log = new Logger('NewBankingProcessComponent');
+
+const log = new Logger('NewBankingProcessComponent');
+
 @Component({
   selector: 'app-new-banking-process',
   templateUrl: './new-banking-process.component.html',
   styleUrls: ['./new-banking-process.component.css'],
 })
-export class NewBankingProcessComponent {
-  bankingForm: FormGroup;
-  
-  
+export class NewBankingProcessComponent implements OnInit {
+  bankingForm!: FormGroup;
+
   paymentMethods = ['mpesa', 'cash', 'bank'];
+
   receiptData: ReceiptDto[] = [
     {
       receiptId: 'RO1',
@@ -36,66 +35,46 @@ export class NewBankingProcessComponent {
       date: new Date('2000/02/24'),
     },
   ];
+
   selectedReceipt!: ReceiptDto;
+
   constructor(
     public translate: TranslateService,
     private fb: FormBuilder,
     private router: Router,
-    private globalMessagingService:GlobalMessagingService,
-    
-  ) {
-   
-  }
-    private router: Router,
-    private globalMessagingService:GlobalMessagingService,
-    
-  ) {
-   
-  }
+    private globalMessagingService: GlobalMessagingService
+  ) {}
+
   ngOnInit() {
     this.createBankingForm();
   }
+
   createBankingForm(): void {
     this.bankingForm = this.fb.group({
       startDate: ['', Validators.required],
       endDate: ['', Validators.required],
       paymentMethod: ['mpesa', Validators.required],
-      paymentMethod: ['mpesa', Validators.required],
     });
   }
-onClickRetrieveRcts(){
-  //  Mark all fields as touched to show any validation errors in the UI
-this.bankingForm.markAllAsTouched();
-  if (!this.bankingForm.valid){
-    this.globalMessagingService.displayErrorMessage("","please fill the required fields");
-    return;
-  }
-}
-onClickRetrieveRcts(){
-  //  Mark all fields as touched to show any validation errors in the UI
-this.bankingForm.markAllAsTouched();
-  if (!this.bankingForm.valid){
-    this.globalMessagingService.displayErrorMessage("","please fill the required fields");
-    return;
-  }
-}
-  navigateToBatch(): void {
-//console.log("the record is",this.selectedReceipt);
-//console.log("the record is",this.selectedReceipt);
-    this.router.navigate(['/home/fms/process-batch']);
-    
-  }
-onRowSelected(event:any):void{
-  const s =(event.target as HTMLInputElement).value;
-console.log("event",s);
-}
 
-}
-export interface ReceiptDto {
-  receiptId: string;
-  date: Date;
-  customer: string;
-  amount: number;
-  collectionAcc: string;
-  assignedTo: string;
+  onClickRetrieveRcts() {
+    this.bankingForm.markAllAsTouched();
+    if (!this.bankingForm.valid) {
+      this.globalMessagingService.displayErrorMessage(
+        '',
+        'Please fill the required fields'
+      );
+      return;
+    }
+    // TODO: implement retrieval logic
+  }
+
+  navigateToBatch(): void {
+    this.router.navigate(['/home/fms/process-batch']);
+  }
+
+  onRowSelected(event: any): void {
+    const s = (event.target as HTMLInputElement).value;
+    console.log('event', s);
+  }
 }
