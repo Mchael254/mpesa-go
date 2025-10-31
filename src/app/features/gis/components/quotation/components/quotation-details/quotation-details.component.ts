302bd4958bd118267ead155d0ff5577e5a67e0b7
Mchael254 <michealotieno729@gmail.com>
2025-10-31 09:46:26 +0300

    new client journey redefined

--- src/app/features/gis/components/quotation/components/quotation-details/quotation-details.component.ts
+++ src/app/features/gis/components/quotation/components/quotation-details/quotation-details.component.ts
@@ -52,6 +52,7 @@ export class QuotationDetailsComponent implements OnInit, OnDestroy {
   @ViewChild(Table) private dataTable: Table;
   @ViewChild('reassignProductModal') reassignProductModalElement!: ElementRef;
   @ViewChild('chooseClientReassignModal') chooseClientReassignModal!: ElementRef;
+  @ViewChild('createClientModal') createClientModalElement!: ElementRef;
 
   private modals: { [key: string]: bootstrap.Modal } = {};
 
@@ -474,10 +475,21 @@ export class QuotationDetailsComponent implements OnInit, OnDestroy {
     this.selectedClientType = value;
     this.newClient = value === 'new';
 
-    this.newClient && (this.isNewClientSelected = true);
-    sessionStorage.setItem('isNewClientSelected', JSON.stringify(this.isNewClientSelected))
-    log.debug("New client status", this.newClient)
+    this.isNewClientSelected = (value === 'new');
+    sessionStorage.setItem('isNewClientSelected', JSON.stringify(this.isNewClientSelected));
+    log.debug("New client status", this.newClient);
   }
+
+  openCreateClientModal() {
+    this.setClientType('new');
+
+    if (this.createClientModalElement) {
+      const modalElement = this.createClientModalElement.nativeElement;
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
   handleSaveClient(eventData: any) {
     log.debug('Event received from Client search comp', eventData);
     const clientCode = eventData.id;
