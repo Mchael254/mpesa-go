712cc7886688f472f0e80ca790f9a8015878305c
Mchael254 <michealotieno729@gmail.com>
2025-11-05 16:13:32 +0300

    reassign quotation

--- src/app/features/gis/components/quotation/services/quotations/quotations.service.ts
+++ src/app/features/gis/components/quotation/services/quotations/quotations.service.ts
@@ -1325,6 +1325,7 @@ export class QuotationsService {
   getSystemsAssignedToUser(userId: number): Observable<any> {
     return this.api.GET<Observable<any>>(`users/${userId}/systems`, API_CONFIG.USER_ADMINISTRATION_SERVICE_BASE_URL)
   }
+
   getAllTickets(): Observable<any> {
     const params = {
       pageNo: 0,
@@ -1339,6 +1340,29 @@ export class QuotationsService {
       params
     );
   }
+
+  reassignTicket(taskId: string, newAssignee: string) {
+    return this.api.PUT<any>(
+      `v1/tickets/reassign?taskId=${taskId}&newAssignee=${newAssignee}`,
+      null,
+      API_CONFIG.GIS_TICKETING_SERVICE
+    ).pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    );
+  }
+
+  getTaskById(quotationCode: string) {
+    return this.api.GET<any>(
+      `v1/process-flow/bySourceCodeAndSystemModule?sourceCode=${quotationCode}&systemModule=Q`,
+      API_CONFIG.GIS_TICKETING_SERVICE
+    ).pipe(
+      retry(1),
+      catchError(this.errorHandl)
+    );
+  }
+
+
 
 
 
