4f631d283c9e9f5326646595d82111a3c1909c37
Tunde Ogunjimi <ogunjimi.tunde@turnkeyafrica.com>
2025-11-25 12:56:08 +0100

    VQ-4960: ability to display intermediary table

--- src/app/features/entities/components/intermediary/list-intermediary/list-intermediary.component.ts
+++ src/app/features/entities/components/intermediary/list-intermediary/list-intermediary.component.ts
@@ -2,19 +2,20 @@ import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {Pagination} from "../../../../../shared/data/common/pagination";
 import {AgentDTO} from "../../../data/AgentDTO";
 import {Observable} from "rxjs";
-import {Logger, untilDestroyed} from "../../../../../shared/shared.module";
+import {Logger, untilDestroyed, UtilService} from "../../../../../shared/shared.module";
 import {LazyLoadEvent} from "primeng/api";
 import {TableLazyLoadEvent} from "primeng/table";
-import {map, tap} from "rxjs/operators";
+import {tap} from "rxjs/operators";
 import {IntermediaryService} from "../../../services/intermediary/intermediary.service";
-import {FormBuilder} from "@angular/forms";
 import {Router} from "@angular/router";
 import {BreadCrumbItem} from "../../../../../shared/data/common/BreadCrumbItem";
-import {TableDetail} from "../../../../../shared/data/table-detail";
+import {TableDetail, TableFieldConfig} from "../../../../../shared/data/table-detail";
 import { NgxSpinnerService } from 'ngx-spinner';
-import { AccountService } from '../../../services/account/account.service';
-import { PartyAccountsDetails } from '../../../data/accountDTO';
-import { SortFilterService } from 'src/app/shared/services/sort-filter.service';
+import {
+  DynamicScreensSetupService
+} from "../../../../../shared/services/setups/dynamic-screen-config/dynamic-screens-setup.service";
+import {DynamicScreenSetupDto} from "../../../../../shared/data/common/dynamic-screens-dto";
+import {GlobalMessagingService} from "../../../../../shared/services/messaging/global-messaging.service";
 
 const log = new Logger('ListIntermediaryComponent');
 
@@ -31,13 +32,13 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
   pageSize = 5;
   isSearching = false;
   searchTerm = '';
-  cols = [
-    { field: 'name', header: 'Name' },
-    { field: 'modeOfIdentity', header: 'Primary ID Type' },
-    { field: 'agentIdNo', header: 'ID Number' },
-    { field: 'primaryType', header: 'Account Type' },
-    { field: 'accountType', header: 'Business Unit' }
-  ];
+
+  columns: TableFieldConfig[];
+  actionLabel: {} = {};
+  columnLabel: {} = {};
+  viewLabel: {} = {};
+
+  columnDialogVisible: boolean = false;
 
   globalFilterFields = ['name','modeOfIdentity', 'agentIdNo', 'accountType'];
   intermediaryBreadCrumbItems: BreadCrumbItem[] = [
@@ -54,25 +55,27 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
       url: '/home/entity/intermediary/list'
     }
   ];
+
   filterObject: {
     name:string, agentIdNo:string, modeOfIdentity:string, primaryType:string, accountType:string
   } = {
     name:'', agentIdNo:'', modeOfIdentity:'', primaryType:'' , accountType:''
   };
 
+  language: string;
+  dynamicSetupData: DynamicScreenSetupDto;
+
   constructor(
     private router: Router,
-    private fb: FormBuilder,
     private intermediaryService: IntermediaryService,
-    // private sortFilterService: SortFilterService,
-    // private accountService: AccountService,
-    private sortFilterService: SortFilterService,
-    private accountService: AccountService,
     private cdr: ChangeDetectorRef,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private utilService: UtilService,
+    private dynamicScreenSetupService: DynamicScreensSetupService,
+    private globalMessagingService: GlobalMessagingService,
   ) {
     this.tableDetails = {
-      cols: this.cols,
+      // cols: this.cols,
       rows: this.intermediaries?.content,
       globalFilterFields: this.globalFilterFields,
       isLazyLoaded: true,
@@ -80,14 +83,19 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
       showFilter: false,
       showSorting: false
     }
+
+    this.utilService.currentLanguage.subscribe(lang => {
+      this.language = lang;
+    });
   }
 
   /**
    * The ngOnInit function initializes the tableDetails object and shows a spinner.
    */
   ngOnInit(): void {
+    this.fetchTableConfig();
     this.tableDetails = {
-      cols: this.cols,
+      // cols: this.cols,
       rows: this.intermediaries?.content,
       globalFilterFields: this.globalFilterFields,
       showFilter: false,
@@ -98,27 +106,61 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
       isLazyLoaded: true,
       viewDetailsOnView: true,
       viewMethod: this.viewDetailsWithId.bind(this),
-    },
+    }
     this.spinner.show();
   }
 
-  viewDetailsWithId(rowId: number) {
-    let partyId: number;
+  fetchTableConfig(): void {
+    this.columns = [];
+    const screenId = 'entities_records_intermediaries';
 
-    // fetch account details to fetch party id before routing to 360 view
-    this.accountService
-      .getAccountDetailsByAccountCode(rowId)
-      .pipe(
-        map((data: PartyAccountsDetails) => {
-            this.accountService.setCurrentAccounts(data); // set this current as current account.
-            return data?.partyId;
-          },
-          untilDestroyed(this)
-        ))
-          .subscribe( (_x) => {
-            partyId = _x;
-            this.router.navigate([ `/home/entity/view/${partyId}`]);
+    this.dynamicScreenSetupService.fetchDynamicSetupByScreen(
+      null,
+      screenId,
+      null,
+      null,
+      null
+      ).subscribe({
+      next: data => {
+        this.dynamicSetupData = data;
+
+        data.fields.forEach(field => {
+          const header = (field.fieldId.split('_'))[2];
+
+          if (field.fieldId.includes('columnLabel')) {
+            this.columnLabel = field.label;
+            return;
+          }
+
+          if (field.fieldId.includes('actionLabel')) {
+            this.actionLabel = field.label;
+            return;
+          }
+
+          if (field.fieldId.includes('viewLabel')) {
+            this.viewLabel = field.label;
+            return;
+          }
+
+          this.columns.push({
+            field: field.fieldId,
+            header,
+            label: field.label,
+            visible: field.visible,
           });
+
+        });
+        log.info('dynamic setup data', data, this.columns);
+      },
+      error: err => {
+        this.globalMessagingService.displayErrorMessage('Error', err.error.message)
+      }
+    })
+
+  }
+
+  viewDetailsWithId(rowId: number, category?: string): void {
+    this.router.navigate([ `/home/entity/view/${rowId}`], { queryParams: { category } });
   }
 
   /**
@@ -126,6 +168,7 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
    * service.
    * @param {number} pageIndex - The pageIndex parameter is used to specify the index of the page to retrieve. It is a
    * number that represents the page number.
+   * @param pageSize
    * @param {any} [sortField=createdDate] - The `sortField` parameter is used to specify the field by which the agents
    * should be sorted. It can be any value, but it is typically a string representing the name of a field in the `AgentDTO`
    * object.
@@ -134,7 +177,6 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
    * default, the sort order is set to "desc".
    * @returns an Observable of type Pagination<AgentDTO>.
    */
-
   getAgents(pageIndex: number,
             pageSize: number,
             sortField: any = 'createdDate',
@@ -185,32 +227,6 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
 
   }
 
-  /**
-   * The function `searchIntermediary` searches for an intermediary agent by name and assigns the search results to the
-   * `intermediaries` variable.
-   * @param {string} name - The "name" parameter is a string that represents the name of the intermediary or agent you want
-   * to search for.
-   */
-  /*searchIntermediary(name: string) {
-    this.intermediaryService.searchAgent(
-      0,
-      5,
-      name
-    )
-      .pipe(untilDestroyed(this))
-      .subscribe( (data) => {
-        this.intermediaries =  data;
-      });
-  }
-  viewDetailsById(rowId: any) {
-    // Implement your logic here using the rowId
-    log.info(`Viewing details for row with ID: ${rowId}`);
-  }
-
-
-  /**
-   * The function navigates to a new entity page with the entity type set to 'Agent'.
-   */
   gotoEntityPage() {
     this.router.navigate(['/home/entity/new'],
       {queryParams: {entityType: 'Agent'}}).then(r => {
@@ -220,117 +236,60 @@ export class ListIntermediaryComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
   }
 
-  /*sortIntermediary() {
-    const sortValues = this.sortingForm.getRawValue();
-    const payload: any = { // todo: create DTO for payload
-      sortListFields: sortValues.dateCreated ? 'dateCreated' : null,
-      order: sortValues.sortOrder
-    }
-    this.sortFilterService.dateSortIntermerdiary(payload.sortListFields, payload.order)
-      .subscribe(data => {
-        this.intermediaries = data;
-        this.cdr.detectChanges();
-      })
-  }*/
-
-  /*intermediarySummary(id: number){
-    let partyId: number;
-
-    // fetch account details to fetch party id before routing to 360 view
-    this.accountService
-      .getAccountDetailsByAccountCode(id)
-      .pipe(
-        map((data: PartyAccountsDetails) => {
-            this.accountService.setCurrentAccounts(data); // set this current as current account.
-            return data?.partyId;
-          },
-          untilDestroyed(this)
-        ))
-      .subscribe( (_x) => {
-        partyId = _x;
-        this.router.navigate([ `/home/view-entity/${partyId}`]);
-      });
-  }*/
-
   filter(event, pageIndex: number = 0, pageSize: number = event.rows, keyData: string) {
 
-   this.intermediaries = null; // Initialize with an empty array or appropriate structure
+   this.intermediaries = null;
 
-   /* const value = (event.target as HTMLInputElement).value;
-
-    log.info('myvalue>>>', value)*/
-
-    // this.searchTerm = value;
-    let data = this.filterObject[keyData];
-    console.log('datalog>>',data, keyData)
+    const key = keyData.split('_')[2];
+    let data = this.filterObject[key];
 
     this.isSearching = true;
     this.spinner.show();
 
     if (data.trim().length > 0 || data === undefined || data === null) {
       this.intermediaryService
-        .searchAgent(
-          pageIndex, pageSize,
-          keyData, data)
-        .subscribe((data) => {
-            this.intermediaries = data;
-            this.spinner.hide();
-          },
-          error => {
-            this.spinner.hide();
-          });
+        .searchAgent(pageIndex, pageSize, key, data).subscribe({
+        next: data => {
+          this.intermediaries = data;
+          this.spinner.hide();
+        },
+        error: err => {
+          this.spinner.hide();
+        }
+      });
     }
     else {
       this.getAgents(pageIndex, pageSize)
         .pipe(
           untilDestroyed(this),
           tap((data) => log.info(`Fetching Agents>>>`, data))
-        )
-        .subscribe(
-          (data: Pagination<AgentDTO>) => {
-            data.content.forEach( intermediary => {
-              intermediary.primaryType = 'I' ? 'Individual' : 'Corporate';
-            });
-            this.intermediaries = data;
-            this.tableDetails.rows = this.intermediaries?.content;
-            this.tableDetails.totalElements = this.intermediaries?.totalElements;
-            this.cdr.detectChanges();
-            this.spinner.hide();
-          },
-          error => { this.spinner.hide(); }
-        );
+        ).subscribe({
+        next: (data: Pagination<AgentDTO>) => {
+          data.content.forEach( intermediary => {
+            intermediary.primaryType = 'I' ? 'Individual' : 'Corporate';
+          });
+          this.intermediaries = data;
+          this.tableDetails.rows = this.intermediaries?.content;
+          this.tableDetails.totalElements = this.intermediaries?.totalElements;
+          this.cdr.detectChanges();
+          this.spinner.hide();
+        },
+        error: err => {
+          this.spinner.hide();
+        }
+      });
     }
 
   }
 
-  inputName(event) {
-
-    const value = (event.target as HTMLInputElement).value;
-      this.filterObject['name'] = value;
-  }
-
-  inputModeOfIdentity(event) {
-
-    const value = (event.target as HTMLInputElement).value;
-    this.filterObject['modeOfIdentity'] = value;
-  }
-
-  inputIdNumber(event) {
-
-    const value = (event.target as HTMLInputElement).value;
-    this.filterObject['agentIdNo'] = value;
-  }
-
-  inputPrimaryType(event) {
-
-    const value = (event.target as HTMLInputElement).value;
-    this.filterObject['primaryType'] = value;
-  }
-
-  inputAccountType(event) {
-
-    const value = (event.target as HTMLInputElement).value;
-    this.filterObject['accountType'] = value;
+  /**
+   * Filter records beased on column filter
+   * @param event
+   * @param field
+   */
+  processInput(event: Event, field: string) {
+    const columnName: string = field.split('_')[2];
+    this.filterObject[columnName] = (event.target as HTMLInputElement).value;
   }
 
 }
