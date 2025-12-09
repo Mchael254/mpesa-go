0af1fa6c4033aa2c29f43dc1288374bc1420da70
HopeIbrahim <hope.ibrahim@turnkeyafrica.com>
2025-12-09 22:44:30 +0300

    VQ-6109

--- src/app/features/base/components/dashboard/tickets-summary/tickets-summary.component.ts
+++ src/app/features/base/components/dashboard/tickets-summary/tickets-summary.component.ts
@@ -1,10 +1,13 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {TicketsService} from "../../../../administration/services/tickets.service";
-import {Router} from "@angular/router";
-import {Logger} from "../../../../../shared/services";
-import {takeUntil, tap} from "rxjs/operators";
-import {TicketCountDTO} from "../../../../administration/data/ticketsDTO";
-import {ReplaySubject} from "rxjs";
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { TicketsService } from "../../../../administration/services/tickets.service";
+import { Router } from "@angular/router";
+import { Logger } from "../../../../../shared/services";
+import { takeUntil, tap } from "rxjs/operators";
+import { TicketCountDTO } from "../../../../administration/data/ticketsDTO";
+import { ReplaySubject } from "rxjs";
+import { TicketingService } from 'src/app/shared/services/ticketing/ticketing.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { TicketCount } from '../../../data/tickets';
 
 const log = new Logger('TicketsSummaryComponent');
 @Component({
@@ -17,26 +20,34 @@ export class TicketsSummaryComponent implements OnInit {
   ticketCount: TicketCountDTO[];
 
   ticketCountTotal: number;
+  user: string;
+  ticketSummary: TicketCount[] = [];
+
 
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   constructor(
     private router: Router,
     private ticketsService: TicketsService,
     private cdr: ChangeDetectorRef,
-  ) {}
+    private ticketingService: TicketingService,
+    public authService: AuthService,
+
+  ) { }
 
   /**
    * The ngOnInit function is called when the component is initialized and it calls the getTicketCountPerModule function.
    */
   ngOnInit(): void {
     this.getTicketCountPerModule();
+    this.getuser()
+    this.fetchGisTickets()
   }
 
   /**
    * The function `goToViewTickets()` navigates to the tickets view in the administration section of the home page.
    */
   goToViewTickets() {
-    this.router.navigate([ `/home/administration/tickets`]);
+    this.router.navigate([`/home/administration/tickets`]);
   }
 
   /**
@@ -51,22 +62,11 @@ export class TicketsSummaryComponent implements OnInit {
       )
       .subscribe((data) => {
         this.ticketCount = data;
-        this.ticketCountTotal = this.addTicketCounts();
         this.cdr.detectChanges();
       })
   }
 
-  /**
-   * The function calculates the total number of tickets by summing up the totalTickets property of each object in the
-   * ticketCount array.
-   * @returns the total count of tickets by summing up the `totalTickets` property of each object in the `ticketCount`
-   * array.
-   */
-  addTicketCounts() {
-    return this.ticketCount.reduce((acc, cur) => {
-      return acc + cur.totalTickets
-    }, 0);
-  }
+
 
   /**
    * The ngOnDestroy function is a lifecycle hook in Angular that is called when a component is about to be destroyed.
@@ -81,13 +81,41 @@ export class TicketsSummaryComponent implements OnInit {
     const totalTickets = ticketCountPerModule?.totalTickets;
     log.info('Dash ticket>>', ticketCountPerModule.activityName, ticketCountPerModule.totalTickets);
 
-    this.ticketsService.ticketFilterObject.set({...ticketCountPerModule, fromDashboardScreen: true});
+    this.ticketsService.ticketFilterObject.set({ ...ticketCountPerModule, fromDashboardScreen: true });
     this.router.navigate(['/home/administration/tickets'],
-      {queryParams: {activityName, totalTickets }}).then(r => {
-    })
+      { queryParams: { activityName, totalTickets } }).then(r => {
+      })
   }
 
   goToDispatch() {
     this.router.navigate([`home/administration/document-dispatch`]);
+  }
+  getuser() {
+    this.user = this.authService.getCurrentUserName();
+    const userDetails = this.authService.getCurrentUser();
+    log.info('Login UserDetails', userDetails);
+
+  }
+  fetchGisTickets() {
+    this.ticketingService.fetchTicketSummary(this.user).subscribe({
+      next: (res) => {
+        log.debug('TICKET Summary:', res)
+        this.ticketSummary = res
+        this.ticketCountTotal = res.reduce((sum, item) => sum + item.taskCount, 0);
+
+
+
+      },
+      error: (error) => {
+        log.debug("error", error)
+        const apiError = error.error;
+        const message =
+          apiError?.errors?.[0] ??
+          apiError?.developerMessage ??
+          'Failed to send message';
+
+        // this.globalMessagingService.displayErrorMessage('error', message);
+      }
+    });
   }
 }
